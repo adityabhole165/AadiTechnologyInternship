8b4d3096a41a77e2a13ebf3996e29c7f4ad425ff
Sejal Patil <sejalpatil2315@gmail.com>
2022-08-05 18:57:04 +0530

    COMPOSE

--- src/components/MessageCenter/Composee.tsx
+++ src/components/MessageCenter/Composee.tsx
@@ -45,7 +45,7 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 function Form13() {
 
-
+  const { Text, To ,Text2 , Attachments, BODY} = useParams();
 
   const classes = Styles();
   const classes1 = useStyles();
