15911d996e33241621f78582359ccf3f9c5b1f45
Ajit Sagale <ajitsagale@gmail.com>
2022-08-18 09:53:32 +0530

    changes for design of progress report card

--- src/libraries/card/Card30.tsx
+++ src/libraries/card/Card30.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
-import { Card, Typography } from '@mui/material';
+import { Container, Card, Typography, Grid } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
 export const Card30 = ({ header }) => {
+    const ExpandIcon = ({ expanded }) =>
+        expanded ? <ExpandLessIcon sx={{ float: "right" }} /> : <ExpandMoreIcon sx={{ float: "right" }} />;
+
     const [enableRow, setEnableRow] = useState(-1)
     const expand = (index) => {
         if (enableRow === index)
@@ -9,28 +14,53 @@ export const Card30 = ({ header }) => {
         else
             setEnableRow(index)
     }
+    const searchBarProps = {
+        markInformation: "A",
+        mark: "B"
+    }
     return (
-        <>
+        <><Container>
             {
                 header.map((Header) => (
-                    <Card key={Header.Id}>
-                        <Typography
-                            onClick={() => expand(Header.Id)}>
-                            {Header.Name}
-                        </Typography>
+
+                    <Card key={Header.Id} sx={{ mt: 1, bgcolor: 'white' }}>           
+                    <Grid container>
+                        <Grid item xs={10} 
+                                onClick={() => expand(Header.Id)}>
+                            <Typography
+                                variant="h4"
+                                sx={{ py: 1, mx: 1 }}>
+                                {Header.Name}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={2} 
+                        alignItems="center"
+                        justifyContent="center"
+                       >
+                            <ExpandIcon expanded={enableRow === Header.Id} />
+                        </Grid>
+
                         {
                             Header.Child.map((Detail) => (
                                 enableRow === Header.Id &&
-                                <Card key={Detail.Id}>
-                                    <Typography>{Detail.Name}</Typography>
-                                    <Typography>{Detail.Value}</Typography>
-                                </Card>
+
+                                (<Grid container key={Detail.Id}>
+                                    <Grid item xs={10} >
+                                        <Typography variant="h6" sx={{ py: 1, mx: 1 }} >
+                                            {Detail.Name}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={2}  >
+                                        {Detail.Value}
+                                    </Grid>
+                                </Grid>)
                             ))
                         }
+                    </Grid>
                     </Card>
                 ))
             }
-        </>
+        </Container></>
     )
 }
 export default Card30;
