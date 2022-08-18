eb6a15db9d6461890292eee8d0e52c4b72432a9e
Ajit Sagale <ajitsagale@gmail.com>
2022-08-18 20:10:28 +0530

    click icon make working

--- src/libraries/card/Card32.tsx
+++ src/libraries/card/Card32.tsx
@@ -4,27 +4,32 @@ import { Container, Card, Typography, Grid, useTheme } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from 'react';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-const Card32 = ({ Id, Name, enableRow, expand }) => {
+const Card32 = ({ Id, Name, expand }) => {
   const theme = useTheme();
-  const ExpandIcon = ({ expanded }) =>
+  const ExpandIcon = () =>
     expanded ? (
       <ExpandLessIcon sx={{float:"right",mr:1}}/>
     ) : (
       <ExpandMoreIcon sx={{float:"right",mr:1}} />
     );
-
+    const [expanded, setExpanded] = useState(true)
+    const expandFunc= () => {
+        setExpanded(!expanded)
+        expand(Id)
+    }
   return (
     <Grid
       container
       sx={{ background: `${theme.colors.gradients.pink1}`, cursor: 'pointer' }}
+      onClick={expandFunc}
     >
-      <Grid item xs={10} onClick={() => expand(Id)}>
+      <Grid item xs={10}>
         <Typography variant="h4" sx={{ py: 1, mx: 1 }}>
           {Name}
         </Typography>
       </Grid>
       <Grid item xs={2} sx={{ py:0.5 }}>
-        <ExpandIcon expanded={enableRow === Id}  />
+        <ExpandIcon />
       </Grid>
     </Grid>
   );
