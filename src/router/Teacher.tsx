8aad03fd208fc6004ad59ecb9e0e7429c196f129
aadiTechnology <aaditechology@gmail.com>
2022-08-21 19:03:23 +0530

    TeacherTimetable folder move from Teacher folder from component

--- src/router/Teacher.tsx
+++ src/router/Teacher.tsx
@@ -13,7 +13,7 @@ const Loader = (Component) => (props) =>
 const TAttendance = Loader(lazy(() => import('src/components/TAttendance/Tattendance')))
 const TExamschedule = Loader(lazy(() => import('src/components/Teacher/TExamschedule/TExamSchedule')))
 const TView = Loader(lazy(() => import('src/components/TAttendance/TView')))
-const TeacherTimetable = Loader(lazy(() => import('src/components/Teacher/TeacherTimetable/TeacherTimetable')));
+const TeacherTimetable = Loader(lazy(() => import('src/components/TeacherTimetable/TeacherTimetable')));
 const MissingAttandence = Loader(lazy(() => import('src/components/TAttendance/MissingAttandence')))
 
 const teacherRoutes = [
