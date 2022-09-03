24c6b376774ab731bf2f11d3b1fa33d9544c913c
renukaadwe <renukaadwe25@gmail.com>
2022-09-03 12:55:20 +0530

    styled

--- src/libraries/card/card2.tsx
+++ src/libraries/card/card2.tsx
@@ -2,6 +2,12 @@ import { Box, Card, Typography, useTheme, Container } from '@mui/material';
 import PropTypes from 'prop-types';
 import { Styles } from 'src/assets/style/student-style';
 import BackButton from '../button/BackButton';
+import {
+  CardDetail3,
+  CardDetail1,
+  ListStyle,
+  BoxWrapper
+} from '../styled/CardStyle';
 
 Card2.propTypes = {
   Title: PropTypes.string,
@@ -24,50 +30,26 @@ function Card2({
   const theme = useTheme();
   const classes = Styles();
   return (
-    <>
-      <Container>
-        <Card
-          sx={{
-            background: `${theme.colors.gradients.pink1}`
-          }}
-        >
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            p={3}
-            alignItems="flex-start"
-            flexDirection="column"
-          >
-            <Typography className={classes.Cardfont1}>
-              {ViewDetail.Title}
-            </Typography>
-            <Typography className={classes.Cardfont2}>{Title}</Typography>
-
-            <Typography className={classes.Cardfont1}>
-              {ViewDetail.Start_Date}
-            </Typography>
-            <Typography className={classes.Cardfont2}>{StartDate}</Typography>
-
-            <Typography className={classes.Cardfont1}>
-              {ViewDetail.End_Date}
-            </Typography>
-            <Typography className={classes.Cardfont2}>{EndDate}</Typography>
-
-            <Typography className={classes.Cardfont1}>
-              {ViewDetail.Standards}
-            </Typography>
-            <Typography className={classes.Cardfont2}>{Standard}</Typography>
-
-            <Typography className={classes.Cardfont1}>
-              {ViewDetail.Description}
-            </Typography>
-            <Typography className={classes.CardBottomMargin}>
-              {Description}
-            </Typography>
-          </Box>
-        </Card>
-      </Container>
-    </>
+    <Container>
+      <ListStyle>
+        <BoxWrapper>
+          <CardDetail1>{ViewDetail.Title}</CardDetail1>
+          <CardDetail3>{Title}</CardDetail3>
+        </BoxWrapper>
+        <BoxWrapper>
+          <CardDetail1>{ViewDetail.Start_Date}</CardDetail1>
+          <CardDetail3>{StartDate}</CardDetail3>
+        </BoxWrapper>
+        <BoxWrapper>
+          <CardDetail1>{ViewDetail.Standards}</CardDetail1>
+          <CardDetail3>{Standard}</CardDetail3>
+        </BoxWrapper>
+        <BoxWrapper>
+          <CardDetail1>{ViewDetail.Description}</CardDetail1>
+          <CardDetail3>{Description}</CardDetail3>
+        </BoxWrapper>
+      </ListStyle>
+    </Container>
   );
 }
 export default Card2;
--- src/libraries/styled/ErrormessageStyled.tsx
+++ src/libraries/styled/ErrormessageStyled.tsx
@@ -4,5 +4,7 @@ export const ErrorDetail = styled(Typography)`
   font-size: 12px;
   color: red;
   margin-top: -10px;
+  text-Align:center
 
 `;
+
