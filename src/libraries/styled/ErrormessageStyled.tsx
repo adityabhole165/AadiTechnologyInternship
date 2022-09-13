73e20ff2a096a73f0f4125d976c3beddcb21ff9f
renukaadwe <renukaadwe25@gmail.com>
2022-09-13 18:11:12 +0530

    styled

--- src/libraries/styled/ErrormessageStyled.tsx
+++ src/libraries/styled/ErrormessageStyled.tsx
@@ -3,7 +3,7 @@ import {  Typography, styled} from '@mui/material';
 export const ErrorDetail = styled(Typography)`
   font-size: 14px;
   color: red;
-   text-Align:center
+   text-Align:center;
    font-family: 'Roboto'
 `;
 export const ErrorDetail1 = styled(Typography)`
