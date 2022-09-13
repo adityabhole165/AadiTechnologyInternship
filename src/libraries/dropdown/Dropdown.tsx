7afbd948faa0cd448a510310beefba99edc7c390
Sejal Patil <sejalpatil2315@gmail.com>
2022-09-13 17:23:23 +0530

    dropdown

---
+++ src/libraries/dropdown/Dropdown.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import moment from 'moment';
+import { FormControl, NativeSelect } from '@mui/material';
+import { GetStandardDivisionsResult } from 'src/interfaces/Teacher/TAttendance';
+
+const Dropdown = ({Array,handleChange}) => {
+
+  
+    return (
+        <>
+            <FormControl variant="standard" fullWidth sx={{ m: 1, mb: 2 }}>
+                <NativeSelect
+                    sx={{ mr: '14px', ml: '-8px' }}
+                    onChange={(e) => handleChange(e.target.value)}
+                    
+                >
+                    <option value="0">Select Class</option>
+                    {Array.map(
+                        (items, i) => {
+                            return (
+                                <>
+                                    <option value={items.value} key={i}>
+                                        {items.Name}
+                                    </option>
+                                </>
+                            );
+                        }
+                    )}
+                </NativeSelect>
+            </FormControl>
+        </>
+    )
+}
+
+export default Dropdown
