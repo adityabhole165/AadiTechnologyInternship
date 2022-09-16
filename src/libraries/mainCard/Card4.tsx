7122312729c42c06b95f126402a06f0cd2301005
renukaadwe <renukaadwe25@gmail.com>
2022-09-16 11:52:22 +0530

    card4cleancode

--- src/libraries/mainCard/Card4.tsx
+++ src/libraries/mainCard/Card4.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
-import { Typography, Grid, useTheme, Box } from '@mui/material';
-import { Styles } from 'src/assets/style/student-style';
-import { useLocation } from 'react-router-dom';
 import {
   CardDetail,
   CardDetail1,
   CardDetail2,
-  CardDetail3,
-  CardWrapper
+  CardDetail3
 } from '../styled/CardStyle';
 
 function Card4({ header, text1, text2, text3 }) {
-  const theme = useTheme();
-  const classes = Styles();
-  const location = useLocation();
-  const pathname = location.pathname;
-  const pageName = pathname.replace('/extended-sidebar/Student/', '');
-
   return (
     <>
-      {pageName === 'Timetable' ? (
-        <CardWrapper>
-          <CardDetail1>{header}</CardDetail1>
-          <CardDetail2>{text3}</CardDetail2>
-        </CardWrapper>
-      ) : (
-        <CardDetail>
-          <CardDetail1>{header}</CardDetail1>
-          <CardDetail2>{text3}</CardDetail2>
-        </CardDetail>
-      )}
+      <CardDetail>
+        <CardDetail1>{header}</CardDetail1>
+        <CardDetail2>{text3}</CardDetail2>
+      </CardDetail>
 
       <CardDetail>
         <CardDetail3>{text1}</CardDetail3>
