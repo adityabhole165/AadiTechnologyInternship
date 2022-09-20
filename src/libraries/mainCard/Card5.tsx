ed2bd1c7167a24eac7621e31071019bbbe36fd88
renukaadwe <renukaadwe25@gmail.com>
2022-09-20 14:58:54 +0530

    centercardaliggnmentfees

--- src/libraries/mainCard/Card5.tsx
+++ src/libraries/mainCard/Card5.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  ListStyle1 } from '../styled/CardStyle';
+import {  CardDetail4, ListStyle } from '../styled/CardStyle';
 import {
   CardDetail,
   CardDetail1,
@@ -11,12 +11,12 @@ import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 function Card5({ text1, text2, clickIcon }) {
   return (
     <div>
-    <ListStyle1>
+    <ListStyle>
         <CardDetail>
-          <CardDetail3><b>{text1}</b>&nbsp;:&nbsp;{text2}</CardDetail3>
+          <CardDetail4><b>{text1}</b>&nbsp;:&nbsp;{text2}</CardDetail4>
           <CardDetail2 onClick={clickIcon}>{<FileDownloadOutlinedIcon />}</CardDetail2>
         </CardDetail>
-      </ListStyle1>
+      </ListStyle>
     </div>
   );
 }
--- src/libraries/styled/CardStyle.tsx
+++ src/libraries/styled/CardStyle.tsx
@@ -18,7 +18,13 @@ export const CardDetail2 = styled(Typography)`
 export const CardDetail3 = styled(Typography)`
   font-size: 12px;
   margin-left: 5px;
- 
+
+  font-family: Roboto
+`;
+export const CardDetail4 = styled(Typography)`
+  font-size: 12px;
+  margin-left: 5px;
+ margin-top:5px;
   font-family: Roboto
 `;
 export const CardDetail = styled(Box)(
@@ -62,24 +68,7 @@ border-radius: 6px;
    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
  `
 );
-export const ListStyle1 = styled(Box)(
-  ({ theme ,color}) => `
 
-//  background: ${theme.colors.gradients.listColor}; 
- background: ${color === 'primary' ? theme.colors.gradients.listColor :
-      color === 'secondary' ? theme.colors.gradients.HighlightedlistColor :
-        color === 'warning' ? theme.colors.gradients.selectedlistColor :
-          theme.colors.gradients.listColor
-    };
-    position:relative;
-
- padding-top:8px;
- padding-left:8px;
-border-radius: 6px;
-   margin-bottom: 8px;
-   box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
- `
-);
 
 export const BoxStyle = styled(Box)(
   ({ theme }) => `
