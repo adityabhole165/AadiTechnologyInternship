d66e5143b4741b005f074dd16d98c8058a9a4fee
renukaadwe <renukaadwe25@gmail.com>
2022-09-26 14:08:33 +0530

    design profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,18 @@
+import { Box } from '@mui/material';
+import { Container } from '@mui/system';
 import Card24 from 'src/libraries/card/Card24';
 import PageHeader from 'src/libraries/heading/PageHeader';
-
+import Card6 from 'src/libraries/mainCard/Card6';
 function Profile() {
   return (
-    <>
-      <PageHeader heading={'My Profile'} subheading={''} />
-      <Card24 />
-    </>
+    // <Box sx={{backgroundColor:"#bbdefb",Top:0,  position: 'fixed',width:"100%",height:"100%"}}>
+      <Container>
+          <PageHeader heading={'My Profile'} subheading={''} />
+          <Card24 />
+      </Container>
+  
+      // {/* <Card6/> */}
+    // </Box>
   );
 }
 export default Profile;
