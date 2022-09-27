b17fa9b2739e82d6eeff84bd4426317d14f0e031
Ajit Sagale <ajitsagale@gmail.com>
2022-09-27 10:09:40 +0530

    add dotlegend styled component

---
+++ src/libraries/styled/DotLegendStyled.tsx
@@ -0,0 +1,19 @@
+import { Box, styled } from "@mui/material";
+
+export const DotLegendStyled = styled(Box)(
+    ({ theme, color }) => `
+        border-radius: 22px;
+        width: ${theme.spacing(1.5)};
+        height: ${theme.spacing(1.5)};
+        display: inline-block;
+        margin-right: ${theme.spacing(1)};
+        margin-top: -${theme.spacing(0.1)};
+        
+        background: ${color === 'primary' ? theme.colors.gradients.listColor :
+            color === 'secondary' ? theme.colors.gradients.HighlightedlistColor :
+            color === 'warning' ? theme.colors.gradients.selectedlistColor :
+            color === 'info' ? theme.colors.gradients.pageBackground :
+                    theme.colors.gradients.primayButton
+        }
+    `
+);
