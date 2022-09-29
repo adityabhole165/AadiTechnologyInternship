8496c009115930dee9672eb8b039781ff53eb2c2
MayurChavan26 <mayurchavan07777@gmail.com>
2022-09-29 18:12:36 +0530

    Remove link

--- src/libraries/mainCard/Card3.tsx
+++ src/libraries/mainCard/Card3.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import {
-  Paper,
-  Grid,
-  useTheme,
-  Container,
-  IconButton,
-  Typography,
-  Box,
-  Card,
-  Avatar
-} from '@mui/material';
-
+import {Paper,Grid,useTheme,Container,IconButton,Typography,Box,Card,Avatar} from '@mui/material';
 import EventIcon from '@mui/icons-material/Event';
 import CakeIcon from '@mui/icons-material/Cake';
 import SmsIcon from '@mui/icons-material/Sms';
@@ -24,36 +13,20 @@ import LockOpenIcon from '@mui/icons-material/LockOpen';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import AssignmentIcon from '@mui/icons-material/Assignment';
-
 import { Link } from 'react-router-dom';
 import { iteratorSymbol } from 'immer/dist/internal';
 import { makeStyles } from '@mui/styles';
 
-function Card3({
-  color,
-  text1,
-  text2,
-  icon,
-  iconColor,
-  opacityLevel,
-  Link1,
-  isAvtar,
-    
-  ImageUrl
-}) {
+function Card3({color,text1,text2,icon,iconColor,opacityLevel,Link1,isAvtar,ImageUrl}) {
   const theme = useTheme();
-  if (
-    theme.colors.gradients.pink1 !==
-    'linear-gradient(135deg, white 0%, white 100%);'
-  )
+  if (theme.colors.gradients.pink1 !== 'linear-gradient(135deg, white 0%, white 100%);')
     iconColor = 'white';
   return (
     <>
-         <Link to={`/${location.pathname.split('/')[1]}/${Link1}`} >
+      <Link to={`/${location.pathname.split('/')[1]}/${Link1}`} style={{textDecoration:'none'}}>
       <Box
         sx={{
           mb: '10px',
@@ -64,8 +37,6 @@ function Card3({
         <IconButton>
           <Box
             sx={{
-              // width: '150px',
-         
               width: 70,
               height: 70,
               background: `${theme.colors.gradients.pink1}`,
@@ -78,7 +49,6 @@ function Card3({
                 ' 1px 4px 5px 2px rgba(0,0,0,0.3)'
             }}
           >
-   
             {icon === 1 && <AssignmentIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
             {icon === 2 && <EventIcon  sx={{color: iconColor ,mt:"7px"}} fontSize="medium" />}
             {icon === 3 && <DateRangeIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
@@ -90,11 +60,9 @@ function Card3({
             {icon === 9 && <MenuBookIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
             {icon === 10 && <LockOpenIcon sx={{color: iconColor,mt:"7px"}}  fontSize="medium" />}
             {icon === 11 && <PeopleIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
-            
-           
-             {icon === 12 && <img src={ImageUrl} alt="" style={{  width:"20px", height:"22px" ,marginTop:"8px",filter: "invert(54%) sepia(88%) saturate(362%) hue-rotate(151deg) brightness(94%) contrast(92%)" }} />}
+
+            {icon === 12 && <img src={ImageUrl} alt="" style={{  width:"20px", height:"22px" ,marginTop:"8px",filter: "invert(54%) sepia(88%) saturate(362%) hue-rotate(151deg) brightness(94%) contrast(92%)" }} />}
             {icon === 13 && <img src={ImageUrl} alt="" style={{  width:"20px", height:"22px" ,marginTop:"8px"}}/>}
-            
             {icon === 14 && <img src={ImageUrl} alt="" style={{  width:"20px", height:"22px" ,marginTop:"8px",filter: "invert(40%) sepia(87%) saturate(1676%) hue-rotate(333deg) brightness(92%) contrast(105%)" }} />}
             {icon === 15 && <img src={ImageUrl} alt="" style={{  width:"20px", height:"22px" ,marginTop:"8px",filter:"invert(54%) sepia(88%) saturate(362%) hue-rotate(151deg) contrast(94%)  brightness(92%) "}}/>}
             {icon === 16 && <img src={ImageUrl} alt="" style={{  width:"20px", height:"22px",marginTop:"8px"}}/>}
@@ -105,11 +73,7 @@ function Card3({
             {icon === 20 && <CakeIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
             {icon === 21 && <ForwardToInboxIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
             {icon === 22 && <ForwardToInboxIcon sx={{color: iconColor,mt:"7px"}} fontSize="medium" />}
-            {/* <IconButton> */}
            
-            {/* </IconButton> */}
-           
-        
             <Typography
               sx={{
                 mt: '-3px',
@@ -138,7 +102,6 @@ function Card3({
             </Typography>
           </Box>
           </IconButton>
-        
       </Box>
       </Link>
  
