21ef8acee675974f0994fb478c80e43d57bfae70
Sejal Patil <sejalpatil2315@gmail.com>
2022-10-07 08:54:45 +0530

    radio

--- src/libraries/mainCard/Card4.tsx
+++ src/libraries/mainCard/Card4.tsx
@@ -3,18 +3,21 @@ import { CardDetail,CardDetail1,CardDetail2,CardDetail3} from '../styled/CardSty
 import { useLocation } from 'react-router-dom';
 
 function Card4({ header, text1, text2, text3, text5 }) {
+  console.log("text1",text1);
+  
   const location = useLocation();
   const pathname = location.pathname;
   const pageName = pathname.replace('/extended-sidebar/Common/', '');
   return (
     <>
       <CardDetail>
-        {pageName == "Feedback" ? null : <CardDetail1>{header}</CardDetail1>}
-        {pageName == "Feedback" ? null : <CardDetail2>{text3}</CardDetail2>}
+      {pageName == "Feedback" ? null :<CardDetail1>{header}</CardDetail1>}
+         <CardDetail2>{text3}</CardDetail2>
         </CardDetail><CardDetail>
-        <CardDetail3>{text1}</CardDetail3>
-        {pageName == "Feedback" ? null : <CardDetail2>{text2}</CardDetail2>} </CardDetail>
-      {pageName == "Feedback" ? null : <CardDetail3 color="primary">{text5}</CardDetail3>}
+        {pageName == "Feedback" ? null : <CardDetail3>{text1}</CardDetail3>}
+        {pageName == "Feedback" ? null : <CardDetail2>{text2}</CardDetail2>}
+          </CardDetail>
+         {pageName == "Feedback" ? null : <CardDetail3 color="primary">{text5}</CardDetail3>}
     </>
   );
 }
