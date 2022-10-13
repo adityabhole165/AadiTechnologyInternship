8735831411817c79abb026cf595dd40dfff10024
renukaadwe <renukaadwe25@gmail.com>
2022-10-13 11:37:16 +0530

    profile fontsize

--- src/libraries/styled/ProfileStyled.tsx
+++ src/libraries/styled/ProfileStyled.tsx
@@ -2,24 +2,26 @@ import { Typography, styled } from '@mui/material';
 
 export const ProfileDetail1 = styled(Typography)`
   font-weight: 600;
-
+font-size:12px;
   margin-top: 10px;
   margin-left: 10px;
 `;
 export const ProfileDetail2 = styled(Typography)`
   margin-bottom: 10px;
   margin-top: -7px;
+  font-size:12px;
  
 `;
 export const ProfileDetail3 = styled(Typography)`
   font-weight: bold;
-
+  font-size:12px;
   margin-bottom: 10px;
   margin-top: 7px;
 `;
 export const ProfileDetail4 = styled(Typography)`
   margin-top: 10px;
   margin-left: 10px;
+  font-size:12px;
   
 `;
 export const ProfileWrapper = styled(Typography)`
