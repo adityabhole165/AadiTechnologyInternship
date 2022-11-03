abcdc848d09ee820e42363cd393df9bba18176ee
Sejal Patil <sejalpatil2315@gmail.com>
2022-11-03 15:19:39 +0530

    changes

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PageHeader from 'src/libraries/heading/PageHeader';
 import Card4 from 'src/libraries/mainCard/Card4';
 import Card1 from 'src/libraries/mainCard/Card1';
-import { Container } from '@mui/system';
+import { Container } from '@mui/material';
 import { ListStyle } from 'src/libraries/styled/CardStyle';
 import List1 from 'src/libraries/mainCard/List1';
 import Card7 from 'src/libraries/mainCard/Card7';
