239db1abddd52b9ab6a887994f9d4b221e439102
Ajit Sagale <ajitsagale@gmail.com>
2023-01-10 14:20:45 +0530

    new component

---
+++ src/libraries/dropdown/DropdownNew.tsx
@@ -0,0 +1,29 @@
+import { FormControl, InputLabel, NativeSelect } from '@mui/material';
+
+const DropdownNew = ({ Itemlist, onChange, Label, DefaultValue }) => {
+    const onClick = (value) => {
+        onChange(
+        Itemlist.map((item) => {
+            return { ...item, IsActive: item.Value === value ? true : false }
+        }))
+    }
+    return (
+        <FormControl fullWidth>
+            <InputLabel htmlFor="select">{Label}</InputLabel>
+            <NativeSelect fullWidth
+                value={DefaultValue}
+                onChange={(e) => onClick(e.target.value)}
+                id="select"
+            >
+                {Itemlist.map((Item, i) => {
+                    return (
+                        <option value={Item.Value} key={i}>
+                            {Item.Name}
+                        </option>
+                    );
+                })}
+            </NativeSelect>
+        </FormControl>)
+}
+
+export default DropdownNew
