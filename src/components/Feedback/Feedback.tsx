9394568a8c61b9d9d11b8e707d4beeb044b8f71b
renukaadwe <renukaadwe25@gmail.com>
2023-01-17 16:15:18 +0530

    error message feedback

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -8,13 +8,17 @@ import CardText3 from 'src/libraries/card/CardText3';
 import { Container } from '@mui/material';
 import { ButtonPrimary } from 'src/libraries/styled/ButtonStyle';
 import TopScroll from 'src/libraries/card/TopScroll';
-
+import ErrorMessages from 'src/libraries/ErrorMessages/ErrorMessages';
+import SuspenseLoader from 'src/layouts/components/SuspenseLoader';
 function Feedback() {
 
   const dispatch = useDispatch();
   const myRef = useRef(null);
   const Feedback: any = useSelector(
     (state: RootState) => state.FeedBack.FeedbackList
+  );
+  const loading = useSelector(
+    (state: RootState) => state.FeedBack.Loading
   );
   const executeScroll = () => myRef.current.scrollIntoView({ top: 0, behavior: 'smooth' });
   const FeedbackBody =
@@ -38,16 +42,26 @@ function Feedback() {
   }, []);
 
   return (
-    <>
-    <div ref={myRef}></div>
     <Container>
-      <PageHeader heading={'Feedback'} subheading={''} />
-      <ButtonPrimary sx={{float:"right",mt:-1}}>Add Feedback</ButtonPrimary>
-      <br/>
-      <CardText3 itemList={Feedback}/>
-      <TopScroll executeScroll={executeScroll}/>
-    </Container>
-     </>
+    <div ref={myRef}></div>
+    <PageHeader heading={'Feedback'} subheading={''} />
+    <ButtonPrimary sx={{float:"right",mt:-1}}>Add Feedback</ButtonPrimary>
+    <br/>
+    {loading ? (
+        <SuspenseLoader />
+      ) : (
+        <>
+           {(Feedback.length == undefined) ? (<ErrorMessages Error={'No records found'} />):
+             (<>
+                   <CardText3 itemList={Feedback}/>       
+                   <TopScroll executeScroll={executeScroll}/>
+             </>)}
+       </>
+      
+      )}
+   
+     </Container>
+  
   )
 }
 
