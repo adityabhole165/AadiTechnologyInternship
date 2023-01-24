6e534a8632c5c3c40baeefbc9e7e1bf2a98d9129
renukaadwe <renukaadwe25@gmail.com>
2023-01-24 18:03:21 +0530

    delete for everyone

--- src/interfaces/MessageCenter/IDeleteMsgPermanently..ts
+++ src/interfaces/MessageCenter/IDeleteMsgPermanently..ts
@@ -3,7 +3,7 @@ export interface IDeleteMessagePermanentlyBody {
     asSchoolId: string,
     asAcademicYearId: string,
     asUserId: string,
-    asMessaageIds: string,
+    asMessageIds: string,
 }
 
 export interface IDeleteMessagePermanentlyResult {
