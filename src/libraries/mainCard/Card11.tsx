414f248c1bc329259be7e397a1d778adb6c193ff
renukaadwe <renukaadwe25@gmail.com>
2023-01-30 16:56:40 +0530

    fontSize

--- src/libraries/mainCard/Card11.tsx
+++ src/libraries/mainCard/Card11.tsx
@@ -4,7 +4,7 @@ import { Typography} from "@mui/material"
 function Card11({item}) {
   return (
     <>
-         <Typography variant='h5'>{item.header} </Typography>
+         <Typography variant='body1'>{item.header} </Typography>
           <Typography variant='body2'>{item.text1}</Typography>
     </>
   )
