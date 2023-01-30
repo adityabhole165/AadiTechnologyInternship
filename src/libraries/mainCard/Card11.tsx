7cc791d0b412bee48ff4e4fd331730a2940b5013
renukaadwe <renukaadwe25@gmail.com>
2023-01-30 17:10:00 +0530

    font

--- src/libraries/mainCard/Card11.tsx
+++ src/libraries/mainCard/Card11.tsx
@@ -4,8 +4,8 @@ import { Typography} from "@mui/material"
 function Card11({item}) {
   return (
     <>
-         <Typography variant='body1'>{item.header} </Typography>
-          <Typography variant='body2'>{item.text1}</Typography>
+         <Typography  variant='body2' sx={{color:"black",fontWeight:"bold"}}>{item.header} </Typography>
+          <Typography variant='body2' sx={{color:"black"}}>{item.text1}</Typography>
     </>
   )
 }
