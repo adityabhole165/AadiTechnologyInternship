3b4316d77585373b56bd38641afb6a92498c4447
renukaadwe <renukaadwe25@gmail.com>
2023-01-30 10:08:30 +0530

    fontFamily

--- src/theme/schemes/PurpleFlowTheme.ts
+++ src/theme/schemes/PurpleFlowTheme.ts
@@ -1206,7 +1206,7 @@ export const PurpleFlowTheme = createTheme({
   },
   typography: {
     fontFamily:
-      '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+      'Roboto',
     h1: {
       fontWeight: 700,
       fontSize: 35
