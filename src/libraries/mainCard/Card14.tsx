7d3842522ea3af6b15efbe0157db803629b9f011
renukaadwe <renukaadwe25@gmail.com>
2023-02-01 10:11:08 +0530

    libray

--- src/libraries/mainCard/Card14.tsx
+++ src/libraries/mainCard/Card14.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Typography,Box,Card } from '@mui/material'
+import { Typography,Box,Card, IconButton } from '@mui/material'
 
-function Card14({Text1,Text2,Text3,Text4,Text5,Text6}) {
+function Card14({Text1,Text2,Text3,Text4,Text5,Text6,conformMsg}) {
+
   return (
     <div>
-       <Card  component={Box} p={1}>
+      <Card  component={Box} p={1}>
     <Box display={"flex"} justifyContent={'space-between'} >
       <Typography variant='body2'> <b>Author : </b> {Text1}</Typography>
       <Typography variant='body2'> <b>Available  : </b> {Text5}</Typography>
@@ -15,13 +16,10 @@ function Card14({Text1,Text2,Text3,Text4,Text5,Text6}) {
     </Box>
     <Box display={"flex"} justifyContent={'space-between'} >
     <Typography variant='body2'> <b> Language : </b>{Text4}</Typography>
-    <Typography variant='body2'> Claim </Typography>
+    <Typography variant='body2' onClick={conformMsg}> Claim </Typography>
     </Box>
-      
-      <Typography variant='body2'> <b>  Standards : </b> {Text3}</Typography>
-  
-   
-    </Card>
+    <Typography variant='body2'> <b>  Standards : </b> {Text3}</Typography>
+   </Card>
     </div>
   )
 }
