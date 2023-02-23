e9ee3ac59f76fc018bff3330b48711fe4e122ae4
Ajit Sagale <ajitsagale@gmail.com>
2023-02-23 16:06:36 +0530

    rank wise image

--- src/libraries/card/Card36.tsx
+++ src/libraries/card/Card36.tsx
@@ -5,28 +5,38 @@ import { AccordianHeader, Header1, Header2 } from '../styled/AccordianStyled';
 import { Box } from '@mui/system';
 
 const Card36 = ({ Id, Name, expand, isActive, Rank, Percentage, Rollno, Presentdays }) => {
-
+  let isThirdAbove = false
+  if (Rollno.toString() === sessionStorage.getItem('RollNo')) {
+    const arr = ["1", "2", "3"]
+    arr.map((item) => {
+      if (Rank.indexOf(item) > 0)
+        isThirdAbove = true;
+    })
+  }
+  else
+    isThirdAbove = true
 
   const theme = useTheme();
   return (
     <AccordianHeader onClick={() => expand(Id)}>
-      {/* <Medale Rank={Rank} /> */}
-      <Box sx={{mr:0.8,mt:0.8}}>
-      <img src={localStorage.getItem('SiteURL') + '/' + Rank.replace('~/','')} width={25} height={35} />
-      </Box><Grid container>
+      {isThirdAbove &&
+        <Box sx={{ mr: 0.8, mt: 0.8 }}>
+          <img src={localStorage.getItem('SiteURL') + '/' + Rank.replace('~/', '')} width={25} height={35} />
+        </Box>}
+      <Grid container>
         <Grid item xs={12}>
-         
-            <Header1 color={isActive ? 'secondary' : ''}>{Rollno + " "}{Name} </Header1>
 
-         
+          <Header1 color={isActive ? 'secondary' : ''}>{Rollno + " "}{Name} </Header1>
+
+
         </Grid>
-        
-          <Grid item xs={6}>
-            Present days: {Presentdays}
-          </Grid>
-          <Grid item xs={6}>
-            Percentage: {Percentage}
-         
+
+        <Grid item xs={6}>
+          Present days: {Presentdays}
+        </Grid>
+        <Grid item xs={6}>
+          Percentage: {Percentage}
+
 
 
         </Grid>
