d4add87ab70d971989ace1d8528ae6b3d9605278
Sejal Patil <sejalpatil2315@gmail.com>
2023-02-25 10:53:42 +0530

    attendance topper ui

--- src/libraries/card/Card36.tsx
+++ src/libraries/card/Card36.tsx
@@ -20,37 +20,35 @@ const Card36 = ({ Id, Name, expand, isActive, Rank, Percentage, Rollno, Presentd
   const theme = useTheme();
   return (
     <AccordianHeader onClick={() => expand(Id)} >
-         <Grid container>
+      <Grid container>
       {isThirdAbove &&
-        <Grid item xs={2}>
-          <img src={localStorage.getItem('SiteURL') + '/' + Rank.replace('~/', '')} width={25} height={35} />
+        <Grid item xs={1}>
+          <img src={localStorage.getItem('SiteURL') + '/' + Rank.replace('~/', '')} width={25} height={30} />
           </Grid>
         }
    
         <Grid item xs={9}>
        <Header1 color={isActive ? 'secondary' : ''}>{Rollno + "." + " "+ " "} {Name} </Header1>
         </Grid>
-        <Grid item xs={1} sx={{float:"right"}}>
+        <Grid item xs={1} sx={{ml:'20px'}}>
         {isActive ?
           <ExpandLessIcon /> :
           <ExpandMoreIcon />}
           </Grid>
     
-          <Grid item xs={2}/>
-        <Grid item xs={6}>
-          <b>
-          Present days:</b> {Presentdays}
+          <Grid item xs={1.5}/>
+        <Grid item xs={5}>
+         <Box sx={{mr:"5px"}} fontSize={'12px'}><b> Present days:</b> {Presentdays}</Box> 
           
         </Grid>
-        <Grid item xs={4} sx={{float:"right"}}>
+        <Grid item xs={5} sx={{float:"right",fontSize:'12px'}}>
           <b>Percentage:</b> {Percentage + "%"}
           </Grid>
       
 
       </Grid>
- 
-      
-    
+
+
 
     </AccordianHeader>
   );
