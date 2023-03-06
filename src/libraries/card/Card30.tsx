bb1b117364c4d0bffcde96862b9a7516a0b3a7cc
Ajit Sagale <ajitsagale@gmail.com>
2023-03-06 17:54:38 +0530

    grow on list and not on card

--- src/libraries/card/Card30.tsx
+++ src/libraries/card/Card30.tsx
@@ -2,22 +2,31 @@ import { useState } from 'react';
 import Card32 from './Card32';
 import List23 from '../list/List23';
 import { ListStyle } from '../styled/CardStyle';
+import { Grow } from '@mui/material';
 
 export const Card30 = ({ header }) => {
 
     const [enableRow, setEnableRow] = useState(-1)
+    const [checked, setChecked] = useState(true)
 
     return (<>
+
         {
             header.map((Header, index) => (
-                <ListStyle key={index}>
-                    <Card32 Id={index} Name={Header.Name}
-                        expand={() => setEnableRow(enableRow === index ? -1 : index)}
-                        isActive={enableRow === index} />
-                    {enableRow === index &&
-                        <List23 data={Header.Child} />
-                    }
-                </ListStyle>
+                <Grow key={index}
+                    in={checked}
+                    style={{ transformOrigin: '0 0 0' }}
+                    {...(checked ? { timeout: 1000 } : {})}
+                >
+                    <ListStyle>
+                        <Card32 Id={index} Name={Header.Name}
+                            expand={() => setEnableRow(enableRow === index ? -1 : index)}
+                            isActive={enableRow === index} />
+                        {enableRow === index &&
+                            <List23 data={Header.Child} />
+                        }
+                    </ListStyle>
+                </Grow>
             ))
         }
     </>)
