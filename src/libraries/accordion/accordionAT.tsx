f4d5ac96cd106bc6ec649c4ca34ea6b8ab435e7a
Snehavkale <snehavkale04@gmail.com>
2023-03-08 11:19:56 +0530

    Attendance

---
+++ src/libraries/accordion/accordionAT.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { Accordion, AccordionSummary,AccordionDetails , Typography,Box} from '@mui/material'
+
+const AccordionAT = ({ Item }) => {
+
+    
+    return (
+        <div>
+            <Accordion>
+                <AccordionSummary expandIcon={"^"}><Typography variant="body2">{Item.RollNo}. {Item.Header}</Typography> 
+              <b>Present Days:</b> <Typography> {Item.Presentdays} <b>Percentage:</b>{Item.Percentage} </Typography></AccordionSummary>
+                <AccordionDetails>
+                    <Box>
+                        <Typography></Typography>
+                    </Box>
+                </AccordionDetails>
+            </Accordion>
+               
+                
+
+        </div>
+    )
+}
+
+export default AccordionAT
