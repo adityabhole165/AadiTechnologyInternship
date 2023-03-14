69f9221b699699aaefdc9e3f8106b1c7e697deed
Snehavkale <snehavkale04@gmail.com>
2023-03-14 12:48:14 +0530

    transport

--- src/libraries/accordion/AccordionTrc.tsx
+++ src/libraries/accordion/AccordionTrc.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Accordion, AccordionDetails, useTheme } from '@mui/material';
+import { Accordion, AccordionDetails, useTheme, Card } from '@mui/material';
 import { Accordionsummary, Header1 } from '../styled/AccordianStyled';
 import { Styles } from 'src/assets/style/student-style';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -8,35 +8,45 @@ import { ListStyle } from '../styled/CardStyle';
 import CardNew from '../card/CardNew';
 
 
-function AccordionTrC({ header, handleChange, isExpanded, Name }) {
-
+function AccordionTrC({ header, handleChange, isExpanded, Name, Data }) {
+   
     const theme = useTheme();
     const classes = Styles();
     return (
         <div>
             <Accordion expanded={isExpanded}
-             className={classes.background}
+                className={classes.background}
                 onChange={handleChange}
                 elevation={0}
                 disableGutters>
 
                 <Accordionsummary
-                   expandIcon={<ExpandMoreIcon sx={{ color: 'black' }} />}
-                   aria-controls="panel1bh-content"
-                   id="panel1bh-header"
-                   sx={{
-                       background: `${theme.colors.gradients.pink1}`,
-                       boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
-                       mb: 1,
-                       color: isExpanded
-                           ? `${theme.colors.gradients.accordianHeadercolor}`
-                           : ''
-                   }}
-                    >
+                    expandIcon={<ExpandMoreIcon sx={{ color: 'black' }} />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                    sx={{
+                        background: `${theme.colors.gradients.pink1}`,
+                        boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
+                        mb: 1,
+                        color: isExpanded
+                            ? `${theme.colors.gradients.accordianHeadercolor}`
+                            : ''
+                    }}
+                >
                     {header}
                 </Accordionsummary>
-                <AccordionDetails sx={{ borderRadius: 1 ,mb: -1}}>
-                    <CardNew/>
+                <AccordionDetails sx={{ borderRadius: 1, mb: -1 }}>
+
+                    {
+                        Data.map((Item, i) => {
+                            return (
+                                <CardNew key={i}
+
+                                    Data={Item} />
+                            )
+                        })
+                    }
+
                 </AccordionDetails>
             </Accordion>
 
