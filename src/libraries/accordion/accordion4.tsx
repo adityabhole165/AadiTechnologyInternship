e3d3e14f8430cd5cb421d2423e4883a80d37107a
Ajit Sagale <ajitsagale@gmail.com>
2023-03-15 22:51:54 +0530

    AllowBookClaimForParent

--- src/libraries/accordion/accordion4.tsx
+++ src/libraries/accordion/accordion4.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -7,8 +7,8 @@ import { Styles } from 'src/assets/style/student-style';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import PropTypes from 'prop-types';
-import { Box, Container, Grow, Grid, List, useTheme , makeStyles, Dialog, Button} from '@mui/material';
-import {CardDetail1,CardDetail3,ListStyle,CardWrapper} from '../styled/CardStyle';
+import { Box, Container, Grow, Grid, List, useTheme, makeStyles, Dialog, Button } from '@mui/material';
+import { CardDetail1, CardDetail3, ListStyle, CardWrapper } from '../styled/CardStyle';
 import { Accordionsummary1, Header3, HeaderAcc } from '../styled/AccordianStyled';
 import Card14 from '../mainCard/Card14';
 import { ButtonPrimary } from '../styled/ButtonStyle';
@@ -41,82 +41,83 @@ function Accordion4({
   IsForIssue,
   Collapse, expand, ClickReserve, AllowBookClaimForParent
 }) {
-  
 
- 
+
+
   const theme = useTheme();
   const [checked, setChecked] = useState(true);
   const classes = Styles();
   const [open, setOpen] = React.useState(false);
 
-const clickParentStudent = (value) =>{
-  ClickReserve({aiBookId:Book_Id,aiFlag:value})
-  setOpen(false);
-}
+  const clickParentStudent = (value) => {
+    ClickReserve({ aiBookId: Book_Id, aiFlag: value })
+    setOpen(false);
+  }
 
+  const clickClaim = () => {
+    if (AllowBookClaimForParent)
+      setOpen(true);
+    else
+      clickParentStudent(0)
+  }
   return (
     <>
-   
-    <Grow
-      in={checked}
-      style={{ transformOrigin: '0 0 0' }}
-      {...(checked ? { timeout: 1000 } : {})}
-      >
-      <Accordion
-        className={classes.background}
-        expanded={expand === index}
-        onChange={Collapse(index)}
-        elevation={0}
-        disableGutters sx={{mb:"10px"}}
-      >
-        <Accordionsummary1
-          expandIcon={<ExpandMoreIcon sx={{ color: 'black' }} />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-          sx={{background: `${theme.colors.gradients.pink1}`}} >
-         <HeaderAcc  color={expand === index ? 'secondary' : ''}>
-      
-         <b>Book Title</b>  : {title}
-         <Box sx={{display:"flex"}}>
-       
-         <b>Available</b> : ({available}/{total})
-         {available === 0 &&
-         <Typography  onClick={()=>{setOpen(true)}} sx={{color:"#628def",ml:"70px"}}> Claim </Typography>
-         }
-        
-         <Dialog
-          open={open}
-          onClose={()=>{setOpen(false)}}
-          PaperProps={{ sx: { position: 'fixed', top: 190, m: 0 ,p:1} }}
-        >
-          <Typography>Claim this book for -- </Typography>
 
-         <Grid container spacing={2}>
-            <Grid item xs={6}>
-            <ButtonPrimary onClick={()=>{clickParentStudent(0)}}> Student</ButtonPrimary>
-            </Grid>
-            <Grid item xs={6}>
-            <ButtonPrimary onClick={()=>{clickParentStudent(1)}}> Parent</ButtonPrimary>
-            </Grid>
-          </Grid>
-          
-         
-        
-    
-   
-      </Dialog>
-         </Box>   
-       </HeaderAcc >
-       </Accordionsummary1>
-       <AccordionDetails>
-      <Card14 Text1={author} Text2={publisher} 
-      Text3={standard} Text4={language} Text5={available} Text6={total} Text7={no} />
-      </AccordionDetails>
-      </Accordion>
-           
-          </Grow>
-       
-     
+      <Grow
+        in={checked}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(checked ? { timeout: 1000 } : {})}
+      >
+        <Accordion
+          className={classes.background}
+          expanded={expand === index}
+          onChange={Collapse(index)}
+          elevation={0}
+          disableGutters sx={{ mb: "10px" }}
+        >
+          <Accordionsummary1
+            expandIcon={<ExpandMoreIcon sx={{ color: 'black' }} />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+            sx={{ background: `${theme.colors.gradients.pink1}` }} >
+            <HeaderAcc color={expand === index ? 'secondary' : ''}>
+
+              <b>Book Title</b>  : {title}
+              <Box sx={{ display: "flex" }}>
+
+                <b>Available</b> : ({available}/{total})
+                {available === 0 &&
+                  <Typography onClick={clickClaim} sx={{ color: "#628def", ml: "70px" }}>
+                    Claim </Typography>
+                }
+
+                <Dialog open={open}
+                  onClose={() => { setOpen(false) }}
+                  PaperProps={{ sx: { position: 'fixed', top: 190, m: 0, p: 1 } }}
+                >
+                  <Typography>Claim this book for -- </Typography>
+
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <ButtonPrimary onClick={() => { clickParentStudent(0) }}> Student</ButtonPrimary>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <ButtonPrimary onClick={() => { clickParentStudent(1) }}> Parent</ButtonPrimary>
+                    </Grid>
+                  </Grid>
+                </Dialog>
+              </Box>
+            </HeaderAcc >
+          </Accordionsummary1>
+          <AccordionDetails>
+            <Card14 Text1={author} Text2={publisher}
+              Text3={standard} Text4={language} Text5={available} Text6={total} Text7={no} />
+          </AccordionDetails>
+        </Accordion>
+
+      </Grow>
+
+
     </>
   );
 }
