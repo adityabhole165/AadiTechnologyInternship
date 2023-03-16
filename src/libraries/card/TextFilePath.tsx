c5e02c56ba4f203dd2277ad6a666a253a2bff887
Ajit Sagale <ajitsagale@gmail.com>
2023-03-16 14:34:57 +0530

    file dimension valiadtion

--- src/libraries/card/TextFilePath.tsx
+++ src/libraries/card/TextFilePath.tsx
@@ -1,35 +1,47 @@
-import { TextField, Box ,Grid, Typography,Stack, IconButton} from '@mui/material'
+import { TextField, Box, Grid, Typography, Stack, IconButton } from '@mui/material'
 import { useEffect, useRef, useState } from 'react'
 import { ChangeFileIntoBase64, CheckFileValidationUploadPic } from 'src/components/Common/Util';
 import ErrorMessages from '../ErrorMessages/ErrorMessages';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 function TextFilePath({ item, onFileSelect, onTextChange }) {
+  const  width = 112, height = 151, maxFileSize = 80000
+  const arrAllowedFiles = ['jpg', 'jpeg', 'png', 'bmp']
   const [error, setError] = useState('')
   const aRef = useRef(null);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     setError('')
-    aRef.current.value=""
-  },[item.Id])
+    aRef.current.value = ""
+  }, [item.Id])
   const handleClick = event => {
     aRef.current.click();
-};
+  };
 
   const changeFile = async (e) => {
-    let isValid = CheckFileValidationUploadPic(e.target.files[0], ['jpg', 'jpeg', 'png', 'bmp'], 80000)
+    let isValid = CheckFileValidationUploadPic(e.target.files[0], arrAllowedFiles, maxFileSize)
     setError(isValid)
     if (isValid === null) {
       if (e.target.files && e.target.files.length > 0) {
         let base64URL: any = await ChangeFileIntoBase64(e.target.files[0]);
         let DataAttachment = base64URL.slice(base64URL.indexOf(',') + 1);
-        item = {
-          ...item,
-          Value: DataAttachment,
-          fileName: e.target.files[0].name,
-          selectedFile: e.target.files[0].name
+        let img = new Image()
+        img.src = window.URL.createObjectURL(e.target.files[0])
+        img.onload = () => {
+          if (img.width > width && img.height > height) {
+            setError(`Image is ${img.height} x ${img.width}, Upload image with Max Height: 151px and Max Width: 112px.`);
+          }
+          else {
+            item = {
+              ...item,
+              Value: DataAttachment,
+              fileName: e.target.files[0].name,
+              selectedFile: e.target.files[0].name
+            }
+            onFileSelect(item)
+          }
         }
-        onFileSelect(item)
+
       }
     }
     else {
@@ -41,26 +53,26 @@ function TextFilePath({ item, onFileSelect, onTextChange }) {
     <div>
       {(item.Id == 3) ?
         (<TextField value={item.Text} fullWidth variant="standard"
-          onChange={(e) => { onTextChange({ ...item, Text: e.target.value }) }}/>) :
+          onChange={(e) => { onTextChange({ ...item, Text: e.target.value }) }} />) :
         (<TextField value={item.Text} fullWidth variant="standard" />)}
-       <Box sx={{ textAlign: "center",  mt: "10px" }}>
-        <img width="112" height="151" style={{ border: "1px solid gray" }}
-          src={(item.Value === "" || item.Value === null) ? 
-          '/imges/defualtUser.jpg' : 'data:image/png;base64,' + item.Value} />
-        </Box>
-      <Stack alignItems={"center"}>
-     <Box sx={{display:"flex" ,mt:"5px"}}>
-     <CloudUploadIcon onClick={handleClick} sx={{mt:"-2px"}} color={item.choosefileDisable? "disabled" :"primary"} />
-     <Typography sx={{mt:"2px" , fontWeight:"bold",ml:"2px",fontSize:"12px"}}>Upload files : </Typography>
-      <Box sx={{width:'100px',overflow: "hidden",  textOverflow: "ellipsis"}}>
-      {(item.selectedFile ==="" || item.selectedFile === undefined)? 
-         " No file selected": item.selectedFile}
+      <Box sx={{ textAlign: "center", mt: "10px" }}>
+        <img id="imgParent" width="112" height="151" style={{ border: "1px solid gray" }}
+          src={(item.Value === "" || item.Value === null) ?
+            '/imges/defualtUser.jpg' : 'data:image/png;base64,' + item.Value} />
       </Box>
-      </Box></Stack> 
-        <input ref={aRef} type="file" onChange={changeFile} 
-        disabled={item.choosefileDisable} style={{ display: 'none' }}/>
-         {error && <ErrorMessages Error={error} />}
-        </div>
+      <Stack alignItems={"center"}>
+        <Box sx={{ display: "flex", mt: "5px" }}>
+          <CloudUploadIcon onClick={handleClick} sx={{ mt: "-2px" }} color={item.choosefileDisable ? "disabled" : "primary"} />
+          <Typography sx={{ mt: "2px", fontWeight: "bold", ml: "2px", fontSize: "12px" }}>Upload files : </Typography>
+          <Box sx={{ width: '100px', overflow: "hidden", textOverflow: "ellipsis" }}>
+            {(item.selectedFile === "" || item.selectedFile === undefined) ?
+              " No file selected" : item.selectedFile}
+          </Box>
+        </Box></Stack>
+      <input ref={aRef} type="file" onChange={changeFile}
+        disabled={item.choosefileDisable} style={{ display: 'none' }} />
+      {error && <ErrorMessages Error={error} />}
+    </div>
   )
 }
 
