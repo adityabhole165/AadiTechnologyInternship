f7e737e54863ccf87e47ac3e8cb8f8683aed1bcb
renukaadwe <renukaadwe25@gmail.com>
2023-03-21 09:27:14 +0530

    BookWithme

--- src/libraries/mainCard/Card13.tsx
+++ src/libraries/mainCard/Card13.tsx
@@ -10,13 +10,14 @@ function Card13({Text1, Text2,Text3,Text4,Text5}) {
     style={{ transformOrigin: '0 0 0' }}
     {...(checked ? { timeout: 1000 } : {})}
     >
-    <Card  component={Box} p={1}>
+    <Card  component={Box} p={1} m={1}>
     <Typography variant='body2'> <b>Book Title :</b> {Text1}</Typography>
     <Typography variant='body2'> <b>Issue Date :</b> {Text3}</Typography>
     <Typography variant='body2'> <b>Return Date :</b> {Text2}</Typography>
     <Typography variant='body2'> <b>Accession No. :</b> {Text5}</Typography>
     <Typography variant='body2'><b>Issued To Parent :</b> {Text4 === true ? "Yes" : "No"}</Typography>
     </Card>
+    
     </Grow>
   )
 }
