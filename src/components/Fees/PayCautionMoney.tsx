1db38d1a02c4ab4f51b669e86f39eaab1a874b45
Snehavkale <snehavkale04@gmail.com>
2023-03-27 16:41:11 +0530

    onlinereport

--- src/components/Fees/PayCautionMoney.tsx
+++ src/components/Fees/PayCautionMoney.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Note from 'src/libraries/Note/Note';
 import { ButtonPrimary } from 'src/libraries/styled/ButtonStyle';
-function PayCautionMoney({ShowCaution,note,IspaidCautionMoney}) {
+function PayCautionMoney({ ShowCaution, note, IspaidCautionMoney }) {
   return (
     <div>
-      { ShowCaution=== "School" && 
-    <>
-   {!IspaidCautionMoney ?
-    
-     <Note NoteDetail={note} />  :
-     <ButtonPrimary fullWidth>Pay caution Money</ButtonPrimary> 
-   }
-   </>
-     }
+      {ShowCaution === "School" &&
+        <>
+          {!IspaidCautionMoney ?
+
+            <Note NoteDetail={note} /> :
+            <ButtonPrimary fullWidth>Pay caution Money</ButtonPrimary>
+          }
+        </>
+      }
     </div>
   )
 }
--- src/libraries/Page/DropdownandList.tsx
+++ src/libraries/Page/DropdownandList.tsx
@@ -3,14 +3,20 @@ import React from 'react'
 import PageHeader from 'src/libraries/heading/PageHeader';
 import DropdownNew from '../dropdown/DropdownNew';
 import CardItemList from './CardItemList';
-const DropdownandList = ({ heading, Itemlist, onChange, Label, DefaultValue, CardItemlist }) => {
-
+import ErrorMessages from 'src/libraries/ErrorMessages/ErrorMessages';
+const DropdownandList = ({ heading, Itemlist, onChange, Label, DefaultValue, CardItemlist,getExamDetailslist }) => {
+console.log("DefaultValue",DefaultValue)
   return (
     <div>
       <PageHeader heading={heading} />
       <DropdownNew Itemlist={Itemlist} onChange={onChange} Label={Label} DefaultValue={DefaultValue} />
       <Box sx={{mt:"12px"}}>
-      <CardItemList CardItemList={CardItemlist} />
+        {
+              getExamDetailslist.length === 0 ? 
+             
+             <ErrorMessages Error={"Exam is not available"} />
+             : <CardItemList CardItemList={CardItemlist} />
+      }
       </Box>
       
     </div>
