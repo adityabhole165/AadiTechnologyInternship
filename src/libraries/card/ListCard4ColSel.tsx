6d3cc4ba8c2fac4c9a20d642afd3ca5b43123745
Ajit Sagale <ajitsagale@gmail.com>
2023-04-15 11:43:36 +0530

    responsive card4

--- src/libraries/card/ListCard4ColSel.tsx
+++ src/libraries/card/ListCard4ColSel.tsx
@@ -4,6 +4,7 @@ import { ListStyle } from '../styled/CardStyle';
 import { useNavigate } from 'react-router-dom';
 import Card4 from 'src/libraries/mainCard/Card4';
 import { useState } from 'react';
+import CardMessage from '../mainCard/CardMessage';
 
 const ListCard4ColSel = ({ Item, onChange,ActiveTab }) => { 
   const navigate = useNavigate();
@@ -26,19 +27,19 @@ const ListCard4ColSel = ({ Item, onChange,ActiveTab }) => {
               checked={Item.isActive} onChange={onChange}
             />
           </Grid>
-          <Grid item xs={11} >
-            <Card4
-              header={Item.text1} text1={Item.text2} text4=""
-              text2={Item.text3} text3={''}
-              text5={''} text6={''}
+          <Grid item xs={11}>
+            <CardMessage
+              header={Item.text1} 
+              text1={Item.text2} 
+              text2={Item.text3} 
               ActiveTab={ActiveTab}
               IsRead={Item.IsRead}
               IsSchedule={Item.IsSchedule}
               HasReadReceipt={Item.HasReadReceipt}
               RequestReadReceipt={Item.RequestReadReceipt}
               IsAttachmentExist={Item.IsAttachmentExist}
-              clickCard={() => { clickCard(Item.NavPath) }}
               DetailsId={Item.DetailsId}
+              NavPath={Item.NavPath}
             />
           </Grid>
         </Grid>
---
+++ src/libraries/mainCard/CardMessage.tsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { useLocation, Link as RouterLink } from 'react-router-dom';
+import { Dialog, Grid, Typography } from '@mui/material';
+import AttachmentIcon from '@mui/icons-material/Attachment';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import EmailIcon from '@mui/icons-material/Email';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import { useNavigate } from 'react-router-dom';
+import Card15 from './Card15';
+import { RootState } from 'src/store';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { ReadReceiptDetail } from 'src/requests/MessageCenter/MessaageCenter';
+
+function CardMessage({ header, text1, text2, DetailsId,
+    ActiveTab, IsRead, IsSchedule, IsAttachmentExist, HasReadReceipt,
+    RequestReadReceipt, NavPath }) {
+    
+        const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const SchoolId = localStorage.getItem('localSchoolId');
+    const AcademicYearId = sessionStorage.getItem('AcademicYearId');
+    const [popup, setPopup] = useState(false);
+    let clickParent = true;
+
+    const ReadReceipt = useSelector(
+        (state: RootState) => state.MessageCenter.ReadReceiptDetails
+    );
+
+    const ReadReceipts = {
+        aiSchoolId: SchoolId,
+        aiAcademicYearId: AcademicYearId,
+        aiMessageDetailId: DetailsId
+    }
+    const handleClickToOpen = (e) => {
+        e.stopPropagation();
+        setPopup(true);
+        dispatch(ReadReceiptDetail(ReadReceipts))
+        clickParent = false
+    };
+
+    const clickNav = () => {
+        if (clickParent) {
+            navigate('/' + location.pathname.split('/')[1] + '/MessageCenter/viewMSg/' + NavPath);
+        }
+    }
+
+    let IsReadColor = (ActiveTab == "Inbox" && IsRead == 'N') ?
+        'blue' :
+        (ActiveTab == "Sent" && IsSchedule) ? 'blue' :
+            ''
+    return (
+        <>
+
+            <Grid container alignItems={"center"} onClick={clickNav}>
+                <Grid item xs={11} sm={4} md={4} >
+                    <Typography variant='h6' sx={{ color: IsReadColor, overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap", width: "150px" }}>{header}</Typography>
+                </Grid>
+                {/* This attachment is used for Mobile view */}
+                <Grid item display={{ xs: "block", sm: "none" }} xs={1} >
+                    {IsAttachmentExist &&
+                        <AttachmentIcon fontSize='small' />}
+                </Grid>
+
+                <Grid item xs={6} sm={2} md={4.5}>
+                    <Typography variant="body1" sx={{ overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>{text1}</Typography>
+                </Grid>
+                {/* This attachment is used for web view */}
+                <Grid item display={{ xs: "none", sm: "block" }} sm={1.5} md={0.5} >
+                    {IsAttachmentExist &&
+                        <AttachmentIcon fontSize='small' sx={{ ml: "20px" }} />}
+                </Grid>
+
+                <Grid item xs={6} sm={4.5} md={3}>
+                    <Typography variant="body1" sx={{ float: "right" }}>
+                        <>    {text2}  {
+                            IsSchedule &&
+                            <ScheduleIcon fontSize="small" color="primary" sx={{ mb: "-5px" }} />
+                        }</>
+                        <>{RequestReadReceipt === "True" &&
+                            <>{HasReadReceipt ? <>
+                                <DraftsIcon fontSize="small" color="success" sx={{ mt: "-2px", ml: "4px" }} onClick={(e) => { handleClickToOpen(e) }} />
+                                <Dialog open={popup}
+                                    onClose={() => { setPopup(false) }}
+                                >
+                                    <Card15 text1={text2} text2={text1} />
+                                </Dialog></> :
+                                <EmailIcon fontSize="small" color="error" sx={{ mt: "-2px", ml: "4px" }} />
+                            }</>
+                        }</>
+                    </Typography>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default CardMessage;
