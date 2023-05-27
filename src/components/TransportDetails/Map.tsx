a13616ebd35c6ca45ef73870a7bcf020287b06e0
Ajit Sagale <ajitsagale@gmail.com>
2023-05-27 16:53:51 +0530

    refresh

--- src/components/TransportDetails/Map.tsx
+++ src/components/TransportDetails/Map.tsx
@@ -15,55 +15,59 @@ import { useParams } from 'react-router-dom';
 import TransportDetails from './TransportDetails';
 import { getTransportDetails } from 'src/requests/TransportDetails/RequestTransportDetails';
 import { GetStudentTransportDetailsBody } from 'src/interfaces/Student/ITransportDetails';
+import SuspenseLoader from 'src/layouts/components/SuspenseLoader';
 function Map() {
     const dispatch = useDispatch();
-    const location = useLocation();  
+    const location = useLocation();
     const { PickDrop } = useParams();
 
     // const variable = location.state?.variable;
     const StopDetails: any = useSelector(
         (state: RootState) => state.TransportDetails.StopDetails
-      );
-      const OtherTrackingDetails: any = useSelector(
+    );
+    const OtherTrackingDetails: any = useSelector(
         (state: RootState) => state.TransportDetails.OtherTrackingDetails
-      );
+    );
+    const loading = useSelector(
+        (state: RootState) => state.TransportDetails.Loading
+    );
     const [showMyStop, setShowMyStop] = useState(true)
     const [isRefresh, setIsRefresh] = React.useState(false);
     const [alignment, setAlignment] = React.useState('1');
-   
+
     const refresh = () => window.location.reload()
     const handleChange = (
-      event: React.MouseEvent<HTMLElement>,
-      newAlignment: string,
+        event: React.MouseEvent<HTMLElement>,
+        newAlignment: string,
     ) => {
-      if (newAlignment != null)
-        setAlignment(newAlignment);
+        if (newAlignment != null)
+            setAlignment(newAlignment);
     };
     useEffect(() => {
         const TransportBody: GetStudentTransportDetailsBody = {
-          aiUserId: parseInt(sessionStorage.getItem('Id')),
-          aiSchoolId: parseInt(localStorage.getItem('localSchoolId')),
-          aiAcademicYearId: parseInt(sessionStorage.getItem('AcademicYearId')),
-          aiTypeId: parseInt(alignment)
+            aiUserId: parseInt(sessionStorage.getItem('Id')),
+            aiSchoolId: parseInt(localStorage.getItem('localSchoolId')),
+            aiAcademicYearId: parseInt(sessionStorage.getItem('AcademicYearId')),
+            aiTypeId: parseInt(alignment)
         }
         dispatch(getTransportDetails(TransportBody));
-         }, [alignment, isRefresh]);
-  
-  
+    }, [alignment, isRefresh]);
 
-  
+
+
+
 
     return (
         <Container>
 
-              
 
-            <BackButton  FromRoute={"/Student/TransportDetails/"+PickDrop} />
-          
-           
+
+            <BackButton FromRoute={"/Student/TransportDetails/" + PickDrop} />
+
+
             <PageHeader heading={PickDrop} subheading={''} />
-            
-            <Avatar onClick={() => { setIsRefresh(!isRefresh) }} sx={{ height: 40, width: 40, color: "black", float:"right" , mt:"-50px" }}><RefreshIcon fontSize='small' /></Avatar>
+
+            <Avatar onClick={() => { setIsRefresh(!isRefresh) }} sx={{ height: 40, width: 40, color: "black", float: "right", mt: "-50px" }}><RefreshIcon fontSize='small' /></Avatar>
             {OtherTrackingDetails.ShowStops ?
                 <>
                     {StopDetails?.map((item, i) => {
@@ -90,11 +94,13 @@ function Map() {
             {OtherTrackingDetails.TrackingURL !== "" ?
                 <><Grid container>
                     <Grid item xs={11}>
-                        
+
                     </Grid><Grid item xs={1}>
-                      
+
                     </Grid></Grid>
-                    {OtherTrackingDetails.TrackingMessage == "" ?
+
+                    {loading ? <SuspenseLoader /> :
+                    OtherTrackingDetails.TrackingMessage == "" ?
                         <>
                             <iframe allowFullScreen style={{ border: "none" }} width="100%" height="580px" title="Vehicle Tracking"
                                 src={OtherTrackingDetails.TrackingURL}
@@ -106,10 +112,10 @@ function Map() {
                 </> : null
             }
 
-       
- 
-    </Container >
-  )
+
+
+        </Container >
+    )
 }
 
 export default Map
