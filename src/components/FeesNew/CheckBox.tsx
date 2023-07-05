5212e9d45d1544e420fe600bbb0aa1d22ce58312
SejalPatill23 <sejalpatil2315@gmail.com>
2023-07-05 12:48:00 +0530

    disable checkbox if enableonlinefee is false

--- src/components/FeesNew/CheckBox.tsx
+++ src/components/FeesNew/CheckBox.tsx
@@ -2,22 +2,28 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { Styles } from 'src/assets/style/student-style'
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
-const CheckBox = ({ item, onChange }) => {
+const CheckBox = ({ item, onChange, IsNotDisabled, InternalOrSchool }) => {
     const classes = Styles();
     const onClick = () => {
         onChange({ ...item, IsActive: !item.IsActive })
     }
-
+  
     return (<>
         {item.ParentId !== "0" ? null :
-            (item.IsEnabled)?
-                item.IsActive ?
-                    <CheckCircleIcon sx={{ color: 'green' }}
-                        onClick={onClick} className={classes.checkboxSize} /> :
-                    <RadioButtonUncheckedIcon sx={{ color: 'green' }}
-                        onClick={onClick} className={classes.checkboxSize} /> :
-                <RadioButtonUncheckedIcon sx={{ color: 'grey' }}
-                    className={classes.checkboxSize} />
+            <>
+                {((IsNotDisabled == true && InternalOrSchool == "SchoolFees")|| InternalOrSchool == "internalFees" )?
+                    (item.IsEnabled) ?
+                        item.IsActive ?
+                            <CheckCircleIcon sx={{ color: 'green' }}
+                                onClick={onClick} className={classes.checkboxSize} /> :
+                            <RadioButtonUncheckedIcon sx={{ color: 'green' }}
+                                onClick={onClick} className={classes.checkboxSize} /> :
+                        <RadioButtonUncheckedIcon sx={{ color: 'grey' }}
+                            className={classes.checkboxSize} /> :
+                    <RadioButtonUncheckedIcon sx={{ color: 'grey' }}
+                        className={classes.checkboxSize} />
+                }
+            </>
         }
     </>)
 }
