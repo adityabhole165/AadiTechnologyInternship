4bc4b51068508b2a3bea7ab6386a5cc49ee1ab68
renukaadwe <renukaadwe25@gmail.com>
2023-07-22 11:18:44 +0530

    DailyLog

--- src/components/Homework/DailyLogs.tsx
+++ src/components/Homework/DailyLogs.tsx
@@ -1,4 +1,4 @@
-import { Card, Container, Typography ,Link} from '@mui/material'
+import { Card, Container, Typography ,Link, Box} from '@mui/material'
 import React from 'react'
 import BackButton from 'src/libraries/button/BackButton'
 import PageHeader from 'src/libraries/heading/PageHeader'
@@ -6,51 +6,55 @@ import { RootState } from 'src/store';
 import { useEffect, useState } from 'react';
 import { getDateMonthYearFormatted, getHomeworkDateFormatted, getNextDate } from '../Common/Util';
 import { useDispatch, useSelector } from 'react-redux';
-import { getHomeworkDates } from 'src/requests/Homework/RequestHomeworkNew';
+import { getHomeworkDailyLogs } from 'src/requests/Homework/RequestHomeworkNew';
 import ListH from 'src/libraries/mainCard/ListH';
+import { useParams } from 'react-router-dom';
 
 function DailyLogs() {
   const dispatch = useDispatch();
+  const { Month, Year } = useParams();
   const [startdate, setStartDate] = useState('')
   const [endDate, setEndDate] = useState('')
   const asSchoolId = localStorage.getItem('localSchoolId');
   const asAcademicYearId = sessionStorage.getItem('AcademicYearId');
   const asStandardDivision = (sessionStorage.getItem('StandardDivisionId'));
-  const GetHomeworkDates = useSelector((state: RootState) => state.HomeworkNew.HomeworkDailyLogs);
+  const GetHomeworkDailyLogs = useSelector((state: RootState) => state.HomeworkNew.HomeworkDailyLogs);
   const loading = useSelector((state: RootState) => state.HomeworkNew.Loading);
-
+  const [month, setMonth] = useState(0);
+  const [year, setYear] = useState(0);
   useEffect(() => {
-    const HomeworkBody =
-    {
-      aiSchoolId: asSchoolId,
-      aiAcademicYearId: asAcademicYearId,
-      aiStandardDivisionId: asStandardDivision,
-      asStartdate: '',
-      asEnddate: ''
+    if (Month !== undefined) {
+      setMonth(Number(Month))
+      setYear(Number(Year))
+    } else {
+      setMonth(new Date().getMonth())
+      setYear(new Date().getFullYear())
     }
-    dispatch(getHomeworkDates(HomeworkBody))
+    const HomeworkDailyBody =
+    { aiSchoolId:asSchoolId,
+      aiAcademicYearId:asAcademicYearId,
+      aiStandardDivisionId:asStandardDivision,
+      aiMonthId:"7",
+      asYear:"2023"
+    }
+    dispatch(getHomeworkDailyLogs(HomeworkDailyBody))
   }, [])
 
 
-  
+  console.log(GetHomeworkDailyLogs,"item.Header")
   return (
     <div>
       <Container>
         <BackButton FromRoute='/Student/Homework'></BackButton>
         <PageHeader heading={'Daily Logs'} subheading={''} />
-        {/* <ListH itemList={GetHomeworkDates}/> */}
-        {GetHomeworkDates.map((item, i) => (
+         {GetHomeworkDailyLogs.map((item, i) => (
           <div key={i}>
-            <Card sx={{mt:"5px" ,padding:"5px" ,display:"flex" , justifyContent:"space-between"}}>
+            <Card sx={{display:"flex" , justifyContent:"space-between"}} component={Box} mt={1} p={1}>
               <Typography>{item.Header}</Typography>
-        <a href={localStorage.getItem('SiteURL') + item.Text1} rel="noreferrer" target="_blank" style={{ textDecoration: 'none' }}>
-        <Typography >Click here</Typography>
-        </a>
-               
-            
-            </Card>
-
-
+          <a href={localStorage.getItem('SiteURL') + item.Text1} rel="noreferrer" target="_blank" style={{ textDecoration: 'none' }}>
+             <Typography >Click here</Typography>
+          </a>
+          </Card>
           </div>
         ))}
 
