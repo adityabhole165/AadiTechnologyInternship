6764978e3baa28132b2c338f9f72f9726e899390
Ajit Sagale <ajitsagale@gmail.com>
2023-11-10 14:34:42 +0530

    add Calendar Days

--- src/libraries/DateSelector/CalendarDetails.tsx
+++ src/libraries/DateSelector/CalendarDetails.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import MonthSelector from './MonthSelector'
+import CalenderDays from './CalenderDays'
 
-const CalendarDetails = ({ItemList, DefaultDate, ClickDate}) => {
-  if(ItemList.length>0)
-  console.log(ItemList[0].DefaultDate,"CalendarDetails - ItemList")
+const CalendarDetails = ({ ItemList, DefaultDate, ClickDate }) => {
+  if (ItemList.length > 0)
+    console.log(ItemList[0].DefaultDate, "CalendarDetails - ItemList")
   return (
     <div>
-                <MonthSelector
-        DefaultDate={DefaultDate}
-        ClickDate={ClickDate}/>
+      <MonthSelector DefaultDate={DefaultDate} ClickDate={ClickDate} />
+      <CalenderDays ItemList={ItemList} DefaultDate={DefaultDate} ClickDate={ClickDate}/>
     </div>
   )
 }
