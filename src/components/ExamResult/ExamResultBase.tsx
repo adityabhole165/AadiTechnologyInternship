571037439a9742269afe9cf34f83246ca89fb2a7
Ajit Sagale <ajitsagale@gmail.com>
2023-12-26 12:29:50 +0530

    examresult formating

--- src/components/ExamResult/ExamResultBase.tsx
+++ src/components/ExamResult/ExamResultBase.tsx
@@ -10,6 +10,7 @@ import { Box, Button, Card, Container, Grid, Typography } from '@mui/material';
 import Dropdown from 'src/libraries/dropdown/Dropdown';
 import { useNavigate } from 'react-router';
 import { ButtonPrimary } from 'src/libraries/styled/ButtonStyle';
+import Note from 'src/libraries/Note/Note';
 
 const ExamResultBase = () => {
   const dispatch = useDispatch();
@@ -18,14 +19,14 @@ const ExamResultBase = () => {
   const asAcademicYearId = sessionStorage.getItem("AcademicYearId")
   const [StandardDivisionId, setStandardDivisionId] = useState("0")
   const [TestId, setTestId] = useState("0")
-  
+
   const [IconList, setIconList] = useState([])
   const ClassTeachers: any = useSelector((state: RootState) => state.ExamResult.ClassTeachers);
   const IsSubmitted: any = useSelector((state: RootState) => state.ExamResult.IsSubmitted);
   const HeaderList: any = useSelector((state: RootState) => state.ExamResult.HeaderList);
   const ClassPassFailDetailsForTest: any = useSelector((state: RootState) => state.ExamResult.ClassPassFailDetailsForTest);
   const AllTestsForClass: any = useSelector((state: RootState) => state.ExamResult.AllTestsForClass);
-  
+
 
   const ClassTeachersBody: IGetClassTeachersBody = {
     asSchoolId: asSchoolId,
@@ -95,37 +96,27 @@ const ExamResultBase = () => {
   return (
     <Container>
       <PageHeader heading={'Exam Results'} subheading={''} />
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>  
-      {IsSubmitted === 'Y' ? (
-      <Card sx={{ backgroundColor: "#f8bbd0", height: '40px', width: '90vw', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid #000' }}>
-        
-      <h4 style={{ margin: 0 , color: '#9c27b0'}}>Results for this exam have been published.</h4>
-    </Card>
-    ) : (
-     
+      {IsSubmitted === 'Y' ?
+        (<Note NoteDetail={["Results for this exam have been published."]} />)
+        : (
+          <Card sx={{
+            backgroundColor: "#f8bbd0", height: '35px',
+            display: 'flex', alignItems: 'center', justifyContent: 'center',
+            border: '1px solid #000'
+          }}>
 
-<Card sx={{ backgroundColor: "#f8bbd0", height: '35px', width: '90vw', display: 'flex', alignItems: 'center', justifyContent: 'center' ,border: '1px solid #000' }}>
-        
-<h4 style={{ margin: 0 , color: '#9c27b0'}}>Not all results for this exam have been submitted.</h4>
+            <h4 style={{ margin: 0, color: '#9c27b0' }}>Not all results for this exam have been submitted.</h4>
 
-</Card>
-    )}
-      </Box>
-
+          </Card>
+        )}
       <br></br>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
-        {/* <Grid item xs={2}>
-          <Typography component={Box} sx={{ border: "1px solid black" }} p={0.5}>
-            Select Class Teacher:
-          </Typography>
-        </Grid> */}
- 
 
         <Grid item xs={4}>
-          
+
           <Dropdown Array={ClassTeachers} handleChange={clickTeacher}
             label={"Teacher"} defaultValue={StandardDivisionId} />
-          <br></br>  
+          <br></br>
 
         </Grid>
 
@@ -141,32 +132,35 @@ const ExamResultBase = () => {
 
         </Grid>
         <ButtonPrimary >Topper</ButtonPrimary>
-  
+
       </Grid>
       <br></br>
       <Box mb={1}>
-      <Card sx={{ backgroundColor: "#4dd0e1", height: '40px', width: '55vw', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid white' }}>
-        
-        <h4 style={{ margin: 0 , color: 'balck'}}>Subject</h4>
-      </Card>
+        <Card sx={{
+          backgroundColor: "#4dd0e1", height: '40px',
+          display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid white'
+        }}>
+
+          <h4 style={{ margin: 0, color: 'balck' }}>Subject</h4>
+        </Card>
       </Box>
       <Box mb={1}>
-      <DynamicList HeaderList={HeaderList} ItemList={ClassPassFailDetailsForTest}
-        IconList={IconList} ClickItem={ClickItem} />
-         </Box>
-         <Box sx={{ display: 'flex', justifyContent: 'flex-start', gap: '8px' }}>
-  <Button variant="contained" color="primary">VIEW PROGRESS REPORT</Button>
-  <Button variant="contained" color="primary">GENERATE TOPPERS</Button>
-  <Button variant="contained" color="primary">PUBLISH</Button>
-  <Button variant="contained" color="primary">UNPUBLISH</Button>
-  <Button variant="contained" color="primary">Progress Remarks</Button>
-  <Button variant="contained" color="primary"  >Transfer Optional Subject Marks</Button>
-  <Button variant="contained" color="primary" onClick={TermwiseHighwight}>Termwise Height-Weight</Button>
-</Box>
-        
-         
-  
-         
+        <DynamicList HeaderList={HeaderList} ItemList={ClassPassFailDetailsForTest}
+          IconList={IconList} ClickItem={ClickItem} />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-start', gap: '8px' }}>
+        <Button variant="contained" color="primary">VIEW PROGRESS REPORT</Button>
+        <Button variant="contained" color="primary">GENERATE TOPPERS</Button>
+        <Button variant="contained" color="primary">PUBLISH</Button>
+        <Button variant="contained" color="primary">UNPUBLISH</Button>
+        <Button variant="contained" color="primary">Progress Remarks</Button>
+        <Button variant="contained" color="primary"  >Transfer Optional Subject Marks</Button>
+        <Button variant="contained" color="primary" onClick={TermwiseHighwight}>Termwise Height-Weight</Button>
+      </Box>
+
+
+
+
     </Container>
   )
 }
