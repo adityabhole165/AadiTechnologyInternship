dcf45c6062084ee143277ad22486e2af03fb44c6
Pramod <Pramodmane043@gmail.com>
2024-01-02 14:28:34 +0530

    buildurl

--- src/requests/SchoolService/schoolServices.ts
+++ src/requests/SchoolService/schoolServices.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.45:81/",
+  baseURL: "http://192.168.1.53:81/",
   // baseURL: "http://localhost:44307/",
   // baseURL: "http://schoolappapi.aaditechnology.com/",
  // baseURL: "https://mobileappapi.regulusit.net/",
