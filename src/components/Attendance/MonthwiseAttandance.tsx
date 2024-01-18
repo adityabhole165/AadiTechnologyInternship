ce6ca3ebdb1dfd804dcde4a5c4a15b6d9c09fa3f
Pramod <Pramodmane043@gmail.com>
2024-01-18 18:57:58 +0530

    MonthwiseAttandance

--- src/components/Attendance/MonthwiseAttandance.tsx
+++ src/components/Attendance/MonthwiseAttandance.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { Box, Container,Grid} from '@mui/material';
+import { Box, Container,Divider,Grid, IconButton, InputBase, ListItemSecondaryAction, Paper, } from '@mui/material';
 import PageHeader from 'src/libraries/heading/PageHeader';
 import { TextField } from '@mui/material';
 import { ButtonPrimary } from 'src/libraries/styled/ButtonStyle';
@@ -10,9 +10,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, } from 'react';
 import { getattendance } from 'src/requests/Attendance/requestGetMonthWiseAttendance'
 import { IGetMonthwiseAttendanceBody } from 'src/interfaces/MonthwiseAttendance/IMonthwiseAttendance';
+import SearchIcon from '@mui/icons-material/Search';
 import TableAttendace from 'src/libraries/ResuableComponents/TableAttendance';
 import WebBackButton from 'src/libraries/button/WebBackButton';
 import Iconhelp from 'src/libraries/icon/Iconhelp';
+import DirectionsIcon from '@mui/icons-material/Directions';
 
 const MonthwiseAttandance = () => {
     const dispatch = useDispatch();
@@ -21,7 +23,7 @@ const MonthwiseAttandance = () => {
     const asAcademicYearId = Number(sessionStorage.getItem('AcademicYearId'));
     const StandardDivisionId = Number(sessionStorage.getItem('StandardDivisionId'));
     const StudentId = Number(sessionStorage.getItem('StudentId'))
-
+    const [search, setSearch] = useState(false)
     const HeaderArray = [{ Id: 1, Header: "Roll No." }, { Id: 2, Header: "Student Name" }, { Id: 3, Header: "Mar" }, { Id: 4, Header: "Apr" }, { Id: 5, Header: "May" }, { Id: 6, Header: "Jun" }
         , { Id: 7, Header: "Jul" }, { Id: 8, Header: "Aug" }, { Id: 9, Header: "Sep" }, { Id: 10, Header: "Oct" }, { Id: 10, Header: "Nov" }, { Id: 10, Header: "Dec" }, { Id: 11, Header: "Jan" }, { Id: 12, Header: "Feb" }, { Id: 13, Header: "Present Days" }, { Id: 14, Header: "Total Days" }, { Id: 15, Header: "%" }]
     const [MonthWiseAttendanceList, setMonthWiseAttendanceList] = useState([])
@@ -63,44 +65,149 @@ const MonthwiseAttandance = () => {
     };
     return (
         <>
-          <Container maxWidth={'xl'}>
-            <PageHeader heading={'Monthwise Attendance'} subheading={''} />
-              <WebBackButton FromRoute={'/Teacher/TAttendance/'} />
-             <Box sx={{ float: "right" }}>
+          <div style={{marginLeft:'70px', marginRight:'50px', marginTop:'15px'}}>
+            {/* <Container sx={{marginTop:'20px'}} > */}
+             
+           <Grid sx={{margintop:'20vh'}} container >
+           <Grid item margin={0} padding={0} xs={3} >
+            <PageHeader heading={'Month Wise Attendance'}  subheading={''} />
+              {/* <Box  sx={{float :'right'}} >
               
-                <Iconhelp  Note={Note}/>
-             </Box>
-           <Grid container>
-           <Grid item xs={2} sm={4}/>
-            <Grid item xs={8}  sm={4}>
-            <TextField label={'Search by Name'} name="SearchText" type="text" variant="standard"
+            <Iconhelp  Note={Note}/> */}
+           {/* </Box> */}
+
+           </Grid>
+           
+            <Grid sx={{mt:5, display:'flex', justifyContent:'flex-end', position:'relative' }} item xs={6}  >
+            
+
+                  
+            {/* <Paper
+            component="form"
+              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', position:'relative' }}
+               > */}
+       
+          {search ? 
+          <TextField label={'Search by Name'}
+          name="SearchText" 
+          type="text"
+          size='small'
+           variant="outlined"
+         id="outlined-search"
+          sx={(theme)=>({flex:1 })}
+
+             value={SearchText} onChange={(e) => { changeSearchText(e.target.value) }} fullWidth
+
+         />
+        //  <InputBase
+        //    sx={{ ml: 1, flex: 1,  }}
+        // placeholder="Search Google Maps"
+        //    inputProps={{ 'aria-label': 'search google maps' }}
+        //    />
+            :''}
+          
+
+      <IconButton  type="button" sx={{ position:'absolute', right:'8px' }} aria-label="search">
+        {/* <SearchIcon /> */}
+      </IconButton>
+      
+      {/* <Grid sx={{position:'relative'}} xs={1}> */}
+
+      {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+      {/* <IconButton  sx={{ p: '10px' }} aria-label="directions"> */}
+      {/* <Iconhelp   Note={Note}/> */}
+      {/* </IconButton> */}
+      {/* </Grid> */}
+   </Grid>
+   
+    {/* </Paper> */}
+            {/* <TextField label={'Search by Name'}
+             name="SearchText" 
+             type="text"
+             size='small'
+              variant="outlined"
+            id="outlined-search"
+             sx={(theme)=>({mt:5})}
+
                 value={SearchText} onChange={(e) => { changeSearchText(e.target.value) }} fullWidth
 
-            />
+            /> */}
+            
+            <Grid item xs={1}/>
+            <Grid item sx={{position:'relative'}} xs={1}>
+            <IconButton onClick={()=>setSearch(!search)} type="button" sx={{ position:'absolute', right:'8px', bottom:'12px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+            </Grid>
+            {/* <Grid sx={(theme)=>({
+                display:'flex',
+                mt: 2,
+                bgcolor:'primary.600'
+             
+
+            })} item xs={1}>
+                
+            
+             
+            <Box sx={{ }} >
+              
+              <Iconhelp  Note={Note}/>
+           </Box>
+            </Grid> */}
+            <Grid item sx={{marginTop:'45px'}} xs={1}>
+                <Grid container>
+                    <Grid item xs={3}>
+                     <Iconhelp  Note={Note}/>
+
+                    </Grid>
+                    {/* <Grid item xs={5}>
+                      <WebBackButton FromRoute={'/Teacher/TAttendance/'} />
+                    </Grid> */}
+                    <Grid item xs={9}>
+                      <Box sx={{ textAlign:"right", marginTop:''}} >
+                <ButtonPrimary
+                    style={{backgroundColor: '#ef5350', marginBottom:'20px'}}
+                    onClick={click}
+                    className='bold'
+                >
+                   Close
+                </ButtonPrimary>
+                      </Box>
+
+                    </Grid>
+                </Grid>
+                
             </Grid>
            </Grid>
-         
+           {/* <Grid container>
+            <Grid item xs={4} sm={4}>bbbbbbbbbbbb</Grid>
+            <Grid item xs={4} sm={4}>bbbbbbbbbbbb</Grid>
+            <Grid item xs={4} sm={4}>bbbbbbbbbbbb</Grid>
+           </Grid> */}
+           {/* </Container> */}
          
       
-            <br></br><br></br>
+          
+           
 
+            <TableAttendace ItemList={MonthWiseAttendance} HeaderArray={HeaderArray} />
 
+                </div>
 
-            <TableAttendace ItemList={MonthWiseAttendanceList} HeaderArray={HeaderArray} />
-
-
-            <Box sx={{ textAlign: "center" }} m={2}>
+            <Box sx={{ textAlign: "center", gap:'10px' }}  m={2}>
+                {/* <ButtonPrimary sx={{paddingX:'20px', marginRight:'10px'}}
+                >Back</ButtonPrimary> */}
                 <ButtonPrimary
                     style={{backgroundColor: '#ef5350'}}
                     onClick={click}
+                    className='bold'
                 >
                    Close
                 </ButtonPrimary>
             </Box>
 
 
-
-        </Container>
+        
         </>
     )
 }
---
+++ src/libraries/ResuableComponents/Model.tsx
@@ -0,0 +1,52 @@
+import { Box, Modal, Typography } from '@mui/material'
+import { set } from 'nprogress';
+import React, { useState } from 'react'
+import BarChart from '../Charts/BarChart';
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+ interface Mode {
+    id: String
+    name:String
+ }
+const Model = ({data}) => {
+    const [open, setOpen] = useState(false)
+   
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
+   
+  return (
+  
+  <Box sx={style}>{data ? <>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      {/* {user.Text1} */}
+      {data.Text1}
+    </Typography>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      {/* {user.Text1} */}
+      {data.Text2}
+    </Typography>
+    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+    </Typography>
+    <Box>
+      {/* <BarChart Isgrade={true}/> */}
+    </Box>
+  </>
+  :''}
+  </Box>
+
+
+  )
+}
+
+export default Model
