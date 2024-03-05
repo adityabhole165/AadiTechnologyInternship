ec63afbcabf3d3677c21c543c4200c7236591538
Ajit Sagale <ajitsagale@gmail.com>
2024-03-05 14:17:49 +0530

    view annual Planner based on permissions

--- src/components/AnnualPlannerNew/AnnualPlannerHeader.tsx
+++ src/components/AnnualPlannerNew/AnnualPlannerHeader.tsx
@@ -2,15 +2,32 @@ import AddIcon from '@mui/icons-material/Add';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import Visibility from '@mui/icons-material/Visibility';
 import { Box, Stack } from "@mui/material";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
+import { getFilePath } from 'src/requests/AnnualPlanner/AnnualPlanner';
+import { RootState } from 'src/store';
 import BreadCrumbs from './BreadCrumbs';
 import HeaderIcons from './HeaderIcons';
 import UploadAnnualPlanner from './UploadAnnualPlanner';
 const AnnualPlannerHeader = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [openAnnualPlannerDialog, setOpenAnnualPlannerDialog] = useState(false);
+
+    const FileName: any = useSelector(
+        (state: RootState) => state.AnnualPlanner.FilePath
+    );
+
+    const ScreensAccessPermission = JSON.parse(
+        sessionStorage.getItem('ScreensAccessPermission')
+    );
+    let AnnualPlannerViewAccess = "N"
+    ScreensAccessPermission?.map((item) => {
+        if (item.ScreenName === 'Annual Planner') AnnualPlannerViewAccess = item.IsFullAccess;
+    });
     const Note: string =
         'These events may change due to unavoidable reasons without prior notice.';
     const Note1: string =
@@ -19,7 +36,10 @@ const AnnualPlannerHeader = () => {
         { Id: 1, Icon: <PriorityHighIcon />, Title: Note, Action: 'Note' },
         { Id: 2, Icon: <QuestionMarkIcon />, Title: Note1, Action: 'Note1' },
         { Id: 1, Icon: <CalendarMonthIcon />, Title: 'Events Overview', Action: 'EventsOverview' },
-        { Id: 4, Icon: <AddIcon />, Title: 'Add Annual Planner', Action: 'AddAnnualPlanner' },
+        {
+            Id: 4, Icon: AnnualPlannerViewAccess == "N" ? <AddIcon /> : <Visibility />,
+            Title: AnnualPlannerViewAccess == "N" ? 'Add' : '' + ' Annual Planner', Action: 'AddAnnualPlanner'
+        },
 
     ]
     const ClickIcon = (value) => {
@@ -27,10 +47,26 @@ const AnnualPlannerHeader = () => {
             navigate('/extended-sidebar/Common/EventOverview');
         }
         if (value == 'AddAnnualPlanner') {
-            setOpenAnnualPlannerDialog(true);
+            if (AnnualPlannerViewAccess == "Y") {
+                if (FileName !== '') {
+                    window.open(localStorage.getItem('SiteURL') + FileName);
+                }
+
+            }
+            else
+                setOpenAnnualPlannerDialog(true);
         }
 
     }
+
+    useEffect(() => {
+
+        const FilepathBody = {
+            aiSchoolId: localStorage.getItem('localSchoolId'),
+            aiAcademicYearId: sessionStorage.getItem('AcademicYearId')
+        };
+        dispatch(getFilePath(FilepathBody));
+    }, []);
     const Breadcrumbs = [{
         Id: 1,
         Name: 'Annual Planner',
