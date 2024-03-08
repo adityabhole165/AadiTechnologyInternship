104fab1e0aff73620edee697670bf902ebc8bd4e
Pramod <Pramodmane043@gmail.com>
2024-03-08 18:15:18 +0530

    EventManagementHeader

--- src/components/EventManagementNew/EventManagementHeader.tsx
+++ src/components/EventManagementNew/EventManagementHeader.tsx
@@ -1,5 +1,4 @@
 import AddIcon from '@mui/icons-material/Add';
-import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { Box, Stack } from "@mui/material";
 import { useState } from 'react';
@@ -11,11 +10,11 @@ import UploadAnnualPlanner from '../AnnualPlannerNew/UploadAnnualPlanner';
 const EventManagementHeader = ({ ClickAddNewEvent }) => {
     const navigate = useNavigate();
     const [openAnnualPlannerDialog, setOpenAnnualPlannerDialog] = useState(false);
+
     const Note: string =
-        'Add Events to be held in your school for the current academic year.';
+        '        Event management for your school. Add events to be held in your school for the current academic year.';
     const IconList = [
-        { Id: 1, Icon: <PriorityHighIcon />, Title: Note, Action: 'Note' },
-        { Id: 2, Icon: <QuestionMarkIcon />, Title: 'Help', Action: 'Help' },
+        { Id: 2, Icon: <QuestionMarkIcon />, Title: Note, Action: 'Help' },
         { Id: 4, Icon: <AddIcon />, Title: 'Add New Event', Action: 'AddNewEvent' },
     ]
     const ClickIcon = (value) => {
