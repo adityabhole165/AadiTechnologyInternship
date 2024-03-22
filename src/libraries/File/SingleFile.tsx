0b5dff509b811dbbd528eac090ab705ec41500c7
Ajit Sagale <ajitsagale@gmail.com>
2024-03-22 19:35:45 +0530

    new line for error message

--- src/libraries/File/SingleFile.tsx
+++ src/libraries/File/SingleFile.tsx
@@ -2,7 +2,7 @@ import CheckCircle from '@mui/icons-material/CheckCircle';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { default as DeleteIcon } from '@mui/icons-material/Delete';
 import VisibilityTwoToneIcon from '@mui/icons-material/VisibilityTwoTone';
-import { Box, Button, IconButton, Stack, Tooltip, Typography } from '@mui/material';
+import { Box, Button, Grid, IconButton, Stack, Tooltip, Typography } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import { Styles } from 'src/assets/style/student-style';
 import {
@@ -58,92 +58,97 @@ const SingleFile = ({
     }
   };
   return (
-    <>
-      <Tooltip
-        title={
-          'Supports only ' +
-          ValidFileTypes.join(', ') +
-          ' files types up to ' + (MaxfileSize / 1000000).toString() + ' MB'
-        }
-      >
-        <Button
-          sx={{
-            width: width,
-            border: (theme) =>
-              `1px dashed ${FileName ? theme.colors.success.main : theme.colors.primary.main
-              }`,
-            gap: 1,
-            position: 'relative',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}
-          color={FileName ? 'success' : 'primary'}
+    <Grid container>
+      <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Tooltip
+          title={
+            'Supports only ' +
+            ValidFileTypes.join(', ') +
+            ' files types up to ' + (MaxfileSize / 1000000).toString() + ' MB'
+          }
         >
-          <Stack
-            direction={'row'}
-            alignItems={'center'}
-            gap={1}
+          <Button
             sx={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-            }}>
-            {FileName ? <CheckCircle /> : <CloudUploadIcon />}
-            {FileName == '' ? FileLabel ? FileLabel : ' No file selected' : FileName}
-            {isMandatory && <span style={{ color: 'red' }}>*</span>}
-            <Box sx={{ textAlign: 'center' }}>
-              <input
-                ref={aRef}
-                type="file"
-                onChange={clickFile}
-                style={{
-                  opacity: 0,
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  cursor: 'pointer'
-                }}
-              />
-            </Box>
-          </Stack>
-          <Stack
-            direction={'row'}
-            alignItems={'center'}
-            justifyContent={'center'}
-            gap={1}
+              width: width,
+              border: (theme) =>
+                `1px dashed ${FileName ? theme.colors.success.main : theme.colors.primary.main
+                }`,
+              gap: 1,
+              position: 'relative',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }}
+            color={FileName ? 'success' : 'primary'}
           >
-
-          </Stack>
-        </Button>
-      </Tooltip>
-      <Typography mt={2}>
-        {FileError && <Errormessage Error={FileError} />}
-      </Typography>
-      {FilePath != '' && (
-        <div>
-          {deleteIcon &&
-            <IconButton
-              sx={{ marginRight: 1 }}
-              color={'error'}
-              onClick={clickDelete}
+            <Stack
+              direction={'row'}
+              alignItems={'center'}
+              gap={1}
+              sx={{
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+              }}>
+              {FileName ? <CheckCircle /> : <CloudUploadIcon />}
+              {FileName == '' ? FileLabel ? FileLabel : ' No file selected' : FileName}
+              {isMandatory && <span style={{ color: 'red' }}>*</span>}
+              <Box sx={{ textAlign: 'center' }}>
+                <input
+                  ref={aRef}
+                  type="file"
+                  onChange={clickFile}
+                  style={{
+                    opacity: 0,
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    cursor: 'pointer'
+                  }}
+                />
+              </Box>
+            </Stack>
+            <Stack
+              direction={'row'}
+              alignItems={'center'}
+              justifyContent={'center'}
+              gap={1}
             >
-              <DeleteIcon style={{ fontSize: 32 }} />
+
+            </Stack>
+          </Button>
+        </Tooltip>
+        {FilePath != '' && (
+          <div>
+            {deleteIcon &&
+              <IconButton
+                sx={{ marginRight: 1 }}
+                color={'error'}
+                onClick={clickDelete}
+              >
+                <DeleteIcon style={{ fontSize: 32 }} />
+              </IconButton>
+            }
+            {viewIcon && <IconButton
+              sx={{ marginLeft: 1 }}
+              color={'primary'}
+              onClick={clickFileName}
+            >
+              <VisibilityTwoToneIcon style={{ fontSize: 32 }} />
             </IconButton>
-          }
-          {viewIcon && <IconButton
-            sx={{ marginLeft: 1 }}
-            color={'primary'}
-            onClick={clickFileName}
-          >
-            <VisibilityTwoToneIcon style={{ fontSize: 32 }} />
-          </IconButton>
-          }
-        </div>
-      )}
-    </>
+            }
+          </div>
+        )}
+      </Grid>
+      <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+        <Typography mt={2}>
+          {FileError && <Errormessage Error={FileError} />}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
