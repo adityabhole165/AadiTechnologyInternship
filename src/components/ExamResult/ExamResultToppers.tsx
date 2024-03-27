0c6a3ae69bb3efee0d6f1d8706a5891fa50208ff
Chaitanyashingade <shingadechaitanya31@gmail.com>
2024-03-27 12:51:36 +0530

    ExamResultToppers

--- src/components/ExamResult/ExamResultToppers.tsx
+++ src/components/ExamResult/ExamResultToppers.tsx
@@ -1,15 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import RadioButton1 from 'src/libraries/RadioButton/RadioButton1';
 import PageHeader from 'src/libraries/heading/PageHeader';
+import { RootState, useDispatch } from 'src/store';
+
+import { Box, Grid, Typography } from '@mui/material';
+import {
+    IGetClassSubjectDropdownBodyCT
+} from 'src/interfaces/ExamResult/IExamResultToppers';
+import Dropdown from 'src/libraries/dropdown/Dropdown';
+import {
+    ClassSubjectListDropdownCT
+} from 'src/requests/ExamResult/RequestExamResultToppers';
 
 const ExamResultToppers = () => {
+    const dispatch = useDispatch();
     const [radioBtn, setRadioBtn] = useState('1');
+    const [SelectSubjectCT, setSelectSubjectCT] = useState('0');
+
     const RadioList = [
         { Value: '1', Name: 'Class Toppers' },
         { Value: '2', Name: 'Standard Toppers' }
     ];
+    const GetSubjectdropdownCT: any = useSelector(
+        (state: RootState) => state.ExamResultToppers.ClassSubjectCT
+    );
+
+    const ClassSubjectDropdownCT: IGetClassSubjectDropdownBodyCT = {
+        "asSchoolId": 18,
+        "asAcademicYearId": 54,
+        "asStandardDivId": 1270,
+        "asExamId": 609
+    };
+    useEffect(() => {
+        dispatch(ClassSubjectListDropdownCT(ClassSubjectDropdownCT));
+    }, []);
     const ClickRadio = (value) => {
         setRadioBtn(value);
+    };
+    const clickSubjectDropdownCT = (value) => {
+        setSelectSubjectCT(value);
     };
     return (
         <>
@@ -21,6 +51,29 @@ const ExamResultToppers = () => {
                 defaultValue={radioBtn}
                 Label={''}
             />
+            <Grid item xs={6}>
+                <Typography margin={'1px'}>
+                    <b>Select Standard:</b>
+                </Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <Box
+                    sx={{
+                        marginRight: '0px',
+                        width: '110%',
+                        padding: '0.9px',
+                        boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.2)',
+                        border: '1px solid black'
+                    }}
+                >
+                    <Dropdown
+                        Array={GetSubjectdropdownCT}
+                        handleChange={clickSubjectDropdownCT}
+                        defaultValue={SelectSubjectCT}
+
+                    />
+                </Box>
+            </Grid>
         </>
     )
 }
---
+++ src/requests/ExamResult/RequestExamResultToppers.ts
@@ -0,0 +1,105 @@
+import { createSlice } from '@reduxjs/toolkit';
+import ExamResultToppersApi from 'src/api/ExamResult/ApiExamResultToppers';
+import {
+    IGetClassSubjectDropdownBodyCT
+} from 'src/interfaces/ExamResult/IExamResultToppers';
+import { AppThunk } from 'src/store';
+
+const ExamResultToppersSlice = createSlice({
+  name: 'ExamResultToppers',
+
+  initialState: {
+   ClassSubjectCT:[],
+  },
+  reducers: {
+    SubjectListCT(state, action) {
+      state.ClassSubjectCT = action.payload;
+    },
+    // ExamListCT(state, action) {
+    //   state.ExamDropdownListCT = action.payload;
+    // },
+    // SubjectListCT(state, action) {
+    //   state.SubjectDropdownListCT = action.payload;
+    // },
+    // ToppersListCT(state, action) {
+    //   state.ClassToppersCT = action.payload;
+    // },
+    // SubjectToppersListCT(state, action) {
+    //   state.SubjectToppersCT = action.payload;
+    // }
+  }
+});
+
+
+export const ClassSubjectListDropdownCT =
+  (data: IGetClassSubjectDropdownBodyCT): AppThunk =>
+  async (dispatch) => {
+    const response = await ExamResultToppersApi.ClassSubjectDropDownCT(data);
+    let abc = [{ Id: '0', Name: 'All', Value: '0' }];
+
+    response.data.map((item, i) => {
+      abc.push({
+        Id: item.Subject_Id,
+        Name: item.Subject_Name,
+        Value: item.Subject_Id
+      });
+    });
+    dispatch(ExamResultToppersSlice.actions.SubjectListCT(abc));
+  };
+// export const ClassTopperListCT =
+//   (data: IGetClassToppersListBOdyCT): AppThunk =>
+//   async (dispatch) => {
+//     const response = await FinalResultToppersApiCT.ClassToppersListCT(data);
+//     let abc = response.data.GetTopperList.map((item, i) => {
+//       return {
+//         Text77:
+//           localStorage.getItem('SiteURL') + item.Rank_Image.replace('~', ''),
+//         Text2: item.Roll_No,
+//         Text3: item.Student_Name,
+//         Text4: item.Marks
+//       };
+//     });
+//     dispatch(FinalResultToppersSlice.actions.ToppersListCT(abc));
+
+//     let Subjects = [];
+//     response.data.GetSelectedSubjectTopperList.map((item, i) => {
+//       if (
+//         !Subjects.includes(
+//           item.Subject_Name + '#' + item.Rank_Image + '#' + item.Marks
+//         )
+//       ) {
+//         Subjects.push(
+//           item.Subject_Name + '#' + item.Rank_Image + '#' + item.Marks
+//         );
+//       }
+//     });
+//     let responseData = [];
+//     let child = null;
+//     Subjects.map((obj) => {
+//       child = {
+//         Rank_Image: obj.split('#')[1],
+//         Subject: obj.split('#')[0],
+//         Marks: obj.split('#')[2],
+//         Students: []
+//       };
+//       response.data.GetSelectedSubjectTopperList.map((item, i) => {
+//         if (
+//           obj ==
+//           item.Subject_Name + '#' + item.Rank_Image + '#' + item.Marks
+//         ) {
+//           child.Students.push({
+//             Text1: item.Roll_No,
+//             Text2: item.Student_Name
+//           });
+//         }
+//       });
+//       responseData.push(child);
+//     });
+//     console.log(responseData, 'Subjects');
+
+//     dispatch(
+//       FinalResultToppersSlice.actions.SubjectToppersListCT(responseData)
+//     );
+//   };
+
+export default ExamResultToppersSlice.reducer;
