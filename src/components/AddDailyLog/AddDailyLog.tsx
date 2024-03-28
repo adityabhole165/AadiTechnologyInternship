c8385722357b7c837363054a7f226cd774bedafb
Pramod <Pramodmane043@gmail.com>
2024-03-28 17:04:27 +0530

    AddDailyLog

--- src/components/AddDailyLog/AddDailyLog.tsx
+++ src/components/AddDailyLog/AddDailyLog.tsx
@@ -99,7 +99,7 @@ const AddDailyLog = () => {
   const [HeaderPublish, setHeaderPublish] = useState([
     { Id: 1, Header: 'Date', SortOrder: " Desc" },
     { Id: 2, Header: 'Attachment' },
-    { Id: 3, Header: 'Publish/UnPublish' },
+    { Id: 3, Header: 'Publish / UnPublish' },
     { Id: 4, Header: 'Edit', align: 'center' },
     { Id: 5, Header: 'Delete', align: 'center' }
   ]);
