7fd3d213c19ea388c090d9fcec1f77700c0bda86
Chaitanyashingade <shingadechaitanya31@gmail.com>
2024-04-02 17:46:58 +0530

    Exam Result Toppers

--- src/interfaces/ExamResult/IExamResultToppers.ts
+++ src/interfaces/ExamResult/IExamResultToppers.ts
@@ -2,6 +2,7 @@ export interface IGetClassDropdownBodyCT {
   asSchoolId: number;
   asAcademicYearId: number;
   asTeacherId: number;
+
 }
 export interface IGetClassDropdownResultCT {
   SchoolWise_Standard_Division_Id: string;
@@ -65,3 +66,73 @@ export interface IGetClassToppersListResultCT {
     }
   ];
 }
+export interface IGetStandardDropdownBodyST {
+  asSchoolId: Number;
+  asAcademicYearId: Number;
+  asTeacherId: Number;
+}
+export interface IGetStandardDropdownResultST {
+  Standard_Name: String;
+  Original_Standard_Id: String;
+  Standard_Id: String;
+}
+
+export interface IGetStandardExamDropdownBodyST {
+  asSchoolId: Number;
+  asAcademicYearId: Number;
+  asStandardId: Number;
+}
+export interface IGetStandardExamDropdownResultST {
+  SchoolWise_Test_Id: String;
+  SchoolWise_Test_Name: String;
+  Original_SchoolWise_Test_Id: String;
+  Sort_Order: String;
+}
+export interface IGetSubjectDropdownBodyST {
+  asSchoolId: number;
+  asAcademicYearId: number;
+  asStandardId: number;
+  asExamId: number;
+}
+export interface IGetSubjectDropdownResultST {
+  Subject_Id: string;
+  Subject_Name: string;
+}
+export interface IGetStandardToppersListBOdyST {
+  asSchoolId: number;
+  asAcademicYearId: number;
+  asStandardId: number;
+  asExamId: number;
+  asSubjectId: number;
+}
+export interface IGetStandardToppersListResultST {
+  GetTopperList: [
+    {
+      TopperRank: string;
+      Rank_Image: string;
+      Student_Id: string;
+      Roll_No: string;
+      Student_Name: string;
+      Marks_Scored: string;
+      Total_Marks: string;
+      Marks: string;
+      Standard: string;
+    }
+  ];
+  GetSelectedSubjectTopperList: [
+    {
+      TopperRank: string;
+      Rank_Image: string;
+      Student_Id: string;
+      Subject_Id: string;
+      Subject_Name: string;
+      Roll_No: string;
+      Student_Name: string;
+      Total_Marks_Scored: string;
+      Subject_Total_Marks: string;
+      Marks: string;
+      Standard: string;
+    }
+  ];
+}
+
