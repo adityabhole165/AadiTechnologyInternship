141bdae297c9d2b8b0a84656f487710a9ed4b57d
Chaitanyashingade <shingadechaitanya31@gmail.com>
2024-04-08 15:03:07 +0530

    Subject Mark list

--- src/api/ExamResult/ApiSubjectMarkList.ts
+++ src/api/ExamResult/ApiSubjectMarkList.ts
@@ -1,16 +1,22 @@
 import {
     IGetTestMarkBody,
     IGetTestMarkResult,
+    GetStudentsForSubjectMarkMouseOverResult,
+    GetStudentsForSubjectMarkMouseOverBody
   } from 'src/interfaces/ExamResult/ISubjectMarkList';
   import http from '../../requests/SchoolService/schoolServices';
   
   const TestMarkApi = (data:IGetTestMarkBody,) => {
     return http.post<IGetTestMarkResult>('Teacher/GetTestMark', data);
   };
+  const StudentNameMouseoverApi = (data:GetStudentsForSubjectMarkMouseOverBody,) => {
+    return http.post<GetStudentsForSubjectMarkMouseOverResult[]>('Teacher/GetStudentsForSubjectMarkSheet', data);
+  };
  
 
   const ApiSubjectMarkList= {
     TestMarkApi,
+    StudentNameMouseoverApi
     
   };
   export default ApiSubjectMarkList;
--- src/requests/ExamResult/RequestSubjectMarkList.ts
+++ src/requests/ExamResult/RequestSubjectMarkList.ts
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import ApiSubjectMarkList from 'src/api/ExamResult/ApiSubjectMarkList';
 import {
-    IGetTestMarkBody
+  GetStudentsForSubjectMarkMouseOverBody,
+  IGetTestMarkBody
 } from 'src/interfaces/ExamResult/ISubjectMarkList';
 import { AppThunk } from 'src/store';
 
@@ -9,31 +10,43 @@ const SubjectMarkListSlice = createSlice({
   name: 'SubjectMark',
   initialState: {
     listTestName: [],
+    StudentNameMouseOver: [],
 
-   
+
   },
   reducers: {
     GetTestMark(state, action) {
-        state.listTestName = action.payload;
-      },
+      state.listTestName = action.payload;
+    },
+    StudentListMouseOver(state, action) {
+      state.StudentNameMouseOver = action.payload;
+    },
   }
 });
 export const getmarklist =
   (data: IGetTestMarkBody): AppThunk =>
-  async (dispatch) => {
-    const response = await ApiSubjectMarkList.TestMarkApi(data);
-    let Roll_No=[]
-    let abc = response.data.listSchoolWise_Student_Test_Marks_Detail.map((item, i) => {
-      if(!Roll_No.includes(item.Roll_No)){
-return{
-  Roll_No: item.Roll_No,
-}
-      }
-      
-    });
-    dispatch(SubjectMarkListSlice.actions.GetTestMark(abc));
+    async (dispatch) => {
+      const response = await ApiSubjectMarkList.TestMarkApi(data);
+      let Roll_No = []
+      let abc = response.data.listSchoolWise_Student_Test_Marks_Detail.map((item, i) => {
+        if (!Roll_No.includes(item.Roll_No)) {
+          return {
+            Roll_No: item.Roll_No,
+          }
+        }
 
-  };
+      });
+      dispatch(SubjectMarkListSlice.actions.GetTestMark(abc));
+
+    };
+
+export const studentmouseoverlist =
+  (data: GetStudentsForSubjectMarkMouseOverBody): AppThunk =>
+    async (dispatch) => {
+      const response = await ApiSubjectMarkList.StudentNameMouseoverApi(data);
+      dispatch(SubjectMarkListSlice.actions.StudentListMouseOver(response.data));
+    };
+
 
 
 
