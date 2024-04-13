dd4637fed46a472c699425de0e402cbd66e5825c
Supriya-External <supriya@technisure.com>
2024-04-13 14:04:21 +0530

    Exam Result Base Screen-Pop up Screen work

--- src/components/ExamResultUnpublish/ExamResultUnpublish.tsx
+++ src/components/ExamResultUnpublish/ExamResultUnpublish.tsx
@@ -7,16 +7,12 @@ const ExamResultUnpublish = ({ open, setOpen, ExamName, TeacherName, ClickCloseD
   const dispatch = useDispatch();
   const { Id } = useParams();
   const navigate = useNavigate();
-  //const { ExamId, TeacherId, ExamName, TeacherName } = useParams();
 
   const [Reason, setReason] = useState('');
   const [ReasonError, setReasonError] = useState('');
-  // const [StandardDivisionId, setStandardDivisionId] = useState(
-  //   sessionStorage.getItem('TeacherId')
-  // );
+
   const asSchoolId = Number(localStorage.getItem('localSchoolId'));
   const asAcademicYearId = Number(sessionStorage.getItem('AcademicYearId'));
-  // const [TestId, setTestId] = useState("0");
 
   const PublishUnpublish: any = useSelector(
     (state: RootState) => state.ExamResult.PublishUnpublishExam
@@ -24,38 +20,13 @@ const ExamResultUnpublish = ({ open, setOpen, ExamName, TeacherName, ClickCloseD
   console.log("PublishUnpublish", PublishUnpublish)
 
 
-  // const onClickUnpublish = (IsPublish) => {
-  //   let isError = false;
-  //   if (Reason == '') {
-  //     setReasonError('Field should not be blank');
-  //     isError = true;
-  //   }
-  //   if (!isError) {
-  //     const GetPublishUnpublish: IPublishUnpublishExamResultBody = {
-  //       asSchoolId: Number(asSchoolId),
-  //       asStdDivId: Number(StandardDivisionId),
-  //       asAcadmicYearId: Number(asAcademicYearId),
-  //       asTest_Id: Number(TestId),
-  //       asUnpublishReason: Reason,
-  //       asPublishById: IsPublish
-  //     }
-  //     dispatch(getPublishUnpublishExam(GetPublishUnpublish));
-  //   }
-  //   if (!isError) {
-  //     ResetForm();
-  //   }
-  //   if (Reason != '') {
-  //     navigate('/extended-sidebar/Teacher/ExamResultBase');
-  //   }
-  // };
-
   const ResetForm = () => {
     setReason('');
   };
 
 
   const ClickOk = () => {
-    if (Reason != '') clickPublishUnpublish(false);
+    if (Reason !== '') clickPublishUnpublish(false,Reason);
   };
 
 
@@ -129,92 +100,4 @@ const ExamResultUnpublish = ({ open, setOpen, ExamName, TeacherName, ClickCloseD
   );
 };
 export default ExamResultUnpublish;
-{/* //     <div>
-//       <PageHeader heading="Enter Reason For Unpublish" />
 
-//       <div style={{ textAlign: 'right', color: 'red', paddingRight: '20px' }}>
-//         Mandatory Fields *
-//       </div>
-
-//       <Grid container spacing={1} justifyContent="center" alignItems="center">
-//         <Grid item xs={1}>
-//           <Typography>
-//             <b>Exam :</b>
-//           </Typography>
-//         </Grid>
-
-//         <Grid item xs={2}>
-//           <TextField value={ExamName} />
-//         </Grid>
-//       </Grid>
-//       <br></br>
-
-//       <Grid container spacing={1} justifyContent="center" alignItems="center">
-//         <Grid item xs={1}>
-//           <Typography>
-//             <b>Class Teacher Name :</b>
-//           </Typography>
-//         </Grid>
-
-//         <Grid item xs={2}>
-//           <TextField value={TeacherName} />
-//         </Grid>
-//       </Grid>
-//       <br></br>
-
-//       <Grid container spacing={1} justifyContent="center" alignItems="center">
-//         <Grid item xs={1}>
-//           <Typography>
-//             <b>Reason for Unpublish :</b>
-//           </Typography>
-//         </Grid>
-
-//         <Grid item xs={2}>
-//           <TextField
-//             value={Reason}
-//             onChange={(e) => setReason(e.target.value)}
-//             error={ReasonError !== ''}
-//             helperText={ReasonError}
-//             multiline
-//             style={{ resize: 'both', overflow: 'auto' }}
-//           />
-
-//           <div style={{ color: 'red' }}>*</div>
-//         </Grid>
-//       </Grid>
-
-//       <br></br>
-//       <br></br>
-
-//       <div>
-//         <Grid
-//           container
-//           spacing={2}
-//           style={{
-//             flexDirection: 'row',
-//             justifyContent: 'center',
-//             alignItems: 'center'
-//           }}
-//         >
-//           <Grid item xs={1}>
-//             <ButtonPrimary onClick={onClickUnpublish} variant="contained">
-//               <b>UNPUBLISH</b>
-//             </ButtonPrimary>
-//           </Grid>
-
-//           <Grid item xs={1}>
-//             <ButtonPrimary
-//               onClick={onClickCancel}
-//               variant="contained"
-//               style={{ backgroundColor: 'red', color: 'white' }}
-//             >
-//               CLOSE
-//             </ButtonPrimary>
-//           </Grid>
-//         </Grid>
-//       </div>
-//     </div>
-//   );
-// };
-
- */}
