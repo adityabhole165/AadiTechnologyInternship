42949442a9270721b431a5fb56408367681371ba
Pramod <Pramodmane043@gmail.com>
2024-04-19 14:16:46 +0530

    LessonPlanBaseScreen

--- src/components/LessonPlan/LessonPlanBaseScreen.tsx
+++ src/components/LessonPlan/LessonPlanBaseScreen.tsx
@@ -120,11 +120,16 @@ const LessonPlanBaseScreen = () => {
     { Id: 3, Header: 'View Remark', align: 'center' },
     { Id: 4, Header: 'Edit', align: 'center' },
     { Id: 5, Header: 'Delete', align: 'center' },
-    ...(CanEdit === 'Y' && LessonPlanList.map((item) => item.Text2) != 'False' ? [{ Id: 6, Header: 'View', align: 'center' }] : []),
+    ...(CanEdit === 'Y' && LessonPlanList.map((item) => item.Text2) != 'False' &&  LessonPlanList.map((item) => item.UserId) !==  selectClasstecahernew ? [{ Id: 6, Header: 'View', align: 'center' }] : []),
     { Id: 7, Header: 'Export', align: 'center' },
     { Id: 8, Header: 'Submit Status', align: 'center' }
   ];
+  
 
+  console.log(
+    ...(CanEdit === 'Y' && LessonPlanList.map((item) => item.Text2) != 'False' &&  LessonPlanList.map((item) => item.UserId) !=  selectClasstecahernew ? [{ Id: 6, Header: 'View', align: 'center' }] : [])
+  );
+  
   const GetLessonPlanListBody: IGetLessonPlanListBody = {
     asSchoolId: asSchoolId,
     asAcadmicYearId: asAcademicYearId,
