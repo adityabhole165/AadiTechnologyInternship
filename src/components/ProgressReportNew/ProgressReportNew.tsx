4ac262c2d43c21d0c6f49675e846fc48013cee51
Pramod <Pramodmane043@gmail.com>
2024-05-29 01:24:58 +0530

    ProgressReportNew

--- src/components/ProgressReportNew/ProgressReportNew.tsx
+++ src/components/ProgressReportNew/ProgressReportNew.tsx
@@ -1,10 +1,10 @@
 
-import { Box } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableRow, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { IGetClassTeachersBody,IGetStudentNameDropdownBody,IStudentProgressReportBody } from "src/interfaces/ProgressReport/IprogressReport";
+import { IGetClassTeachersBody, IGetStudentNameDropdownBody, IStudentProgressReportBody } from "src/interfaces/ProgressReport/IprogressReport";
 import SearchableDropdown from 'src/libraries/ResuableComponents/SearchableDropdown';
-import { CDAGetClassTeachers,CDAGetStudentName,CDAStudentProgressReport } from 'src/requests/ProgressReport/ReqProgressReport';
+import { CDAGetClassTeachers, CDAGetStudentName, CDAStudentProgressReport } from 'src/requests/ProgressReport/ReqProgressReport';
 import { RootState } from 'src/store';
 import CommonPageHeader from '../CommonPageHeader';
 
@@ -17,8 +17,8 @@ const ProgressReportNew = () => {
     const asStandardDivisionId = Number(sessionStorage.getItem('StandardDivisionId'));
     const [selectTeacher, SetselectTeacher] = useState('');
     const [StudentId, SetStudentId] = useState('');
-     console.log(StudentId,"StudentId");
-     
+    console.log(StudentId, "StudentId");
+
     const USGetClassTeachers: any = useSelector(
         (state: RootState) => state.ProgressReportNew.ISGetClassTeachers
     );
@@ -29,39 +29,39 @@ const ProgressReportNew = () => {
     const USStudentProgressReport: any = useSelector(
         (state: RootState) => state.ProgressReportNew.ISStudentProgressReport
     );
-    console.log(USStudentProgressReport,"USStudentProgressReport");
-    
+    console.log(USStudentProgressReport, "USStudentProgressReport");
 
-    const GetClassTeachersBody: IGetClassTeachersBody ={
+
+    const GetClassTeachersBody: IGetClassTeachersBody = {
         asSchoolId: Number(asSchoolId),
         asAcademicYearId: Number(asAcademicYearId),
         asTeacherId: Number(TeacherId)
     };
 
-    const GetStudentNameDropdownBody: IGetStudentNameDropdownBody ={
-        asSchoolId:Number(asSchoolId),
+    const GetStudentNameDropdownBody: IGetStudentNameDropdownBody = {
+        asSchoolId: Number(asSchoolId),
         asAcademicYearId: Number(asAcademicYearId),
         asStandardDivisionId: Number(asStandardDivisionId)
-       
+
     };
 
-    const StudentProgressReportBody: IStudentProgressReportBody ={
-        asSchoolId:Number(asSchoolId),
-        asAcadmeicYearId:Number(asAcademicYearId),
-        asStudentId:Number(StudentId),
-        asUserId:asUserId
-    
-       
+    const StudentProgressReportBody: IStudentProgressReportBody = {
+        asSchoolId: Number(asSchoolId),
+        asAcadmeicYearId: Number(asAcademicYearId),
+        asStudentId: Number(StudentId),
+        asUserId: asUserId
+
+
     };
 
     const clickSelectClass = (value) => {
-      SetselectTeacher(value)
+        SetselectTeacher(value)
     };
 
     const clickStudentList = (value) => {
         SetStudentId(value);
-      };
-    
+    };
+
 
 
     useEffect(() => {
@@ -81,30 +81,62 @@ const ProgressReportNew = () => {
 
     return (
         <Box sx={{ px: 2 }}>
-      <CommonPageHeader
-        navLinks={[
-          { title: 'Progress Report', path: '/extended-sidebar/Teacher/ProgressReportNew' }
-        ]}
-        rightActions={<>
-        
-          <SearchableDropdown
-            label={"Subject Teacher"}
-            sx={{ pl: 0, minWidth: '350px' }}
-            ItemList={ USGetClassTeachers}
-            onChange={clickSelectClass }
-            defaultValue={selectTeacher}
-            size={"small"}/>
+            <CommonPageHeader
+                navLinks={[
+                    { title: 'Progress Report', path: '/extended-sidebar/Teacher/ProgressReportNew' }
+                ]}
+                rightActions={<>
 
-          <SearchableDropdown
-            ItemList={USGetStudentNameDropdown}
-            sx={{ minWidth: '300px' }}
-            onChange={clickStudentList}
-            defaultValue={StudentId}
-            label={'Student Name'}
-            size={"small"}/>
-        </>}
-      />
-    </Box>
+                    <SearchableDropdown
+                        label={"Subject Teacher"}
+                        sx={{ pl: 0, minWidth: '350px' }}
+                        ItemList={USGetClassTeachers}
+                        onChange={clickSelectClass}
+                        defaultValue={selectTeacher}
+                        size={"small"} />
+
+                    <SearchableDropdown
+                        ItemList={USGetStudentNameDropdown}
+                        sx={{ minWidth: '300px' }}
+                        onChange={clickStudentList}
+                        defaultValue={StudentId}
+                        label={'Student Name'}
+                        size={"small"} />
+                </>}
+            />
+
+            <Box>
+                <hr />
+                <Typography variant={"h4"} textAlign={'center'} color={"primary"} mb={1}>
+                    Pawar Public Charitable Trust's
+                </Typography>
+                <hr />
+                <Typography variant={"h3"} textAlign={'center'} color={"primary"} mb={1}>
+                    PAWAR PUBLIC SCHOOL
+                </Typography>
+                <hr />
+                <Typography variant={"h4"} textAlign={'center'} color={"primary"} mb={1}>
+                  Progress Report
+                </Typography>
+                <hr />
+
+                <Table>
+                 <TableBody>
+                   
+                     
+                       <TableRow sx={{ bgcolor: 'grey.200' }}>
+                         <TableCell><b>Roll No:</b>{"6"} </TableCell>
+                         <TableCell><b>Name:</b> {"Miss Jahnavi Bhaveshkumar Barot"}	</TableCell>
+                         <TableCell><b>Class:</b> {"10 - C"} 	</TableCell>
+                         <TableCell><b>Year:</b> {"2024-2025"}	</TableCell>
+                       </TableRow>
+                    
+                
+                 </TableBody>
+               </Table>
+            </Box>
+            
+        </Box>
     )
 }
 
