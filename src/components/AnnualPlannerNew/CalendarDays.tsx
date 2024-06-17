97f2f6ec3e9b3cb8fcf9d041d8e378c791eb8698
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-06-17 12:29:32 +0530

    style: Event Data Tooltip

--- src/components/AnnualPlannerNew/CalendarDays.tsx
+++ src/components/AnnualPlannerNew/CalendarDays.tsx
@@ -81,9 +81,15 @@ const CalendarDays = ({ ItemList, ClickItem, DefaultValue, legendColors }) => {
               <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 1 }}>
                 {Item.Text1.map((obj, i) => {
                   return (<b style={{ width: '100%', paddingLeft: 2, paddingRight: 2 }} key={i}>
-                    <Box
-                      sx={{ color: legendColors[obj.Legend] }}
-                    >{obj.Name}</Box>
+                    <Tooltip title={obj.Name}>
+                      <Box
+                        sx={{
+                          color: legendColors[obj.Legend], overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          whiteSpace: 'nowrap'
+                        }}
+                      >{obj.Name}</Box>
+                    </Tooltip>
                   </b>)
                 })}
               </Box>
