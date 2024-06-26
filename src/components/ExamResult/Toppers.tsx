6e6dc056e7d7e5d643e9bb8d6297c3b0be769455
Akankshakurkute <akankshakurkute392@gmail.com>
2024-06-26 18:21:27 +0530

    Subject Toppers

--- src/components/ExamResult/Toppers.tsx
+++ src/components/ExamResult/Toppers.tsx
@@ -549,9 +549,11 @@ const ExamResultToppers = () => {
                             </Typography>
 
                         )}
-                        <Typography variant={"h4"} my={2}>
-                            Subject Toppers
-                        </Typography>
+                        {GetSubjectToppersListCT.length > 0 && (
+                            <Typography variant={"h4"} my={2}>
+                                Subject Toppers
+                            </Typography>
+                        )}
 
                         <Grid container spacing={2} sx={{ display: "flex", flexDirection: "row" }}>
                             {Object.keys(groupedBySubject).map((subject, subjectIndex) => (
