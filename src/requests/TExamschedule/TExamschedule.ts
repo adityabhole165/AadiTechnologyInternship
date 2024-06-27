440b4b37ae929f526458ae89b2202dc940ccc7e0
sofiya1911 <inamdarsofiya1786@gmail.com>
2024-06-27 10:11:34 +0530

    AllExamSchedule

--- src/requests/TExamschedule/TExamschedule.ts
+++ src/requests/TExamschedule/TExamschedule.ts
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import IGetAllStandards, {
-  IExamList,
   IGetExamsList
 } from 'src/interfaces/Teacher/TExamSchedule';
 import { AppThunk } from 'src/store';
@@ -11,20 +10,21 @@ const SelectStandardExamslice = createSlice({
   name: 'selectexam',
   initialState: {
     SelectStandard: [],
-    SelectExam: [],
     ExamData: [],
+    VeiwAllData: [],
     Loading: true
   },
   reducers: {
     getSelectStandardRes(state, action) {
       state.SelectStandard = action.payload;
     },
-    getSelectExamRes(state, action) {
-      state.SelectExam = action.payload;
-    },
     ViewExamDataRes(state, action) {
       state.Loading = false;
       state.ExamData = action.payload;
+    },
+    AllExamData(state, action) {
+      state.Loading = false;
+      state.VeiwAllData = action.payload;
     },
     getLoading(state, action) {
       state.Loading = true;
@@ -32,102 +32,69 @@ const SelectStandardExamslice = createSlice({
     }
   }
 });
-
-// export const GetSelectStandardRes =
-//   (data: IGetAllStandards): AppThunk =>
-//     async (dispatch) => {
-//       const response = await GetTExamResultListApi.GetAllStandards(data);
-//       const itemlist = response?.data.GetAllStandardsResult.map((item) => {
-//         return {
-//           id: item.Id,
-//           Name: item.Name,
-//           Value: item.Id
-//         };
-//       });
-//       dispatch(SelectStandardExamslice.actions.getSelectStandardRes(itemlist));
-//     };
-
-
-
-
-
 export const GetSelectStandardRes =
   (data: IGetAllStandards): AppThunk =>
-  async (dispatch) => {
-    const response = await GetTExamResultListApi.GetAllStandards(data);
-    const itemlist = [{ id: '0', Name: 'All', Value: '0' }];
-    if (response?.data.GetAllStandardsResult) {
-      const standards = response.data.GetAllStandardsResult.map((item) => ({
-        id: item.Id,
-        Name: item.Name,
-        Value: item.Id
-      }));
-      itemlist.push(...standards);
-    }
-    dispatch(SelectStandardExamslice.actions.getSelectStandardRes(itemlist));
-  };
-
-
-  export const GetSelectExamRes =
-  (data: IGetExamsList): AppThunk =>
     async (dispatch) => {
-      const response = await GetTExamResultListApi.IGetExams(data);
-      let itemlist = [];
-      if (response.data !== null)
-        itemlist = response.data?.GetExamsForStandardResult.map((item) => {
-          return {
-            id: item.Id,
-            Name: item.Name,
-            Value: item.Id
-          };
-        });
-      dispatch(SelectStandardExamslice.actions.getSelectExamRes(itemlist));
+      const response = await GetTExamResultListApi.GetAllStandards(data);
+      const itemlist = [{ id: '0', Name: 'All', Value: '0' }];
+      if (response?.data.GetAllStandardsResult) {
+        const standards = response.data.GetAllStandardsResult.map((item) => ({
+          id: item.Id,
+          Name: item.Name,
+          Value: item.Id
+        }));
+        itemlist.push(...standards);
+      }
+      dispatch(SelectStandardExamslice.actions.getSelectStandardRes(itemlist));
+
     };
 
-// export const GetSelectExamRes =
-//   (data: IGetExamsList): AppThunk =>
-//   async (dispatch) => {
-//     const response = await GetTExamResultListApi.IGetExams(data);
-//     let itemlist = [{ id: '0', Name: 'All', Value: '0' }];
-//     if (response.data !== null) {
-//       const exams = response.data.GetExamsForStandardResult.map((item) => ({
-//         id: item.Id,
-//         Name: item.Name,
-//         Value: item.Id
-//       }));
-//       itemlist.push(...exams);
-//     }
-//     dispatch(SelectStandardExamslice.actions.getSelectExamRes(itemlist));
-//   };
 
+// export const EmptyExam = (): AppThunk => async (dispatch) => {
+//   dispatch(SelectStandardExamslice.actions.getSelectExamRes([]));
+// };
 
-
-  
-export const EmptyExam = (): AppThunk => async (dispatch) => {
-  dispatch(SelectStandardExamslice.actions.getSelectExamRes([]));
-};
 export const ViewExamDataRess =
-  (data: IExamList): AppThunk =>
+  (data: IGetExamsList): AppThunk =>
     async (dispatch) => {
       dispatch(SelectStandardExamslice.actions.getLoading(true));
       const response = await GetTExamResultListApi.GetExamsList(data);
 
-      const itemlist = response?.data?.GetExamSchedulesResult.map((item) => {
+      const DataList = response?.data?.GetExamDropDownForExamScheduleDetailsList.map((item) => {
         return {
+          Text1: item.SchoolWise_Test_Name,
+          Text2: item.SchoolWise_Test_Id,
+          Text3: item.Exam_Start_Date,
+          Text4: item.Exam_End_Date,
+          Text5: item.IsCollapsed,
+          Text6: item.Instructions,
+          StandardId: item.StandardId
+        };
+      });
+
+      const itemlist = response?.data?.ExamScheduleList.map((item) => {
+        return {
+          text1: item.SchoolWise_Test_Id,
           header:
             item.SubjectName +
             ' ' +
             (item.TestType !== '' ? '- ' + item.TestType : ''),
-          text2: item.StartTime + ' - ' + item.EndTime ,
+          text2: item.StartTime + ' - ' + item.EndTime,
           text5: item.Description,
           text3: item.StartDayDate.replace('-', ' ').replace('-', ' '),
-          Standard_Name:item.Standard_Name,
+          Standard_Name: item.Standard_Name,
           Instructions: item.Instructions,
           startTime: item.StartTime,
-          endTime: item.EndTime
+          endTime: item.EndTime,
+          Description: item.Description
         };
       });
-      dispatch(SelectStandardExamslice.actions.ViewExamDataRes(itemlist));
+      dispatch(SelectStandardExamslice.actions.ViewExamDataRes(DataList));
+      dispatch(SelectStandardExamslice.actions.AllExamData(itemlist));
+      console.log(response, "DataList");
+
     };
 
 export default SelectStandardExamslice.reducer;
+
+
