4ec27aa1204fc3504c7d0143a9297df95bb8627f
NihalJadhav9291 <nihal.jadhav9291@gmail.com>
2024-06-27 12:27:11 +0530

    investment declaration

--- src/components/InvestmentDeclaration/InvestmentDeclaration.tsx
+++ src/components/InvestmentDeclaration/InvestmentDeclaration.tsx
@@ -1,8 +1,8 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { IGetInvestmentDetailsBody } from "src/interfaces/InvestmentDeclaration/InvestmentDeclaration";
-import { GetInvestmentDetails, CDAGetInvestmentDetails } from "src/requests/InvestmentDeclaration/ReqInvestmentDeclaration";
+import { IGetInvestmentDetailsBody, IGetRegimeDetailsDropdownBody } from "src/interfaces/InvestmentDeclaration/InvestmentDeclaration";
+import { CDAGetInvestmentDetails, CDAGetRegimeDropdown, GetInvestmentDetails } from "src/requests/InvestmentDeclaration/ReqInvestmentDeclaration";
 import { RootState } from "src/store";
 
 const InvestmentDeclaration = () => {
@@ -11,7 +11,7 @@ const InvestmentDeclaration = () => {
     const asSchoolId = Number(localStorage.getItem('localSchoolId'));
     const asUserId = Number(localStorage.getItem('UserId'));
 
-
+    const [newFilter, setnewFilter] = useState('');
 
     const USListInvestmentDetails: any = useSelector(
         (state: RootState) => state.InvestmentDeclaration.ISlistInvestmentDetails
@@ -30,6 +30,12 @@ const InvestmentDeclaration = () => {
 
     const listInvestmentSectionDetails = USListInvestmentSectionDetails?.listInvestmentSectionDetails || [];
 
+    const USGetRegimeDropdown: any = useSelector(
+        (state: RootState) => state.InvestmentDeclaration.ISGetRegimeDropdown
+    )
+    console.log(USGetRegimeDropdown, "USGetRegimeDropdown")
+
+    // console.log(listInvestmentSectionDetails, "listInvestmentSectionDetails");
 
     const GetInvestmentDeclarationBody: IGetInvestmentDetailsBody = {
         asSchoolId: asSchoolId,
@@ -43,6 +49,22 @@ const InvestmentDeclaration = () => {
         asUserId: asUserId
     }
 
+    const GetRegimeDropdown: IGetRegimeDetailsDropdownBody = {
+        asSchoolId: asSchoolId
+    }
+
+    // const filteredSectionDetails = listInvestmentSectionDetails.map(section => {
+    //     const matchingDetails = USListInvestmentDetails.filter(detail => detail.sectionId === section.Id);
+    //     return {
+    //         ...section,
+    //         matchingDetails
+    //     };
+    // });
+
+    useEffect(() => {
+        dispatch(CDAGetRegimeDropdown(GetRegimeDropdown))
+    }, [])
+
     useEffect(() => {
         dispatch(GetInvestmentDetails(GetInvestmentDeclarationBody))
     }, [])
@@ -51,22 +73,38 @@ const InvestmentDeclaration = () => {
         dispatch(CDAGetInvestmentDetails(GetInvestmentDeclarationBodyNew))
     }, [])
 
+    // const filteredSectionDetails = listInvestmentSectionDetails.filter((section: any) => section.UserId === asUserId);
+    const userName = USISlistInvestmentEmpDetails[0]?.UserName || 'Employee';
 
-    
+    // const filteredSectionDetails = listInvestmentSectionDetails.map((section) => section.Id);
+    // console.log(filteredSectionDetails, "filteredSectionDetails");
 
+
+    const filteredSectionDetails = listInvestmentSectionDetails.map(section => {
+        const matchingDetails = USListInvestmentDetails.filter(detail => detail.SectionId === section.Id);
+        return {
+            matchingDetails
+        }
+    });
+
+    console.log(filteredSectionDetails, "filteredSectionDetails");
+
+    const matchingDetailsData = filteredSectionDetails.flatMap(section => section.matchingDetails);
+
+    console.log(matchingDetailsData, "matchingDetailsData");
 
 
     return (
-        <Container>
-            <Typography variant="h4" gutterBottom>
-                Employee Investment Details
-            </Typography>
-            <Grid container spacing={3}>
-                {USISlistInvestmentEmpDetails.map((detail) => (
-                    <Grid item xs={12} key={detail.UserId}>
-                        <Box display="flex" justifyContent="space-between" marginBottom={2}>
-                            <Box>
-                                <Typography variant="h6">
+        <>
+            <Container>
+                <Typography variant="h4" gutterBottom>
+                    Employee Investment Details
+                </Typography>
+                <Grid container spacing={3}>
+                    {USISlistInvestmentEmpDetails.map((detail) => (
+                        <Grid item xs={12} key={detail.UserId}>
+                            <Box textAlign="center" marginBottom={2}>
+                                <Typography variant="h6" gutterBottom>
                                     <Box
                                         display="inline-block"
                                         border={1}
@@ -76,90 +114,186 @@ const InvestmentDeclaration = () => {
                                         mr={1}
                                         component="span"
                                     >
-                                        Name
+                                        {detail.SchoolName}
                                     </Box>
-                                    : {detail.UserName}
                                 </Typography>
-                                <Typography variant="body1">
+                                <Typography variant="h6" gutterBottom>
                                     <Box
                                         display="inline-block"
                                         border={1}
-                                        borderRadius={4}
-                                        px={5.4}
+                                        borderRadius={40}
+                                        px={8}
                                         py={1}
                                         mr={1}
                                         component="span"
                                     >
-                                        Employee No
+                                        {detail.SchoolAddress}
                                     </Box>
-                                    : {detail.EmployeeNo}
                                 </Typography>
-                                <Typography variant="body1">
+                                <Typography variant="h6" gutterBottom>
                                     <Box
                                         display="inline-block"
                                         border={1}
-                                        borderRadius={4}
-                                        px={7.1}
+                                        borderRadius={40}
+                                        px={8}
                                         py={1}
                                         mr={1}
                                         component="span"
                                     >
-                                        Address
+                                        INVESTMENT DECLARATION FORM FOR FINANCIAL YEAR {detail.FinancialYear}
                                     </Box>
-                                    : {detail.Address}
                                 </Typography>
                             </Box>
-                            <Box>
-                                <Typography variant="body1">
-                                    <Box
-                                        display="inline-block"
-                                        border={1}
-                                        borderRadius={4}
-                                        px={5.8}
-                                        py={1}
-                                        mr={1}
-                                        component="span"
-                                    >
-                                        Designation
-                                    </Box>
-                                    : {detail.Designation}
+                            <Box display="flex" justifyContent="space-between" marginBottom={2}>
+                                <Box>
+                                    <Typography variant="h6">
+                                        <Box
+                                            display="inline-block"
+                                            border={1}
+                                            borderRadius={40}
+                                            px={8}
+                                            py={1}
+                                            mr={1}
+                                            component="span"
+                                        >
+                                            Name
+                                        </Box>
+                                        : {detail.UserName}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        <Box
+                                            display="inline-block"
+                                            border={1}
+                                            borderRadius={4}
+                                            px={5.4}
+                                            py={1}
+                                            mr={1}
+                                            component="span"
+                                        >
+                                            Employee No
+                                        </Box>
+                                        : {detail.EmployeeNo}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        <Box
+                                            display="inline-block"
+                                            border={1}
+                                            borderRadius={4}
+                                            px={7.1}
+                                            py={1}
+                                            mr={1}
+                                            component="span"
+                                        >
+                                            Address
+                                        </Box>
+                                        : {detail.Address}
+                                    </Typography>
+                                </Box>
+                                <Box>
+                                    <Typography variant="body1">
+                                        <Box
+                                            display="inline-block"
+                                            border={1}
+                                            borderRadius={4}
+                                            px={5.8}
+                                            py={1}
+                                            mr={1}
+                                            component="span"
+                                        >
+                                            Designation
+                                        </Box>
+                                        : {detail.Designation}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        <Box
+                                            display="inline-block"
+                                            border={1}
+                                            borderRadius={4}
+                                            px={7.2}
+                                            py={1}
+                                            mr={1}
+                                            component="span"
+                                        >
+                                            PAN No
+                                        </Box>
+                                        : {detail.PanNo}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        <Box
+                                            display="inline-block"
+                                            border={1}
+                                            borderRadius={4}
+                                            px={7.4}
+                                            py={1}
+                                            mr={1}
+                                            component="span"
+                                        >
+                                            Gender
+                                        </Box>
+                                        : {detail.Gender}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+
+            <Container>
+                <Typography variant="h4" gutterBottom>
+                    Section Details
+                </Typography>
+                <Grid container spacing={3}>
+                    {listInvestmentSectionDetails.map((section) => (
+
+                        <Grid item xs={12} key={section.Id}>
+                            {/* <Paper elevation={3} style={{ padding: '20px' }}> */}
+                            <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
+                                <Typography variant="h6" gutterBottom>
+                                    {section.Name}
+                                    {/* {filteredSectionDetails.matchingDetails.map((detail) => (
+
+                                        { detail.Name }
+
+                                    ))} */}
+                                    {/* {matchingDetailsData.map((detail) => (
+                                        {detail.Name}
+                                    ))} */}
                                 </Typography>
+                                <br></br>
+                                {matchingDetailsData.map((detail, index) => (
+                                    <Typography variant="body2" key={index}>
+                                        {detail.Name}
+                                    </Typography>
+                                ))}
                                 <Typography variant="body1">
-                                    <Box
-                                        display="inline-block"
-                                        border={1}
-                                        borderRadius={4}
-                                        px={7.2}
-                                        py={1}
-                                        mr={1}
-                                        component="span"
-                                    >
-                                        PAN No
-                                    </Box>
-                                    : {detail.PanNo}
-                                </Typography>
-                                <Typography variant="body1">
-                                    <Box
-                                        display="inline-block"
-                                        border={1}
-                                        borderRadius={4}
-                                        px={7.4}
-                                        py={1}
-                                        mr={1}
-                                        component="span"
-                                    >
-                                        Gender
-                                    </Box>
-                                    : {detail.Gender}
+                                    <strong>Maximum Limit Rs.</strong> {section.GroupMaxAmount}
                                 </Typography>
                             </Box>
-                        </Box>
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
+                            <Box height={20} />
+                            {/* Blank space */}
+                            {/* </Paper> */}
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+
+            <Container>
+                <Box marginTop={4}>
+                    <Typography variant="body1" paragraph>
+                        I further undertake to provide all documentary proofs of payment made by me before 25th January, 2012 and if I fail to do so, the school can make full deduction of income tax dues from February / March 2012 salary.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        I hereby declare that Information as stated above is true and correct. I also authorize the School to recover tax (TDS) from my salary based on the declaration/documents submitted by me. I am personally liable to Income Tax proceedings for any misstatements in the declaration or proofs submitted herewith if they are inconsistent with the requirement of Income Tax Act, 1961.
+                    </Typography>
+                    <Typography variant="body1">
+                        I {userName} solemnly declare that to the best of my knowledge and belief the information given above is correct and complete.
+                    </Typography>
+                </Box>
+            </Container>
+        </>
     );
 
-}
+};
 
 export default InvestmentDeclaration;
