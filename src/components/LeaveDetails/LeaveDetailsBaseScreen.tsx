0d9bf1dc70bf997b6f7247f79d592248c1bec768
Saku2561 <sakshinanavare02019@gmail.com>
2024-07-06 16:26:29 +0530

    asterisk sign forleave details

--- src/components/LeaveDetails/LeaveDetailsBaseScreen.tsx
+++ src/components/LeaveDetails/LeaveDetailsBaseScreen.tsx
@@ -330,25 +330,28 @@ const LeaveDetailsBaseScreen = () => {
                 rightActions={
                     <Box sx={{ display: 'flex', gap: 1 }}>
                         <SearchableDropdown
-                            sx={{ pl: 0, minWidth: '20vw', pr: '16px' }}
+                              sx={{ minWidth: '20vw' }}
                             ItemList={GetAcademicYear.slice(0, 4)}
                             defaultValue={selectAcademicYear}
+                            mandatory
                             onChange={clickAcademicYearDropdown}
                             size={"small"}
                             label='Academic Year'
                         />
                         <SearchableDropdown
-                            sx={{ pl: 0, minWidth: '20vw', pr: '16px' }}
+                             sx={{ minWidth: '20vw' }}
                             ItemList={GetCategoryDropdownList.slice(0, 3)}
                             defaultValue={selectCategory}
+                            mandatory
                             onChange={clickCategoryDropdown}
                             size={"small"}
-                            label='Category'
+                            label={'Category '}
                         />
                         <SearchableDropdown
-                            sx={{ pl: 0, minWidth: '20vw', pr: '16px' }}
+                              sx={{ minWidth: '20vw' }}
                             ItemList={GetStatusDropdown}
                             defaultValue={selectStatus}
+                            mandatory
                             onChange={clickStatusDropdown}
                             size={"small"}
                             label='Status'
