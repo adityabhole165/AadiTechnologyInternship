d2e4611cef44b7bfda11b0e27bbffb2b942ee6ac
Pramod <Pramodmane043@gmail.com>
2024-07-10 14:44:01 +0530

    Studentwiseprogressreport

--- src/components/StudentWiseProgressReport/Studentwiseprogressreport.tsx
+++ src/components/StudentWiseProgressReport/Studentwiseprogressreport.tsx
@@ -56,7 +56,7 @@ const Studentwiseprogressreport = () => {
 
 
   const [HeaderPublish, setHeaderPublish] = useState([
-    { Id: 1, Header: 'Roll No.', SortOrder: "asc" },
+    { Id: 1, Header: 'Roll No', SortOrder: "Roll_No" },
     { Id: 2, Header: 'Student Name' },
     { Id: 3, Header: 'Edit' },
     { Id: 4, Header: 'Delete' }
@@ -152,7 +152,7 @@ const Studentwiseprogressreport = () => {
     asAssessmentId: Number(Assessment),
     asStartIndex: 0,
     asEndIndex: 20,
-    asSortExp: "Roll_No"
+    asSortExp:' ' + HeaderPublish[0].SortOrder
   }
 
   // const oneDeleteStudentTestMarks_Body: IoneDeleteStudentTestMarksBody = {
