7b9c70c6dd39f7fc746df5082ca13bfa68a700b4
Saku2561 <sakshinanavare02019@gmail.com>
2024-07-19 12:49:26 +0530

    Data table row hight

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -65,13 +65,13 @@ const DataTable: React.FC<Props> = ({ columns, data, isLoading = false, isPagina
           <TableBody>
             {isLoading ? (
               <TableRow>
-                <TableCell colSpan={columns.length} align="center">
+                <TableCell colSpan={columns.length} align="center" sx={{ paddingTop: '2.5px', paddingBottom: '2.5px' }}>
                   <CircularProgress />
                 </TableCell>
               </TableRow>
             ) : data.length === 0 ? (
               <TableRow>
-                <TableCell colSpan={columns.length} align="center">
+                <TableCell colSpan={columns.length} align="center"  sx={{ paddingTop: '2.5px', paddingBottom: '2.5px'}}>
                   No data available
                 </TableCell>
               </TableRow>
@@ -88,7 +88,7 @@ const DataTable: React.FC<Props> = ({ columns, data, isLoading = false, isPagina
                 data.map((row, rowIndex) => (
                   <TableRow key={rowIndex}>
                     {columns.map((column) => (
-                      <TableCell {...column.cellProps} key={column.id}>{column.renderCell(row)}</TableCell>
+                      <TableCell {...column.cellProps} key={column.id} sx={{ paddingTop: '2.5px', paddingBottom: '2.5px'}}>{column.renderCell(row)}</TableCell>
                     ))}
                   </TableRow>
                 ))
