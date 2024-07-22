15f6a2a7918ff9a09d6b796cf9980f42d47eb394
Pramod <Pramodmane043@gmail.com>
2024-07-22 16:02:55 +0530

    RequistionView

---
+++ src/components/Requisition/Datepicker.tsx
@@ -0,0 +1,40 @@
+
+import { DatePicker } from '@mui/x-date-pickers';
+
+const Datepicker = ({ DateValue, onDateChange, label, size }) => {
+    return (
+        <>
+            <DatePicker
+                //value={new Date(DateValue)}
+                value={DateValue ? new Date(DateValue) : null}
+                onChange={onDateChange}
+                format="dd MMM yyyy"
+                label={
+                    label ? (
+                        <>
+                            {label} 
+                        </>
+                    ) : (
+                        "Select Date"
+                    )
+                }
+                // label={<>
+                //     {label || "Select Date"} <span style={{ color: 'red' }}>*</span>
+                // </>}
+                views={['year', 'month', 'day']}
+                slotProps={{
+                    textField: {
+                        variant: 'outlined',
+                        fullWidth: true,
+                        size: size || 'medium'
+                    }
+                }}
+            // sx={{
+            //     width: size?.small ? '12vw' : '10vw'
+            // }}
+            />
+        </>
+    )
+}
+
+export default Datepicker;
