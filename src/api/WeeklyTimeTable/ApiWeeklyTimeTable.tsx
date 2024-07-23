7c8cfe44cb12dea0335d9fd382ae5fa79940fe43
AmanKadam-16 <aman16112002kadam@gmail.com>
2024-07-23 18:32:30 +0530

    API: Binding on WeeklyTimetable page

--- src/api/WeeklyTimeTable/ApiWeeklyTimeTable.tsx
+++ src/api/WeeklyTimeTable/ApiWeeklyTimeTable.tsx
@@ -1,4 +1,4 @@
-import { IGetTeacherAndStandardForTimeTableBody, IGetTeacherAndStandardForTimeTableResult } from 'src/interfaces/WeeklyTimeTable/IWeeklyTimetable';
+import { IGetDataForAdditionalClassesBody, IGetDataForAdditionalClassesResult, IGetResetTimetableBody, IGetTeacherAndStandardForTimeTableBody, IGetTeacherAndStandardForTimeTableResult } from 'src/interfaces/WeeklyTimeTable/IWeeklyTimetable';
 import http from '../../requests/SchoolService/schoolServices';
 
 
@@ -7,9 +7,19 @@ const GetTeacherAndStandardForTimeTableApi = (data: IGetTeacherAndStandardForTim
     return http.post<IGetTeacherAndStandardForTimeTableResult>('Teacher/TeacherAndStandardForTimeTable', data);
 };
 
+const GetDataForAdditionalClassesApi = (data: IGetDataForAdditionalClassesBody) => {
+    return http.post<IGetDataForAdditionalClassesResult>('Teacher/GetDataForAdditionalClasses', data);
+};
+
+const GetResetTimetableApi = (data: IGetResetTimetableBody) => {
+    return http.post<string>('Teacher/ResetTimetable', data);
+};
+
 
 const WeeklyTimeTableApi = {
-    GetTeacherAndStandardForTimeTableApi
+    GetTeacherAndStandardForTimeTableApi,
+    GetDataForAdditionalClassesApi,
+    GetResetTimetableApi
 };
 
 export default WeeklyTimeTableApi;
--- src/requests/WeeklyTimeTable/RequestWeeklyTimeTable.tsx
+++ src/requests/WeeklyTimeTable/RequestWeeklyTimeTable.tsx
@@ -1,17 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 import WeeklyTimeTableApi from 'src/api/WeeklyTimeTable/ApiWeeklyTimeTable';
-import { IGetTeacherAndStandardForTimeTableBody } from 'src/interfaces/WeeklyTimeTable/IWeeklyTimetable';
+import { IGetDataForAdditionalClassesBody, IGetResetTimetableBody, IGetTeacherAndStandardForTimeTableBody } from 'src/interfaces/WeeklyTimeTable/IWeeklyTimetable';
 import { AppThunk } from 'src/store';
 
 const WeeklyTimeTableSlice = createSlice({
     name: 'WeeklyTimeTable',
     initialState: {
         ISTeachersList: [],
+        ISAddClassesWeekDay: [],
+        ISAddClassesLectureNumber: [],
+        ISAddClassesSubjectName: [],
+        ISResetTimetableMsg: '',
         Loading: true
     },
     reducers: {
         RGetTeachersList(state, action) {
             state.ISTeachersList = action.payload;
+            state.Loading = false;
+        },
+        RAddClassesWeekDay(state, action) {
+            state.ISAddClassesWeekDay = action.payload;
+            state.Loading = false;
+        },
+        RAddClassesLectureNumber(state, action) {
+            state.ISAddClassesLectureNumber = action.payload;
+            state.Loading = false;
+        },
+        RAddClassesSubjectName(state, action) {
+            state.ISAddClassesSubjectName = action.payload;
+            state.Loading = false;
+        },
+        RGetResetTimetableMsg(state, action) {
+            state.ISResetTimetableMsg = action.payload;
+            state.Loading = false;
+        },
+        RClearResetTimetableMsg(state) {
+            state.ISResetTimetableMsg = '';
             state.Loading = false;
         },
         getLoading(state, action) {
@@ -39,6 +63,60 @@ export const CDAGetTeachersList =
             responseData.unshift({ Id: '0', Name: 'Select', Value: '0' })
             dispatch(WeeklyTimeTableSlice.actions.RGetTeachersList(responseData));
         };
+
+export const CDAGetDataForAdditionalClasses =
+    (data: IGetDataForAdditionalClassesBody): AppThunk =>
+        async (dispatch) => {
+            dispatch(WeeklyTimeTableSlice.actions.getLoading(true));
+            const response = await WeeklyTimeTableApi.GetDataForAdditionalClassesApi(data);
+            console.log(response.data)
+            let WeekDayList = response.data.listWeekDayName.map((item) => {
+                return (
+                    {
+                        Id: item.Weekday_Id,
+                        Name: item.WeekDay_Name,
+                        Value: item.Weekday_Id
+                    }
+                )
+            })
+
+            let LectureNumberList = response.data.listLectureNumber.map((item) => {
+                return (
+                    {
+                        Id: item.Lecture_Number,
+                        Name: item.Lecture_Number,
+                        Value: item.Lecture_Number
+                    }
+                )
+            })
+
+            let SubjectNameList = response.data.listclasssSubjectName.map((item) => {
+                return (
+                    {
+                        Id: item.Subject_Id,
+                        Name: item.classSubjectName,
+                        Value: item.Subject_Id
+                    }
+                )
+            })
+            dispatch(WeeklyTimeTableSlice.actions.RAddClassesLectureNumber(LectureNumberList));
+            dispatch(WeeklyTimeTableSlice.actions.RAddClassesWeekDay(WeekDayList));
+            dispatch(WeeklyTimeTableSlice.actions.RAddClassesSubjectName(SubjectNameList));
+        }
+
+export const CDAResetTimetable =
+    (data: IGetResetTimetableBody): AppThunk =>
+        async (dispatch) => {
+            dispatch(WeeklyTimeTableSlice.actions.getLoading(true));
+            const response = await WeeklyTimeTableApi.GetResetTimetableApi(data);
+            dispatch(WeeklyTimeTableSlice.actions.RGetResetTimetableMsg(response.data));
+        }
+export const CDAGetResetTimetableMsgClear =
+    (): AppThunk =>
+        async (dispatch) => {
+            dispatch(WeeklyTimeTableSlice.actions.getLoading(true));
+            dispatch(WeeklyTimeTableSlice.actions.RClearResetTimetableMsg());
+        }
 
 
 export default WeeklyTimeTableSlice.reducer;
