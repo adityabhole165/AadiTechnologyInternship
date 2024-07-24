9c7a10328be3dcfa80f71ef4eb6ae0b84d4cc301
sofiya1911 <inamdarsofiya1786@gmail.com>
2024-07-24 16:04:03 +0530

    change spelling

--- src/components/AddSchoolNitice/ValidationSchema.tsx
+++ src/components/AddSchoolNitice/ValidationSchema.tsx
@@ -1,14 +1,14 @@
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-    linkName: Yup.string().required('Link name should not be black'),
-    NoticeName: Yup.string().required('Notice name should not be black'),
+    linkName: Yup.string().required('Link name should not be blank.'),
+    NoticeName: Yup.string().required('Notice name should not be blank'),
     // displayLocation: Yup.string().required('Display Location is required'),
-    startDate: Yup.date().nullable().required('Start date should not be balck'),
-    endDate: Yup.date().nullable().required('End date should not be black'),
-    startTime: Yup.date().nullable().required('Start time should not be black'),
-    endTime: Yup.date().nullable().required('End time should not be black'),
-    sortOrder: Yup.string().required('Sort Order should not be black'),
+    startDate: Yup.date().nullable().required('Start date should not be blank'),
+    endDate: Yup.date().nullable().required('End date should not be blank'),
+    startTime: Yup.date().nullable().required('Start time should not be blank'),
+    endTime: Yup.date().nullable().required('End time should not be blank'),
+    sortOrder: Yup.string().required('Sort Order should not be blank'),
     noticeFile: Yup.mixed().required('Notice File is required'),
     // imageFile: Yup.mixed().required('Image is required'),
     // description: Yup.string().min(20).max(255).required('Description is required')
