57debc7b6c5b0cb2f9b094b9683815395f217412
Rakeshsute3 <rakeshsute3@gmail.com>
2024-07-24 13:14:13 +0530

    Color chnage

--- src/components/InvestmentDeclaration/InvestmentDeclaration.tsx
+++ src/components/InvestmentDeclaration/InvestmentDeclaration.tsx
@@ -293,21 +293,21 @@ const InvestmentDeclaration = () => {
                         <Grid container spacing={3}>
                             {USISlistInvestmentEmpDetails.map((detail) => (
                                 <Grid item xs={12} key={detail.UserId}>
-                                    <Box sx={{ backgroundColor: '#f0f0f0', textAlign: 'center', marginBottom: 2 }}>
+                                    <Box sx={{ backgroundColor: '#b3ffff', textAlign: 'center', marginBottom: 2 }}>
                                         <hr />
-                                        <Typography variant={"h4"} textAlign={'center'} color={"primary"} mb={1}>
+                                        <Typography variant={"h4"} textAlign={'center'} color={""} mb={1}>
 
                                             {detail.SchoolName}
 
                                         </Typography>
                                         <hr />
-                                        <Typography variant={"h4"} textAlign={'center'} color={"primary"} mb={1}>
+                                        <Typography variant={"h4"} textAlign={'center'} color={""} mb={1}>
 
                                             {detail.SchoolAddress}
 
                                         </Typography>
                                         <hr />
-                                        <Typography variant={"h4"} textAlign={'center'} color={"primary"} mb={1}>
+                                        <Typography variant={"h4"} textAlign={'center'} color={""} mb={1}>
 
                                             INVESTMENT DECLARATION FORM FOR FINANCIAL YEAR {detail.FinancialYear}
 
