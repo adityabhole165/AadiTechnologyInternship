938452688d6774f136cdc2077aeef1c871a5c64a
Pamyamane <pramodmane043@gmail.com>
2024-08-05 12:45:16 +0530

    RequistionView

--- src/components/Requisition/RequistionView.tsx
+++ src/components/Requisition/RequistionView.tsx
@@ -96,7 +96,7 @@ const RequistionView = () => {
                                 sx={{
                                     color: 'white',
                                     backgroundColor: grey[500],
-                                    mr:-1,
+                                   
                                     height: '36px !important',
                                     ':hover': { backgroundColor: grey[600] }
                                 }}
