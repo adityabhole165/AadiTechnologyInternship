8cc34dc91dc3e7ec9adc1ed2a035ee852239aeb4
Pamyamane <pramodmane043@gmail.com>
2024-08-12 17:15:43 +0530

    PreprimaryProgressReport

--- src/components/PreprimaryProgressReport/PreprimaryProgressReport.tsx
+++ src/components/PreprimaryProgressReport/PreprimaryProgressReport.tsx
@@ -125,7 +125,7 @@ const PreprimaryProgressReport = () => {
     const IsPresent1 = countDuplicates(IsPresent);
     const TotalAttendance = countDuplicates(YearwiseStudentId1);
     const presentCount = IsPresent1["true"] || '';
-    const totalCount = TotalAttendance["38639"] || '';
+    const totalCount = TotalAttendance[StudentId] || '';
 
     const clickPrint = () => {
         window.open('https://schoolwebsite.regulusit.net/RITeSchool/Student/StudentAnnualResultPrint.aspx?eNXR1G7TvKnm53e4OO8B4kK13X5MkQwItrEc3d1VEwmx4YWMbwW4T3xnZE3Dc3QV4xnyziKPOKwj6nT8UFXzenNlqH5PQrTSymfl4ktp7WE/4fc29EcOQXYAkGBiAYJ4ubKxU+rY3xn5qTDv2PMcpA==q');
