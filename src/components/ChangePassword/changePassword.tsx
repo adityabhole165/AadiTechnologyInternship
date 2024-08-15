4605f47c7b128dc1191795b47e1fb66a41979cdd
Saku2561 <sakshinanavare02019@gmail.com>
2024-08-15 09:44:44 +0530

    page header

--- src/components/ChangePassword/changePassword.tsx
+++ src/components/ChangePassword/changePassword.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material';
 import { useEffect } from 'react';
 import Form from 'src/libraries/form/form3';
 import PageHeader from 'src/libraries/heading/PageHeader';
+import CommonPageHeader from '../CommonPageHeader';
 
 function ChangePassword() {
   useEffect(() => {
@@ -11,7 +12,9 @@ function ChangePassword() {
     'Capitalization Matters! Min 6 characters, Max 15 characters. Password should be combination of at least one character, digit & special character.';
   return (
     <Box sx={{ px: 2 }}>
-      <PageHeader heading={'Change Password'} subheading={''} />
+    <CommonPageHeader
+        navLinks={[{ title: 'Change Password', path: ' ' }
+        ]}/>
       <Form />
     </Box>
   );
