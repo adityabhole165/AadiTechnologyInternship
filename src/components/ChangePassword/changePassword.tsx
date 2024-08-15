f59287651f39ff62163e38d77e8b065a67ed0b30
Rakeshsute3 <rakeshsute3@gmail.com>
2024-08-15 17:22:56 +0530

    Message center Ui Set

--- src/components/ChangePassword/changePassword.tsx
+++ src/components/ChangePassword/changePassword.tsx
@@ -1,8 +1,9 @@
-import { Box } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Alert, Box, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import Form from 'src/libraries/form/form3';
 import PageHeader from 'src/libraries/heading/PageHeader';
 import CommonPageHeader from '../CommonPageHeader';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function ChangePassword() {
   useEffect(() => {
@@ -10,12 +11,21 @@ function ChangePassword() {
   }, []);
   const Note: string =
     'Capitalization Matters! Min 6 characters, Max 15 characters. Password should be combination of at least one character, digit & special character.';
-  return (
+    const note = [
+      '1) Capitalization Matters! Min 6 characters, Max 15 characters.',
+      '2) Password should be combination of at least one character, digit & special character.'
+    ];
+    const note1 = [
+      'It seems you have not changed the system generated password. Please reset your password for security purpose.'
+    ];
+    return (
     <Box sx={{ px: 2 }}>
     <CommonPageHeader
         navLinks={[{ title: 'Change Password', path: ' ' }
         ]}/>
-      <Form />
+       
+                     <Form />
+     
     </Box>
   );
 }
