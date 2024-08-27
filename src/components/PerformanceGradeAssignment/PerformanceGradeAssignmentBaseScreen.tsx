e1bda429a502d16d337be325fde1f2b48af5e1cc
Akankshakurkute <akankshakurkute392@gmail.com>
2024-08-27 23:02:00 +0530

    Add Dropdown and help icon

--- src/components/PerformanceGradeAssignment/PerformanceGradeAssignmentBaseScreen.tsx
+++ src/components/PerformanceGradeAssignment/PerformanceGradeAssignmentBaseScreen.tsx
@@ -1,8 +1,16 @@
-import { Box } from "@mui/material"
-import CommonPageHeader from "../CommonPageHeader"
+import { QuestionMark } from "@mui/icons-material";
+import { Box, IconButton, Tooltip } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import SearchableDropdown from "src/libraries/ResuableComponents/SearchableDropdown";
+import CommonPageHeader from "../CommonPageHeader";
 
 const PerformanceGradeAssignmentBaseScreen = () => {
+    const clickYearDropdown = (value) => {
+
+    };
+
     return (
+
         <Box sx={{ px: 2 }}>
             <CommonPageHeader
                 navLinks={[
@@ -10,6 +18,32 @@ const PerformanceGradeAssignmentBaseScreen = () => {
                 ]}
                 rightActions={
                     <>
+                        <Box sx={{ background: 'white' }}>
+                            <SearchableDropdown
+                                sx={{ minWidth: '25vw' }}
+                                ItemList={[0]}
+                                onChange={clickYearDropdown}
+                                label={'Year'}
+                                defaultValue={""}
+                                size={"small"}
+                            />
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <Tooltip title={"Displays user who report to the logged in teacher."}>
+                                <IconButton
+                                    sx={{
+                                        color: 'white',
+                                        backgroundColor: grey[500],
+                                        height: '36px !important',
+                                        ':hover': { backgroundColor: grey[600] },
+                                        marginRight: '-4px',
+                                        // marginLeft: '8px', 
+                                    }}
+                                >
+                                    <QuestionMark />
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
                     </>}
             />
         </Box>
