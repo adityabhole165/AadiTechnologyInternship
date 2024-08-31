74fab8f41293e0b53acecde1bd5e8fed385ef59c
Rakeshsute3 <rakeshsute3@gmail.com>
2024-08-31 16:02:14 +0530

    card change

--- src/librariesWeb/CardDashContanier2.tsx
+++ src/librariesWeb/CardDashContanier2.tsx
@@ -2,19 +2,18 @@ import { Box, Card, Grid, Typography } from '@mui/material';
 import { Calendar } from 'react-multi-date-picker';
 function CardDashContainer2() {
   return (
-    <div>
-      <Card>
-        <Typography variant="h3" p={1} sx={{ color: '#304ffe' }}>
+      <Box sx={{ height: 'auto', width:'auto', backgroundColor:'white', p:1 }}>
+      <Grid item xs={12} sx={{ backgroundColor:'#38548A'  }}>
+        <Typography variant="h3" p={0.8} mb={2} sx={{ color: 'white' }}>
           Upcoming Event
         </Typography>
+        </Grid>
         <Grid container spacing={1} >
           <Grid item sm={6} md={12}>
             <Box
               sx={{ width: '800px', mr: '0px' }}
               justifyContent={'center'}
-              px={3.7}
-              
-            >
+              px={4.5}>
               <Calendar />
             </Box>
           </Grid>
@@ -24,23 +23,25 @@ function CardDashContainer2() {
                 component={Box}
                 m={1.7}
                 p={1}
-                sx={{ backgroundColor: '#4db6ac' }}
+                sx={{ backgroundColor: '#4db6ac', color:'black' }}
               >
-                School Event
+               <b> School Event</b>
               </Card>
               <Card
                 component={Box}
                 m={1.7}
                 p={1}
-                sx={{ backgroundColor: '#64b5f6' }}
+                sx={{ backgroundColor: '#64b5f6', color:'black' }}
               >
-                Holiday
+               <b> Holiday</b>
               </Card>
             </Card>
           </Grid>
+        
         </Grid>
-      </Card>
-    </div>
+        </Box>
+      
+    
   );
 }
 
