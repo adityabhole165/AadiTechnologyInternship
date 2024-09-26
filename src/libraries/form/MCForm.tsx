0a8f217e5bfa20b21ef0a07d67c63a953ed1bd2f
sakshi2561 <Sakshinanavare02019@gmail.com>
2024-09-26 17:56:07 +0530

    search tooltip

--- src/libraries/form/MCForm.tsx
+++ src/libraries/form/MCForm.tsx
@@ -1,12 +1,7 @@
-import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
-import { Avatar, Box, Button, Grid, IconButton, TextField } from '@mui/material';
-import { useState } from 'react';
-import Dropdown from '../dropdown/Dropdown';
-import { ListStyle } from '../styled/CardStyle';
-import Datepicker from '../DateSelector/Datepicker';
-import { blue, red } from '@mui/material/colors';
 import SearchTwoTone from '@mui/icons-material/SearchTwoTone';
+import { Box, Grid, IconButton, TextField, Tooltip } from '@mui/material';
+import { useState } from 'react';
+import Datepicker from '../DateSelector/Datepicker';
 import SearchableDropdown from '../ResuableComponents/SearchableDropdown';
 
 const MCForm = ({
@@ -23,6 +18,7 @@ const MCForm = ({
   const [searchText, setSearchText] = useState('');
   const [operator, setOperator] = useState('=');
   const [searchDate, setSearchDateDate] = useState<string>('');
+
 
   const clickDate = (e) => {
     setSearchDateDate(e.target.value);
@@ -64,9 +60,9 @@ const MCForm = ({
 
   return (
     <>
-    <Box sx={{p:2, backgroundColor:'white', mb: 2}}>
-    {/* <ListStyle> */}
-      {/* <Box display={{ xs: 'block', sm: 'none' }}>
+      <Box sx={{ p: 2, backgroundColor: 'white', mb: 2 }}>
+        {/* <ListStyle> */}
+        {/* <Box display={{ xs: 'block', sm: 'none' }}>
         <Avatar
           onClick={CloseSearchBar}
           sx={{
@@ -86,103 +82,107 @@ const MCForm = ({
         </Avatar>
       </Box> */}
 
-      <Grid container spacing={2} >
-        <Grid item xs={12} md={12}>
-          <TextField
-            id="standard-basic"
-            label="Name / Subject / Message Body :"
-            // variant="standard"
-            fullWidth
-            onChange={textOnChange}
-            size={"medium"}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          {/* <Dropdown
+        <Grid container spacing={2} >
+          <Grid item xs={12} md={12}>
+            <TextField
+              id="standard-basic"
+              label="Name / Subject / Message Body :"
+              // variant="standard"
+              fullWidth
+              onChange={textOnChange}
+              size={"medium"}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            {/* <Dropdown
             Array={AcademicYearList}
             handleChange={clickAY}
             label={'Year'}
             defaultValue={academicYear}
           /> */}
-       
-        <SearchableDropdown
-            ItemList={AcademicYearList}
-            onChange={clickAY}
-            label={'Year'}
-            defaultValue={academicYear}
-            mandatory
-            size={"medium"}
-            sx={{width:'100%'}}
-          /></Grid>
 
-        <Grid item xs={12} md={6} >
-          {/* <Dropdown
+            <SearchableDropdown
+              ItemList={AcademicYearList}
+              onChange={clickAY}
+              label={'Year'}
+              defaultValue={academicYear}
+              mandatory
+              size={"medium"}
+              sx={{ width: '100%' }}
+            /></Grid>
+
+          <Grid item xs={12} md={6} >
+            {/* <Dropdown
             Array={MonthYearList}
             handleChange={clickMY}
             label={'Month'}
             defaultValue={monthYear}
           /> */}
-          <SearchableDropdown
-            ItemList={MonthYearList}
-            onChange={clickAY}
-            label={'Month'}
-            defaultValue={monthYear}
-            size={"medium"}
-            sx={{width:'100%'}}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box>
-            {/* <Dropdown
+            <SearchableDropdown
+              ItemList={MonthYearList}
+              onChange={clickAY}
+              label={'Month'}
+              defaultValue={monthYear}
+              size={"medium"}
+              sx={{ width: '100%' }}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box>
+              {/* <Dropdown
               Array={operatorArray}
               handleChange={clickOperator}
               defaultValue={operator}
               size={"medium"}
             /> */}
-            <SearchableDropdown
-            ItemList={operatorArray}
-            onChange={clickAY}
-            label={'Operator'}
-            defaultValue={operator}
-            size={"medium"}
-            sx={{width:'100%'}}
-          />
-          </Box>
-        </Grid>
-        <Grid item xs={5.4}>
-          {/* <TextField
+              <SearchableDropdown
+                ItemList={operatorArray}
+                onChange={clickAY}
+                label={'Operator'}
+                defaultValue={operator}
+                size={"medium"}
+                sx={{ width: '100%' }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={5.4}>
+            {/* <TextField
             type="date"
             id="outlined-required"
             variant="standard"
             onChange={clickDate}
             fullWidth
           /> */}
-           <Datepicker
-                  DateValue={searchDate}
-                  onDateChange={clickDate}
-                  label={''}
-                  size={"medium"}
+            <Datepicker
+              DateValue={searchDate}
+              onDateChange={clickDate}
+              label={''}
+              size={"medium"}
 
-                />
+            />
+          </Grid>
+          <Grid item xs={0}>
+            <Tooltip
+              title={'Search'}
+            >
+              <IconButton
+                onClick={onClick}
+                sx={{
+                  background: (theme) => theme.palette.primary.main,
+                  color: 'white',
+                  mt: 1,
+                  '&:hover': {
+                    backgroundColor: (theme) => theme.palette.primary.dark
+                  }
+                }}
+              >
+                <SearchTwoTone />
+              </IconButton>
+            </Tooltip>
+          </Grid>
         </Grid>
-        <Grid item xs={0}>
-            <IconButton
-            onClick={onClick}
-            sx={{
-              background: (theme) => theme.palette.primary.main,
-              color: 'white',
-              mt:1,
-              '&:hover': {
-                backgroundColor: (theme) => theme.palette.primary.dark
-              }
-            }}
-          >
-            <SearchTwoTone />
-          </IconButton>
-        </Grid>
-      </Grid>
-    {/* </ListStyle> */}
-    </Box>
+        {/* </ListStyle> */}
+      </Box>
     </>
   );
 };
