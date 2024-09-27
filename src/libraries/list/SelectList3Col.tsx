e219a94d207e9f83d1bf7f77880bb21a00c7645f
Harshwardhanchougule <harshchougule25@gmail.com>
2024-09-27 13:36:03 +0530

    Header added in message box

--- src/libraries/list/SelectList3Col.tsx
+++ src/libraries/list/SelectList3Col.tsx
@@ -32,19 +32,19 @@ const SelectList3Col = ({ Itemlist, refreshData, ActiveTab, DeleteDraft }) => {
         </TableHead>
       </Table> */}
       <Grid container sx={{borderRadius:'7px', mb:1, p:1.5, background: (theme) => theme.palette.secondary.main, border: (theme) => `1px solid ${theme.palette.grey[300]}`, overflow: 'hidden' }}>
-        <Grid  xs={12} sm={2} md={0.7} sx={{color:'white'}}>
-         Check Box
+        <Grid  xs={12} sm={2} md={2} sx={{color:'white'}}>
+        Subject
         </Grid>
         <Grid  xs={12} sm={2} md={3} sx={{color:'white'}}>
-          Subject
+          CC
         </Grid>
-        <Grid  xs={12} sm={2} md={4.8} sx={{color:'white'}}>
-        To
+        <Grid  xs={12} sm={2} md={2.5} sx={{color:'white'}}>
+        From
         </Grid>
-        <Grid  xs={12} sm={2} md={1} sx={{color:'white'}}>
+        <Grid  xs={12} sm={2} md={2.5} sx={{color:'white'}}>
         Attachment
         </Grid>
-        <Grid  xs={12} sm={2} md={2.5} sx={{color:'white', }}>
+        <Grid  xs={12} sm={2} md={2} sx={{color:'white', }}>
         Date
         </Grid>
       </Grid>
