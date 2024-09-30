c65e681bbb7ad348ecf1a16309e7656b73b85263
Akankshakurkute <akankshakurkute392@gmail.com>
2024-09-30 11:32:00 +0530

    View message - Help icon change message

--- src/libraries/card/card7.tsx
+++ src/libraries/card/card7.tsx
@@ -172,7 +172,7 @@ function Card7({
           > */}
 
                 <Box>
-                  <Tooltip title={`View sent/received messages.`}>
+                  <Tooltip title={`View detailed message received to you. To reply the sender click on "Reply" and to sent reply to all the receipients click on "Reply To All".`}>
                     <IconButton
                       sx={{
                         color: 'white',
