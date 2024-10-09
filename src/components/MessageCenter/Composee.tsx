960cd7aadac159ec4d985e02a0e8cdbf62ea38ab
Akankshakurkute <akankshakurkute392@gmail.com>
2024-10-09 15:31:40 +0530

    tooltip title postion change

--- src/components/MessageCenter/Composee.tsx
+++ src/components/MessageCenter/Composee.tsx
@@ -1265,7 +1265,8 @@ function Form13() {
         }}
       >
         <DialogTitle sx={{ bgcolor: '#223354' }}>
-          <Tooltip title={'Select name of the teacher / student / admin staff / other staff and click on "Back to compose / Confirm'}>
+          <Tooltip title={'Select name of the teacher / student / admin staff / other staff and click on "Back to compose / Confirm'}
+            placement="bottom-end"  >
             <QuestionMark
               sx={{
                 color: 'white',
