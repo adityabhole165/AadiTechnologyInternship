6cc64cb99ea7b7cd82e3b8e89bf809a0a5ed020a
Ajit Sagale <ajitsagale@gmail.com>
2024-10-13 23:33:55 +0530

    maintain card notice

--- src/libraries/card/CardNotice.tsx
+++ src/libraries/card/CardNotice.tsx
@@ -1,51 +1,95 @@
-import DeleteIcon from '@mui/icons-material/Delete';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-import { Grid, Typography } from '@mui/material';
+import { Box, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { ListStyle } from '../styled/CardStyle';
-const CardNotice = ({ item, downloadNotice, clickSingle }) => {
-  const navigate = useNavigate();
+import ChechBoX from './CheckBoX';
+
+const CardNotice = ({ itemList, downloadNotice, clickSingle }) => {
   const [isCardVisible, setIsCardVisible] = useState(true);
   const handleCheckboxChange = () => {
     setIsCardVisible(!isCardVisible);
   };
-  const clickDelete = (value) => {
-    clickSingle({ name: name, value: value, checked: !item.isActive })
-  }
-  const clickCard = (path) => {
-    navigate('../' + path.replace('/Common/', ''))
 
-  }
   return (
     <>
-      <Grid item container xs={12}>
-        <Grid item xs={10.8}>
-
-          {/* <Card sx={{ display: 'flex', alignItems: 'center', p: 0.5, mt: 0.7 }}> */}
-          <ListStyle color={item.IsImageNotice ? 'secondary' : 'primary'}
-            sx={{ display: 'flex', alignItems: 'center' }}>
-            <Typography sx={{ textDecoration: item.isActive ? "line-through" : "" }}
-              onClick={() => { clickCard(item.linkPath) }}
-            >{item.header}</Typography>
+      {/* <Grid container>
+      
+        <Grid xs={12}>
+          <Card sx={{ display: 'flex', alignItems: 'center', p: 0.5 }}>
+          <Typography>{item.header}</Typography>
             <div style={{ flex: '1' }}></div>
-            {item.FileName != "" ?
-              <FileDownloadOutlinedIcon onClick={() => { downloadNotice(item.FileName, item.IsImageNotice) }} /> :
-              <FileDownloadOutlinedIcon sx={{ color: 'white' }} />
-
-            }
-          </ListStyle>
-          {/* </Card> */}
-        </Grid>
-        {!item.IsImageNotice &&
-          <Grid item xs={1} sx={{ mt: "10px", ml: "5px" }}
-          >
-            <DeleteIcon onClick={() => clickDelete(item.id)}
-              sx={{ color: !item.isActive ? 'red' : 'grey' }}
+            <FileDownloadOutlinedIcon
+              onClick={() => {
+                downloadNotice(item.FileName, item.IsImageNotice);
+              }}
             />
-          </Grid>
-        }
-      </Grid>
+          </Card>
+        </Grid>
+        <Grid xs={1} sx={{ mt: '10px', ml: '5px' }}>
+          <ChechBoX
+            name={''}
+            value={item.id}
+            checked={item.isActive}
+            onChange={clickSingle}
+          />
+        </Grid>
+      </Grid> */}
+
+      <Box sx={{ backgroundColor: 'white', mb: 2 }}>
+        <TableContainer component={Box} >
+          <Table aria-label="simple table" sx={{ py: 1, border: (theme) => `1px solid ${theme.palette.grey[300]}`, overflow: 'hidden' }}>
+            <TableHead>
+              <TableRow sx={{ background: (theme) => theme.palette.secondary.main, py: 1 }}>
+                <TableCell sx={{ color: 'white', width: '480px' }}>
+                  Checkbox
+                </TableCell>
+                <TableCell sx={{ color: 'white', }} >
+                  Notice Name
+                </TableCell>
+                <TableCell sx={{ color: 'white', textAlign: 'center', py: 1 }}>
+                  Download
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {itemList.map((item, index) => {
+                return (
+                  <>
+                    <TableRow
+                    // onClick={() => { clickCard(item.linkPath) }}
+                    >
+                      <TableCell sx={{ textTransform: 'capitalize', py: 0.5, pl: 3.5 }}>
+                        <ChechBoX
+                          name={''}
+                          value={item.id}
+                          checked={item.isActive}
+                          onChange={clickSingle}
+                        />
+                      </TableCell>
+                      <TableCell sx={{ textTransform: 'capitalize', py: 0.5, }}>
+                        {item.header}
+                      </TableCell>
+                      {/* <TableCell sx={{ textTransform: 'capitalize', py: 0.5, textAlign: 'center', minWidth:'120px'}}>
+                      <IconButton>
+                      <FileDownloadOutlinedIcon onClick={() => { downloadNotice(item.FileName, item.IsImageNotice) }} />
+                      </IconButton></TableCell> */}
+                      <TableCell sx={{ textTransform: 'capitalize', py: 0.5, textAlign: 'center', minWidth: '170px' }}>
+                        {item.FileName ? (
+                          <IconButton onClick={() => downloadNotice(item.FileName, item.IsImageNotice)}>
+                            <FileDownloadOutlinedIcon />
+                          </IconButton>
+                        ) : (
+                          <span>-</span>
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  </>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+      </Box>
     </>
   );
 };
