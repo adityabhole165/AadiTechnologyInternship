829b69dc6e8065cda570078a3c268fd526fe1866
sakshi2561 <Sakshinanavare02019@gmail.com>
2024-10-15 16:17:21 +0530

    weekly attendance defect fix

--- src/librariesWeb/PieChart.tsx
+++ src/librariesWeb/PieChart.tsx
@@ -1,135 +1,3 @@
-// import { Box, Grid } from '@mui/material';
-// import { useEffect, useState } from 'react';
-// import ApexCharts from 'react-apexcharts';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { IWeeklyAttendanceBody } from 'src/interfaces/Student/dashboard';
-// import { CDAgetWeeklyAttendance } from 'src/requests/Dashboard/Dashboard';
-// import { RootState } from 'src/store';
-// import Header from './Header';
-
-// const PieChart = () => {
-//     const dispatch = useDispatch();
-//     const asAcademicYearId = Number(sessionStorage.getItem('AcademicYearId'));
-//     const asSchoolId = Number(localStorage.getItem('localSchoolId'));
-//     const UserId = Number(sessionStorage.getItem('Id'));
-//     const asStandardDivisionId = Number(
-//         sessionStorage.getItem('StandardDivisionId')
-//     );
-//     const filteredAttendance: any = useSelector((state: RootState) => state.Dashboard.ISWeeklyAttendanceCount);
-//     const statusDescriptions: any = useSelector((state: RootState) => state.Dashboard.ISlistAttendanceCalender);
-//     const GetDayDates: any = useSelector((state: RootState) => state.Dashboard.ISGetDayDates);
-
-//     const firstGirlsPercentage = parseFloat(filteredAttendance.map((item: any) => item.TotalGirlsPercentage)[0]) || 0;
-//     const firstBoysPercentage = parseFloat(filteredAttendance.map((item: any) => item.TotalBoysPercentage)[0]) || 0;
-
-//       const isWeekend = (statusDesc) => statusDesc === 'Weekend'; 
-//       const colors = statusDescriptions.map((item) => (
-//         isWeekend(item.StatusDesc) ? 'red' : ''
-//       ));
-
-
-
-//     const options1 = {
-//         chart: {
-//             id: "basic-bar444",
-//         },
-//         series: [firstGirlsPercentage ,firstBoysPercentage ],
-//         labels: ["Girl", "Boys"],
-//     };
-//     const series = [firstGirlsPercentage,firstBoysPercentage];
-//     // Define options with correct types
-//     // const options2: ApexOptions = {
-//     //     chart: {
-//     //          id: "basic-bar444", // Ensure the type is 'pie'
-//     //     },
-//     //     labels: ['Group A', 'Group B', 'Group C', 'Group D'],
-//     //     responsive: [
-//     //         {
-//     //             breakpoint: 480,
-//     //             options: {
-//     //                 chart: {
-//     //                     width: 320,
-//     //                 },
-//     //                 legend: {
-//     //                     position: 'bottom',
-//     //                 },
-//     //             },
-//     //         },
-//     //     ],
-//     // };
-
-//     // const series = [400, 300, 300, 200];
-//     const [state, setState] = useState({
-//         colors: colors,
-//         options: {
-//             chart: {
-//                 id: "basic-bar444",
-//             },
-//             xaxis: {
-//                 categories: statusDescriptions.map((item) => item.DayName) ,// Set categories for the x-axis
-//                 labels: {
-//                   style: {
-//                     colors: colors, // Apply the colors to the x-axis labels
-//                     fontSize: '14px'
-//                   }
-//                 }
-//               }
-//         },
-//         series: [
-//             {
-//                 name: "Absent Student",
-//                 data: filteredAttendance.map(item => item.TotalAbsentPercentage)
-//             },
-//             {
-//                 name: "Present Student",
-
-//                 data: filteredAttendance.map(item => item.TotalPresentPercentage)
-
-//             },
-//         ],
-//     });
-
-//     const WeeklyAttendanceBody: IWeeklyAttendanceBody = {
-//         asSchoolId: asSchoolId,
-//         asAcademicYearId: asAcademicYearId,
-//         asStandardDivisionId: asStandardDivisionId,
-//     };
-
-//     useEffect(() => {
-//         dispatch(CDAgetWeeklyAttendance(WeeklyAttendanceBody));
-//     }, []);
-//     return (
-//         <Box sx={{ backgroundColor: 'white', p: 1 }} >
-//             <Grid item sx={{ overflow: 'auto', display: 'flex', borderRadius: '10px' }}>
-//                 <Header Title="Weekly Attendance" />
-//             </Grid>
-//             <Grid container>
-//                 <Grid item xs={4} mt={5}>
-//                     <ApexCharts
-//                         options={options1}
-//                         series={series}
-//                         type="donut"
-//                         width="100%"
-//                         height="295"
-//                     />
-//                 </Grid>
-//                 <Grid item xs={8} sx={{ height: '320px', overflow: 'auto', mt: 1, }}>
-//                     <ApexCharts
-//                         options={state.options}
-//                         series={state.series}
-//                         type="bar"
-//                         width="95%"
-//                         height="295"
-//                     />
-//                 </Grid>
-
-//             </Grid>
-//         </Box>
-//     );
-// };
-
-// export default PieChart;
-
 import { Box, Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
 import ApexCharts from 'react-apexcharts';
@@ -151,17 +19,50 @@ const PieChart = () => {
 
 
     const validAttendance = filteredAttendance.filter((item: any) => Object.keys(item).length > 0);
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString('en-GB').split('/').join('-');
+    // const TotalBoysPresentPercentage = parseFloat(validAttendance.map((item: any) =>item.TotalBoysPresentPercentage)[0]) || 0;
+    // const TotalGirlsPresentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalGirlsPresentPercentage)[0]) || 0;
+    // const TotalBoysAbsentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalBoysAbsentPercentage)[0]) || 0;
+    // const TotalGirlsAbsentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalGirlsAbsentPercentage)[0]) || 0;
 
+    const TotalBoysPresentPercentage = parseFloat(
+        validAttendance
+            .filter((item: any) => item.Attendance_Date === formattedDate)
+            .map((item: any) => item.TotalBoysPresentPercentage)[0]
+    ) || 0;
 
-    const TotalBoysPresentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalBoysPresentPercentage)[0]) || 0;
-    const TotalGirlsPresentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalGirlsPresentPercentage)[0]) || 0;
-    const TotalBoysAbsentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalBoysAbsentPercentage)[0]) || 0;
-    const TotalGirlsAbsentPercentage = parseFloat(validAttendance.map((item: any) => item.TotalGirlsAbsentPercentage)[0]) || 0;
+    const TotalGirlsPresentPercentage = parseFloat(validAttendance
+        .filter((item: any) => item.Attendance_Date === formattedDate)
+        .map((item: any) => item.TotalGirlsPresentPercentage)[0]
+    ) || 0;
 
+    const TotalBoysAbsentPercentage = parseFloat(validAttendance
+        .filter((item: any) => item.Attendance_Date === formattedDate)
+        .map((item: any) => item.TotalBoysAbsentPercentage)[0]
+    ) || 0;
 
+    const TotalGirlsAbsentPercentage = parseFloat(validAttendance
+        .filter((item: any) => item.Attendance_Date === formattedDate)
+        .map((item: any) => item.TotalGirlsAbsentPercentage)[0]
+    ) || 0;
+    
     const isWeekend = (statusDesc: string) => statusDesc === 'Weekend';
-    const colors = statusDescriptions.map((item) => (isWeekend(item.StatusDesc) ? 'red' : ''));
-    const attendanceNotTaken = validAttendance.length === 0;
+    const isHoliday = (statusDesc: string) => statusDesc === 'Holiday';
+    const isNotDone = (statusDesc: string) => statusDesc === 'Not Done';
+    // const colors = statusDescriptions.map((item) => (isWeekend(item.StatusDesc) ? 'red' : ''));
+    const colors = statusDescriptions.map((item) => {
+        if (isWeekend(item.StatusDesc)) {
+            return 'red';
+        } else if (isHoliday(item.StatusDesc)) {
+            return 'brown';
+        } else if (isNotDone(item.StatusDesc)) {
+            return '#a9a9a9';
+        } else {
+            return '#007305';
+        }
+    });
+    const attendanceNotTaken = validAttendance.length == 0;
 
     const options1 = {
         chart: {
