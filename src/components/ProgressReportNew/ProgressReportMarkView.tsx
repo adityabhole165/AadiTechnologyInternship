403ee7456b008029673d22f86a67cb8431c59482
AmanKadam-16 <aman16112002kadam@gmail.com>
2024-10-19 21:33:52 +0530

    Fix: Progress Report | Column Push

--- src/components/ProgressReportNew/ProgressReportMarkView.tsx
+++ src/components/ProgressReportNew/ProgressReportMarkView.tsx
@@ -1,128 +1,147 @@
 import { Box, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 
 const ProgressReportMarkView = ({ HeaderArray, SubHeaderArray, MarkDetailsList, ListDisplayNameDetails, USListSchoolWiseTestNameDetail, IsTotalConsiderForProgressReport, USListMarkssDetails }) => {
-    const getListDisplayName = (ShortName) => {
-        let returnVal = ""
-        ListDisplayNameDetails.map((Item) => {
-            if (Item.ShortName == ShortName)
-                returnVal = Item.DisplayName
-        })
-        return returnVal
 
-    }
-    let HeaderParent = []
-    let PrevParentId = "0", SubjectName = ""
-    let HeaderCount = 0
-    HeaderArray.map((item) => {
-        // if (item.ParentSubjectId != "0") {
-        if (item.ParentSubjectId != PrevParentId) {
+    const getListDisplayName = (ShortName) => {
+        let returnVal = "";
+        ListDisplayNameDetails.map((Item) => {
+            if (Item.ShortName === ShortName)
+                returnVal = Item.DisplayName;
+        });
+        return returnVal;
+    };
+
+    let HeaderParent = [];
+    let PrevParentId = "0", SubjectName = "";
+    let HeaderCount = 0;
+
+    HeaderArray.forEach((item) => {
+        if (item.ParentSubjectId !== PrevParentId) {
             HeaderParent.push({
                 SubjectName: SubjectName,
                 colSpan: HeaderCount
-            })
-            SubjectName = item.ParentSubjectName
-            PrevParentId = item.ParentSubjectId
-            HeaderCount = 0
-            // }
+            });
+            SubjectName = item.ParentSubjectName;
+            PrevParentId = item.ParentSubjectId;
+            HeaderCount = 0;
         }
-        SubjectName = item.ParentSubjectName
-        HeaderCount = HeaderCount + item.colSpan
-    })
+        SubjectName = item.ParentSubjectName;
+        HeaderCount = HeaderCount + item.colSpan;
+    });
+
     HeaderParent.push({
         SubjectName: SubjectName,
         colSpan: HeaderCount
-    })
+    });
+
+    // Track if there is any parent subject
+    const hasParentSubjects = HeaderArray.some(item => item.ParentSubjectId !== "0");
+    function showParentColumns() {
+
+    }
+    console.log('✨ HeaderParent', HeaderParent);
 
     return (
         <Box>
             <Table>
                 <TableHead>
-                    {HeaderParent.length > 1 &&
-                        (<>
+                    {HeaderParent.length > 1 && (
+                        <>
                             <TableRow sx={{ bgcolor: '#F0F0F0', textAlign: 'center' }}>
                                 <TableCell rowSpan={2}>
-                                    <Typography variant={"h3"} textAlign={'left'} color={"black"} ml={5} >
+                                    <Typography variant={"h3"} textAlign={'left'} color={"black"} ml={5}>
                                         Subjects &#9654;
                                     </Typography>
                                     <Typography variant={"h3"} textAlign={'left'} color={"black"}>
                                         &#9660; Exam
-                                    </Typography></TableCell>
+                                    </Typography>
+                                </TableCell>
 
-                                {HeaderParent.map((item) => (
-                                    <TableCell colSpan={item.colSpan} sx={{ border: '1px solid black', textAlign: 'center' }}>
-                                        <Typography color="black" textAlign={'left'} mr={5}  >
+                                {HeaderParent.map((item, index) => (
+                                    <TableCell key={index} colSpan={item.SubjectName !== '' ? item.colSpan + 4 : item.colSpan} sx={{ border: '1px solid black', textAlign: 'center' }}>
+                                        <Typography color="black" textAlign={'left'} mr={5}>
                                             <b style={{ marginRight: "5px" }}>{item.SubjectName}</b>
-                                        </Typography></TableCell>
-                                ))
-                                }
+                                        </Typography>
+                                    </TableCell>
+                                ))}
                             </TableRow>
+
                             <TableRow sx={{ bgcolor: '#F0F0F0', textAlign: 'center' }}>
-                                {HeaderArray.map((item) => (
-                                    <TableCell colSpan={item.colSpan} sx={{ border: '1px solid black', textAlign: 'center' }}>
-                                        <Typography color="black" textAlign={'left'} mr={5}  >
-                                            <b style={{ marginRight: "5px" }}>{item.SubjectName}</b>
-                                        </Typography></TableCell>
-                                ))
-                                }
+                                {HeaderArray.map((item, index) => (
+                                    <>
+                                        {index > 0 && HeaderArray[index - 1].ParentSubjectId !== "0" && item.ParentSubjectId === '0' && (
+                                            <>
+                                                <TableCell rowSpan={2} >Total Theory</TableCell>
+                                                <TableCell rowSpan={2} >Total M1</TableCell>
+                                                <TableCell rowSpan={2} >Total M2</TableCell>
+                                                <TableCell rowSpan={2} >Total</TableCell>
+                                            </>
+                                        )}
+                                        <TableCell key={index} colSpan={item.colSpan} sx={{ border: '1px solid black', textAlign: 'center' }}>
+                                            <Typography color="black" textAlign={'left'} mr={5}>
+                                                <b style={{ marginRight: "5px" }}>{item.SubjectName}</b>
+                                            </Typography>
+                                        </TableCell>
+
+                                        {/* Check if the previous item has a parent and the current item doesn't */}
+
+                                    </>
+                                ))}
                             </TableRow>
-                        </>)}
-                    {HeaderParent.length <= 1 &&
-                        (<TableRow sx={{ bgcolor: '#F0F0F0', textAlign: 'center' }}>
+
+                        </>
+                    )}
+
+                    {HeaderParent.length <= 1 && (
+                        <TableRow sx={{ bgcolor: '#F0F0F0', textAlign: 'center' }}>
                             <TableCell rowSpan={2}>
-                                <Typography variant={"h3"} textAlign={'left'} color={"black"} ml={5} >
+                                <Typography variant={"h3"} textAlign={'left'} color={"black"} ml={5}>
                                     Subjects &#9654;
                                 </Typography>
                                 <Typography variant={"h3"} textAlign={'left'} color={"black"}>
                                     &#9660; Exam
-                                </Typography></TableCell>
-                            {HeaderArray.map((item) => (
-                                <TableCell colSpan={item.colSpan} sx={{ border: '1px solid black', textAlign: 'center' }}>
-                                    <Typography color="black" textAlign={'left'} mr={5}  >
+                                </Typography>
+                            </TableCell>
+                            {HeaderArray.map((item, index) => (
+                                <TableCell key={index} colSpan={item.colSpan} sx={{ border: '1px solid black', textAlign: 'center' }}>
+                                    <Typography color="black" textAlign={'left'} mr={5}>
                                         <b style={{ marginRight: "5px" }}>{item.SubjectName}</b>
-                                    </Typography></TableCell>
+                                    </Typography>
+                                </TableCell>
                             ))}
                         </TableRow>
-                        )}
+                    )}
+
                     <TableRow>
-                        <TableCell ></TableCell >
-                        {SubHeaderArray.map((item) => (
-                            <><TableCell >
-                                <Typography color="#38548A" textAlign={'center'} mr={9}  >
+                        <TableCell></TableCell>
+                        {SubHeaderArray.map((item, index) => (
+                            <TableCell key={index}>
+                                <Typography color="#38548A" textAlign={'center'} mr={9}>
                                     <b style={{ marginRight: "5px" }}>{item.TestTypeName}</b>
                                 </Typography>
                             </TableCell>
-                            </>
                         ))}
                     </TableRow>
                 </TableHead>
+
                 {MarkDetailsList.map((testItem, i) => (
                     <TableBody key={i} sx={{ backgroundColor: '#F0F0F0', alignItems: 'center' }}>
                         <TableRow>
-                            <TableCell sx={{}}>
-                                <b> {testItem.TestName}</b>
+                            <TableCell>
+                                <b>{testItem.TestName}</b>
                             </TableCell>
 
-                            {testItem.MarksArr.map((MarkItem) => (<>
-                                <TableCell sx={{ backgroundColor: 'white' }}>
-                                    {MarkItem?.MarksScored +
-                                        (MarkItem?.TotalMarks == "-" ? "" :
-                                            (" / " + MarkItem?.TotalMarks))}
+                            {testItem.MarksArr.map((MarkItem, index) => (
+                                <TableCell key={index} sx={{ backgroundColor: 'white' }}>
+                                    {MarkItem?.MarksScored + (MarkItem?.TotalMarks === "-" ? "" : (" / " + MarkItem?.TotalMarks))}
                                 </TableCell>
-
-                            </>))}
-
-
-                            {/* <TableCell sx={{ backgroundColor: 'white' }}>
-                  Total
-                </TableCell> */}
-
+                            ))}
                         </TableRow>
                     </TableBody>
                 ))}
-
             </Table>
         </Box>
-    )
-}
+    );
+};
 
-export default ProgressReportMarkView
+export default ProgressReportMarkView;
