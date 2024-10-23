0ec8e9eb20dbaf89f98684224620c0a28afa46e9
Harshwardhanchougule <harshchougule25@gmail.com>
2024-10-23 14:33:36 +0530

    Student page profile create

---
+++ src/components/Students/StudentProfileHeader.tsx
@@ -0,0 +1,36 @@
+import { Avatar, Grid, Typography } from '@mui/material';
+import React from 'react';
+
+const StudentProfileHeader: React.FC = () => {
+    return (
+        <Grid container spacing={2} style={{ padding: '1px' }}>
+            {/* Left Side - Student Photo, Name, Class */}
+            <Grid item xs={6} container alignItems="center">
+                <Avatar
+                    alt="Student Photo"
+                    src="/path-to-student-photo.jpg" // Replace with actual photo URL
+                    sx={{ width: 80, height: 80, marginRight: '16px' }}
+                />
+                <div>
+                    <Typography variant="h6">Aadi Tech</Typography>
+                    <Typography variant="subtitle1">Class: 5th Grade</Typography>
+                </div>
+            </Grid>
+
+            {/* Right Side - Confirmation and Update Info */}
+            <Grid item xs={6} container direction="column" justifyContent="flex-start" alignItems="flex-end">
+                <Typography variant="h5" >
+                    <b> Confirmed by:</b> Ms. Pallavi A. Shilimkar on 16 Jan 2019 10:30 AM
+                </Typography>
+                <Typography variant="h5" >
+                    <b> Confirmed by:</b> Ms. Manisha J. Mohite on 27 May 2024 11:45 AM
+                </Typography>
+                <Typography variant="h5" >
+                    <b>Admitted In: </b> Nursery
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default StudentProfileHeader;
