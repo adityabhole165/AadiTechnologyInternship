fb06c9d13413a6fc25e5ac346ee135ad1238035f
Pamyamane <pramodmane043@gmail.com>
2024-10-25 17:27:19 +0530

    StudentwiseprogressreportEdit

--- src/components/StudentWiseProgressReport/StudentwiseprogressreportEdit.tsx
+++ src/components/StudentWiseProgressReport/StudentwiseprogressreportEdit.tsx
@@ -20,6 +20,7 @@ import { TeacherXseedSubjects } from 'src/requests/PrePrimaryResult/RequestPrePr
 import { getUserDetailss } from 'src/requests/SchoolSetting/schoolSetting';
 import { CDAAssessmentDropdown } from 'src/requests/StudentWiseProgressReport/ReqStudentWiseProgressReport';
 import { RootState } from 'src/store';
+import { ResizableTextField } from '../AddSchoolNitice/ResizableDescriptionBox';
 import CommonPageHeader from '../CommonPageHeader';
 const StudentwiseprogressreportEdit = () => {
     const dispatch = useDispatch();
@@ -475,7 +476,7 @@ const StudentwiseprogressreportEdit = () => {
                             sx={{ minWidth: '15vw' }}
                             ItemList={USAssessmentDrop}
                             onChange={clickAssessmentId}
-                            label={'Assessment'}
+                            label={'Assessment:'}
                             defaultValue={AssessmentId}
                             size={"small"}
                         />
@@ -605,7 +606,7 @@ const StudentwiseprogressreportEdit = () => {
                         {USFillStudentDetails.map((detail) => (
                             <TableRow sx={{ bgcolor: '#38548A' }}>
                                 <TableCell sx={{ textAlign: 'center', color: 'white' }}><b>Roll No :  {detail.RollNo} </b></TableCell>
-                                <TableCell sx={{ textAlign: 'center', color: 'white' }}><b>Name :  {detail.StudentName} </b></TableCell>
+                                <TableCell sx={{ textAlign: 'center', color: 'white' }}><b>Name:  {detail.StudentName} </b></TableCell>
                                 <TableCell sx={{ textAlign: 'center', color: 'white' }}><b>Class :  {detail.Class} </b></TableCell>
                                 <TableCell sx={{ textAlign: 'center', color: 'white' }}><b>Year :  {detail.AcademicYear} </b></TableCell>
                                 <TableCell sx={{ textAlign: 'center', color: 'white' }}><b>Assessment : {detail.Assessment} </b></TableCell>
@@ -741,74 +742,101 @@ const StudentwiseprogressreportEdit = () => {
                     </TableContainer>
                 </>
                 {
-                    USFillNonXseedSubjectGrades.length > 0 ? (
-                        <div>
-                            <Typography variant={"h4"} textAlign={'left'} color={"#38548a"} mt={2}>
-                                Co-Curricular Subjects
-                            </Typography>
-                            <TableContainer component={Box}>
-                                <Table aria-label="simple table" sx={{ border: '1px solid lightgrey' }}>
-                                    <TableHead>
-                                        <TableRow sx={{ background: (theme) => theme.palette.secondary.main, color: (theme) => theme.palette.common.white }}>
-                                            <TableCell sx={{
-                                                textTransform: 'capitalize', color: (theme) => theme.palette.common.white,
-                                                py: 1
-                                            }}>Subject</TableCell>
+                    USFillNonXseedSubjectGrades.length > 0 ? <div>
+                        <Typography variant={"h4"} textAlign={'left'} color={"#38548a"} mt={2} >
+                            Co-CurricularSubjects
+                        </Typography>
+                        <TableContainer component={Box} >
+                            <Table aria-label="simple table" sx={{ border: '1px solid lightgrey' }}>
+                                <TableHead>
+                                    <TableRow sx={{ background: (theme) => theme.palette.secondary.main, color: (theme) => theme.palette.common.white }}>
+                                        <TableCell sx={{
+                                            textTransform: 'capitalize', color: (theme) => theme.palette.common.white,
+                                            py: 1
+                                        }}>Subject</TableCell>
 
-                                            <TableCell sx={{
-                                                textTransform: 'capitalize', backgroundColor: (theme) => theme.palette.secondary.main,
-                                                color: 'white', pt: '10px', pb: '10px'
-                                            }}>
+
+
+
+
+                                        <TableCell sx={{ textTransform: 'capitalize', backgroundColor: (theme) => theme.palette.secondary.main, color: 'white', pt: '10px', pb: '10px' }}>
+                                            <SearchableDropdown
+                                                ItemList={USFillGradeDetails}
+                                                defaultValue={headerGrade1}
+                                                label={''}
+                                                sx={{ maxWidth: '20vw', backgroundColor: 'white', marginLeft: '5px' }}
+                                                size={"small"}
+                                                DisableClearable={true}
+                                                onChange={clickHeaderGrade1}
+                                                mandatory
+                                                disabled={(StudentWiseAssessmentPublishStatus == "Y" && AssessmentPublishStatus == "N") || (IsPublished == 'Y' && PublishStatus == "Y")}
+
+                                            />
+                                        </TableCell>
+
+
+
+                                        {USFillNonXseedSubjectGrades.map((row, index) => (
+                                            row.Observation !== "" ? (
+                                                <TableCell
+                                                    key={index}
+                                                    sx={{
+                                                        textTransform: 'capitalize',
+                                                        color: (theme) => theme.palette.common.white,
+                                                        py: 1
+                                                    }}
+                                                >
+                                                    Facilitator's Observation
+                                                </TableCell>
+                                            ) : null
+                                        ))}
+
+
+
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {USFillNonXseedSubjectGrades.map((row) => (
+                                        <TableRow key={row.YearwiseStudentId}>
+                                            <TableCell sx={{ py: 1 }}>{row.SubjectName}</TableCell>
+                                            <TableCell key={row.GradeId}>
                                                 <SearchableDropdown
+                                                    key={row.GradeId}
                                                     ItemList={USFillGradeDetails}
-                                                    defaultValue={headerGrade1}
+                                                    defaultValue={grades1[row.GradeId]}
                                                     label={''}
-                                                    sx={{ maxWidth: '20vw', backgroundColor: 'white', marginLeft: '5px' }}
+                                                    sx={{ width: '20vw', backgroundColor: 'white' }}
                                                     size={"small"}
                                                     DisableClearable={true}
-                                                    onChange={clickHeaderGrade1}
+                                                    onChange={(value) => clickGrade1(value, row.GradeId)}
                                                     mandatory
-                                                    disabled={(StudentWiseAssessmentPublishStatus === "Y" && AssessmentPublishStatus === "N") || (IsPublished === 'Y' && PublishStatus === "Y")}
+                                                    disabled={(StudentWiseAssessmentPublishStatus == "Y" && AssessmentPublishStatus == "N") || (IsPublished == 'Y' && PublishStatus == "Y")}
+
                                                 />
                                             </TableCell>
 
-                                            {/* Conditionally render the "Facilitator's Observation" header based on asSchoolId */}
-                                            {asSchoolId !== 18 && (
-                                                <TableCell sx={{
-                                                    textTransform: 'capitalize', color: (theme) => theme.palette.common.white,
-                                                    py: 1
-                                                }}>Facilitator's Observation</TableCell>
-                                            )}
+                                            <TableCell sx={{
+                                                textTransform: 'capitalize', color: (theme) => theme.palette.common.white,
+                                                py: 1
+                                            }}>
+                                                <ResizableTextField
+                                                    rows={3}
+                                                    value={row.Observation}
+
+                                                    fullWidth
+                                                    sx={{
+                                                        resize: 'both',
+                                                        width: '100%',
+                                                    }}
+                                                />
+
+                                            </TableCell>
                                         </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {USFillNonXseedSubjectGrades.map((row) => (
-                                            <TableRow key={row.YearwiseStudentId}>
-                                                <TableCell sx={{ py: 1 }}>{row.SubjectName}</TableCell>
-                                                <TableCell key={row.GradeId}>
-                                                    <SearchableDropdown
-                                                        ItemList={USFillGradeDetails}
-                                                        defaultValue={grades1[row.GradeId]}
-                                                        label={''}
-                                                        sx={{ width: '20vw', backgroundColor: 'white' }}
-                                                        size={"small"}
-                                                        DisableClearable={true}
-                                                        onChange={(value) => clickGrade1(value, row.GradeId)}
-                                                        mandatory
-                                                        disabled={(StudentWiseAssessmentPublishStatus === "Y" && AssessmentPublishStatus === "N") || (IsPublished === 'Y' && PublishStatus === "Y")}
-                                                    />
-                                                </TableCell>
-                                            
-                                                {asSchoolId !== 18 && (
-                                                    <TableCell sx={{ py: 1 }}>{row.Observation}</TableCell>
-                                                )}
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </div>
-                    ) : null
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div> : null
                 }
 
                 <br></br>
