72d1e94b90b213a3f93fabb0e9e8f6a8a7795352
Ajit Sagale <ajitsagale@gmail.com>
2024-10-30 17:00:00 +0530

    formating

--- src/api/ProgressReport/ApiProgressReport.ts
+++ src/api/ProgressReport/ApiProgressReport.ts
@@ -1,89 +1,88 @@
-import { IGetClassTeachersBody, IGetClassTeachersResult, IGetPassedAcademicYearsResult } from "src/interfaces/ProgressReport/IprogressReport";
+import { GetSchoolSettingsBody, GetSchoolSettingsResult, IGetAllMarksGradeConfigurationBody, IGetAllMarksGradeConfigurationResult, IGetClassTeachersBody, IGetClassTeachersResult, IGetPassedAcademicYearsBody, IGetPassedAcademicYearsResult, IGetStudentNameDropdownBody, IGetStudentNameDropdownResult, IsGradingStandarBody, IsTestPublishedForStdDivBody, IsTestPublishedForStudentBody, IStudentProgressReportBody, IStudentProgressReportResult } from "src/interfaces/ProgressReport/IprogressReport";
 import http from '../../requests/SchoolService/schoolServices';
-import { IGetStudentNameDropdownBody, IsTestPublishedForStudentBody,IGetStudentNameDropdownResult,GetSchoolSettingsResult,GetSchoolSettingsBody,IStudentProgressReportBody ,IsTestPublishedForStdDivBody,IsGradingStandarBody,IStudentProgressReportResult,IGetPassedAcademicYearsBody,IGetAllMarksGradeConfigurationResult,IGetAllMarksGradeConfigurationBody} from "src/interfaces/ProgressReport/IprogressReport";
 
-const GetClassTeachers = (data: IGetClassTeachersBody ) => {
-    return http.post<IGetClassTeachersResult[]>(
-      'Teacher/GetClassTeachers',
-      data
-    );
-  };
-
-
-  const GetStudentNameDropdown = (data: IGetStudentNameDropdownBody) => {
-    return http.post<IGetStudentNameDropdownResult[]>(
-      'Teacher/GetStudentNameDropdown',
-      data
-    );
-  };
-
-  const StudentProgressReport = (data: IStudentProgressReportBody) => {
-    return http.post<IStudentProgressReportResult>(
-      'Teacher/StudentProgressReport',
-      data
-    );
-  };
-
-  const GetPassedAcademicYears = (data: IGetPassedAcademicYearsBody) => {
-    return http.post<IGetPassedAcademicYearsResult[]>(
-      'Teacher/GetPassedAcademicYears',
-      data
-    );
-  };
+const GetClassTeachers = (data: IGetClassTeachersBody) => {
+  return http.post<IGetClassTeachersResult[]>(
+    'Teacher/GetClassTeachers',
+    data
+  );
+};
 
 
-  const GetAllMarksGradeConfiguration = (data: IGetAllMarksGradeConfigurationBody) => {
-    return http.post<IGetAllMarksGradeConfigurationResult>(
-      'Teacher/GetAllMarksGradeConfiguration',
-      data
-    );
-  };
+const GetStudentNameDropdown = (data: IGetStudentNameDropdownBody) => {
+  return http.post<IGetStudentNameDropdownResult[]>(
+    'Teacher/GetStudentNameDropdown',
+    data
+  );
+};
 
-  const IsGradingStandard = (data: IsGradingStandarBody) => {
-    return http.post(
-      'Teacher/IsGradingStandard',
-      data
-    );
-  };
-  const IsTestPublishedForStdDiv = (data: IsTestPublishedForStdDivBody) => {
-    return http.post(
-      'Teacher/IsTestPublishedForStdDiv',
-      data
-    );
-  };
+const StudentProgressReport = (data: IStudentProgressReportBody) => {
+  return http.post<IStudentProgressReportResult>(
+    'Teacher/StudentProgressReport',
+    data
+  );
+};
+
+const GetPassedAcademicYears = (data: IGetPassedAcademicYearsBody) => {
+  return http.post<IGetPassedAcademicYearsResult[]>(
+    'Teacher/GetPassedAcademicYears',
+    data
+  );
+};
 
 
+const GetAllMarksGradeConfiguration = (data: IGetAllMarksGradeConfigurationBody) => {
+  return http.post<IGetAllMarksGradeConfigurationResult>(
+    'Teacher/GetAllMarksGradeConfiguration',
+    data
+  );
+};
 
-  const IsTestPublishedForStudent = (data: IsTestPublishedForStudentBody) => {
-    return http.post(
-      'Teacher/IsTestPublishedForStudent',
-      data
-    );
-  };
+const IsGradingStandard = (data: IsGradingStandarBody) => {
+  return http.post(
+    'Teacher/IsGradingStandard',
+    data
+  );
+};
+const IsTestPublishedForStdDiv = (data: IsTestPublishedForStdDivBody) => {
+  return http.post(
+    'Teacher/IsTestPublishedForStdDiv',
+    data
+  );
+};
 
 
 
-  
-  
-  const GetSchoolSettings = (data: GetSchoolSettingsBody) => {
-    return http.post<GetSchoolSettingsResult[]>(
-      'School/GetSchoolSettings',
-      data
-    );
-  };
+const IsTestPublishedForStudent = (data: IsTestPublishedForStudentBody) => {
+  return http.post(
+    'Teacher/IsTestPublishedForStudent',
+    data
+  );
+};
 
-  
+
+
+
+
+const GetSchoolSettings = (data: GetSchoolSettingsBody) => {
+  return http.post<GetSchoolSettingsResult[]>(
+    'School/GetSchoolSettings',
+    data
+  );
+};
+
+
 
 const ApiProgressReport = {
-    GetClassTeachers,
-    GetStudentNameDropdown,
-    StudentProgressReport,
-    GetPassedAcademicYears,
-    GetAllMarksGradeConfiguration,
-    IsGradingStandard,
-    IsTestPublishedForStdDiv,
-    IsTestPublishedForStudent,
-    GetSchoolSettings
-   
-  };
-  export default ApiProgressReport;
+  GetClassTeachers,
+  GetStudentNameDropdown,
+  StudentProgressReport,
+  GetPassedAcademicYears,
+  GetAllMarksGradeConfiguration,
+  IsGradingStandard,
+  IsTestPublishedForStdDiv,
+  IsTestPublishedForStudent,
+  GetSchoolSettings
+
+};
+export default ApiProgressReport;
