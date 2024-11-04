68c793065b90dc2b6855129b9466e774a4484e90
sakshi2561 <Sakshinanavare02019@gmail.com>
2024-11-04 17:42:13 +0530

    staff birthday api binding

--- src/interfaces/Common/StaffBirthday.ts
+++ src/interfaces/Common/StaffBirthday.ts
@@ -1,7 +1,35 @@
+
+
+export default interface IstaffBirthdayBody {
+  pagesize: number;
+  asAcademicYearId: number;
+  asSchoolId: number;
+  startindex: number;
+}
+
+export interface getSchoolStaffBirthdayResult {
+  getSchoolStaffBirthdaylists: [
+    {
+      RowID: number;
+      TotalRows: number;
+      User_Role_Id: number;
+      Email_Address: string;
+      User_Id: number;
+      Mobile_Number: number;
+      StaffName: string;
+      Designation: string;
+      DOB: string;
+      Sort_Order: number;
+      School_Id: number;
+      BinaryPhotoImage: string;
+    }
+  ]
+}
+
 export default interface IstaffBirthday {
   asMonth: Number;
-  asAcademicyearId: string;
-  asSchoolId: string;
+  asAcademicyearId: number;
+  asSchoolId: number;
 }
 
 export interface GetstaffBirthdayList {
