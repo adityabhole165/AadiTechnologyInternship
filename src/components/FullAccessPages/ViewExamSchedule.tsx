53160217eda89bd7d62e3a4d1dd79894bb09691a
Harshwardhanchougule <harshchougule25@gmail.com>
2024-11-12 07:55:52 +0530

    Upgrade code View Exam Schedule

--- src/components/FullAccessPages/ViewExamSchedule.tsx
+++ src/components/FullAccessPages/ViewExamSchedule.tsx
@@ -1,21 +1,18 @@
-import { ExpandMore, QuestionMark } from '@mui/icons-material'
-import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from '@mui/material'
-import { grey } from '@mui/material/colors'
-import { useState } from 'react'
-import SearchableDropdown from 'src/libraries/ResuableComponents/SearchableDropdown'
-import CommonPageHeader from '../CommonPageHeader'
+import { QuestionMark } from '@mui/icons-material';
+import { Box, IconButton, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { useState } from 'react';
+import CommonPageHeader from '../CommonPageHeader';
 
 const ViewExamSchedule = () => {
-    const [expanded, setExpanded] = useState(false);
+    const [expandedCardIndex, setExpandedCardIndex] = useState(null);
     const [selectedGrade, setSelectedGrade] = useState('');
-
-    const handleAccordionChange = () => {
-        setExpanded(!expanded);
-    };
 
     const handleGradeChange = (event) => {
         setSelectedGrade(event.target.value);
+        setExpandedCardIndex(event.target.value ? 0 : null); // Set expandedCardIndex only if a grade is selected
     };
+
     const examData = [
         { date: '01 Jul 2024', subject: 'English', type: 'SEA I', start: '08:10 AM', end: '08:45 AM', total: '00:35', description: '' },
         { date: '03 Jul 2024', subject: 'Mathematics', type: 'SEA I', start: '08:10 AM', end: '08:45 AM', total: '00:35', description: '' },
@@ -25,6 +22,7 @@ const ViewExamSchedule = () => {
         { date: '09 Jul 2024', subject: 'Hindi II, Marathi II', type: 'SEA I', start: '08:10 AM', end: '08:45 AM', total: '00:35', description: '' },
         { date: '10 Jul 2024', subject: 'Hindi III, Marathi III', type: 'SEA I', start: '08:10 AM', end: '08:45 AM', total: '00:35', description: '' },
     ];
+
     const dummyData = [
         { Name: '-- Select --', value: '' },
         { Name: 'Nursery', value: 'nursery' },
@@ -41,8 +39,13 @@ const ViewExamSchedule = () => {
         { Name: '9', value: '9' },
         { Name: '10', value: '10' }
     ];
+
+    const toggleAccordion = () => {
+        setExpandedCardIndex(expandedCardIndex === 0 ? null : 0); // Toggle the index between 0 and null
+    };
+
     return (
-        <Box>
+        <Box px={2}>
             <CommonPageHeader
                 navLinks={[
                     {
@@ -56,18 +59,19 @@ const ViewExamSchedule = () => {
                 ]}
                 rightActions={
                     <>
-                       <Select
-                        value={selectedGrade}
-                        onChange={handleGradeChange}
-                        displayEmpty
-                        sx={{ minWidth: '15vw' }}
-                    >
-                        {dummyData.map((grade, index) => (
-                            <MenuItem key={index} value={grade.value}>
-                                {grade.Name}
-                            </MenuItem>
-                        ))}
-                    </Select>
+                        <Select
+                            size={'small'}
+                            value={selectedGrade}
+                            onChange={handleGradeChange}
+                            displayEmpty
+                            sx={{ minWidth: '15vw' }}
+                        >
+                            {dummyData.map((grade, index) => (
+                                <MenuItem key={index} value={grade.value}>
+                                    {grade.Name}
+                                </MenuItem>
+                            ))}
+                        </Select>
                         <Tooltip title={'Examination schedule for your class.'}>
                             <IconButton
                                 sx={{
@@ -84,79 +88,90 @@ const ViewExamSchedule = () => {
                     </>
                 }
             />
-            <Box p={2} sx={{backgroundColor:'white'}}>
-                {/* <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
-                    <Select
-                        value={selectedGrade}
-                        onChange={handleGradeChange}
-                        displayEmpty
-                        sx={{ minWidth: '15vw' }}
-                    >
-                        {dummyData.map((grade, index) => (
-                            <MenuItem key={index} value={grade.value}>
-                                {grade.Name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <Tooltip title={'Examination schedule for your class.'}>
-                        <IconButton
-                            sx={{
-                                color: 'white',
-                                backgroundColor: grey[500],
-                                '&:hover': {
-                                    backgroundColor: grey[600]
-                                }
-                            }}
-                        >
-                            <QuestionMark />
-                        </IconButton>
-                    </Tooltip>
-                </Box> */}
 
-                {selectedGrade && (
-                    <Accordion expanded={expanded} onChange={handleAccordionChange} TransitionProps={{ timeout: 2000 }}>
-                        <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel-content" id="panel-header">
-                            <Typography variant="h6">Subject Enrichment Analysis - I: 01 Jul 2024 To 10 Jul 2024</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <TableContainer component={Paper}>
-                                <Table>
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell><strong>Exam Date</strong></TableCell>
-                                            <TableCell><strong>Subject</strong></TableCell>
-                                            <TableCell><strong>Exam Type</strong></TableCell>
-                                            <TableCell><strong>Start Time</strong></TableCell>
-                                            <TableCell><strong>End Time</strong></TableCell>
-                                            <TableCell><strong>Total Time</strong></TableCell>
-                                            <TableCell><strong>Description</strong></TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {examData.map((exam, index) => (
-                                            <TableRow key={index}>
-                                                <TableCell>{exam.date}</TableCell>
-                                                <TableCell>{exam.subject}</TableCell>
-                                                <TableCell>{exam.type}</TableCell>
-                                                <TableCell>{exam.start}</TableCell>
-                                                <TableCell>{exam.end}</TableCell>
-                                                <TableCell>{exam.total}</TableCell>
-                                                <TableCell>{exam.description}</TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </AccordionDetails>
-                    </Accordion>
+            <Box p={2} sx={{ backgroundColor: 'white' }}>
+           
+                <Box
+                
+                    onClick={toggleAccordion}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        padding: 1,
+                        backgroundColor: 'whitesmoke',
+                        border: '1px solid #d3d3d3',
+                        borderRadius: '4px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    
+                    <Box sx={{ width: '10px', height: '10px', border: '1px solid black', mr: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <Typography sx={{ color: 'black', fontSize: '16px', fontWeight: 'bold' }}>
+                            {expandedCardIndex === 0 ? '-' : '+'}
+                        </Typography>
+                    </Box>
+                    <Typography variant="h6" sx={{ color: 'red', mr: 1 }}>
+                        <b>Progressive Analysis - I</b>
+                    </Typography>
+                    <Typography variant="h6">
+                        <b>01 July 2024 To 09 July 2024</b>
+                    </Typography>
+                </Box>
+
+                {expandedCardIndex === 0 ? (
+                    <TableContainer component={Box} sx={{ width: '100%', overflowX: 'auto', mt: 2 }}>
+                        <Table aria-label="simple table"
+                            sx={{
+                                border: (theme) => `1px solid ${theme.palette.grey[300]}`,
+                                overflow: 'hidden'
+                            }}>
+                            <TableHead>
+                                <TableRow sx={{
+                                    background: (theme) => theme.palette.secondary.main,
+                                    color: (theme) => theme.palette.common.white,
+                                }}>
+                                    <TableCell sx={{ color: 'white' }}><strong>Exam Date</strong></TableCell>
+                                    <TableCell sx={{ color: 'white' }}><strong>Subject</strong></TableCell>
+                                    <TableCell sx={{ color: 'white' }}><strong>Exam Type</strong></TableCell>
+                                    <TableCell sx={{ color: 'white' }}><strong>Start Time</strong></TableCell>
+                                    <TableCell sx={{ color: 'white' }}><strong>End Time</strong></TableCell>
+                                    <TableCell sx={{ color: 'white' }}><strong>Total Time</strong></TableCell>
+                                    <TableCell sx={{ color: 'white' }}><strong>Description</strong></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {examData.map((exam, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell sx={{ py: 1 }}>{exam.date}</TableCell>
+                                        <TableCell sx={{ py: 1 }}>{exam.subject}</TableCell>
+                                        <TableCell sx={{ py: 1 }}>{exam.type}</TableCell>
+                                        <TableCell sx={{ py: 1 }}>{exam.start}</TableCell>
+                                        <TableCell sx={{ py: 1 }}>{exam.end}</TableCell>
+                                        <TableCell sx={{ py: 1 }}>{exam.total}</TableCell>
+                                        <TableCell sx={{ py: 1 }}>{exam.description}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                ) : (
+                    <Typography
+                        variant="body1"
+                        sx={{
+                            textAlign: 'center',
+                            marginTop: 1,
+                            backgroundColor: '#324b84',
+                            padding: 1,
+                            borderRadius: 2,
+                            color: 'white'
+                        }}
+                    >
+                        <b>No record found.</b>
+                    </Typography>
                 )}
             </Box>
         </Box>
+    );
+};
 
-
-    )
-}
-
-export default ViewExamSchedule
-
-
+export default ViewExamSchedule;
