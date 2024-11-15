dc95b66cb9a1cc0e06d030b4cc17410d329decb1
Harshwardhanchougule <harshchougule25@gmail.com>
2024-11-15 11:31:03 +0530

    Message center Ui change

--- src/components/MessageCenter/MessageList.tsx
+++ src/components/MessageCenter/MessageList.tsx
@@ -742,7 +742,7 @@ const MessageList = () => {
         </Box> */}
         {/* </Hidden> */}
         <Grid container columnGap={1}>
-          <Grid item sm={1.7} xs={12} spacing={1}>
+          <Grid item sm={1.3} xs={12} spacing={1}>
             <Hidden smDown>
               <RouterLink
                 style={{ textDecoration: 'none', color: '#223354' }}
@@ -781,7 +781,7 @@ const MessageList = () => {
               )}
             </Box>
           </Grid>
-          <Grid container sm={10.3} spacing={1}>
+          <Grid container sm={10.7} spacing={1}>
             {((showSearch && isMobile) || !isMobile) && (
               <>
                 <Grid item xs={12} sm={12} md={12}>
