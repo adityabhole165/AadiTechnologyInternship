2c6323831bad6206ece73e9dc51a2da8941bb094
sakshi2561 <Sakshinanavare02019@gmail.com>
2024-11-15 20:55:14 +0530

    color configuration for legends : Fix

--- src/requests/TExamschedule/TExamschedule.ts
+++ src/requests/TExamschedule/TExamschedule.ts
@@ -202,7 +202,7 @@ export const RExamSchedule =
             Array.push({
               Id,
               Name: "N/A",
-              Background: "Yellow",
+              Background: "#F0F0F0",
               IsLink: false,
               fontWeight: '',
               IsConfigured: false,
@@ -217,7 +217,7 @@ export const RExamSchedule =
               Array.push({
                 Id,
                 Name: "Not Configured",
-                Background: "DarkGreen",
+                Background: "#4CAF50",
                 IsLink: true,
                 fontWeight: 'bold',
                 IsConfigured: false,
@@ -230,7 +230,7 @@ export const RExamSchedule =
               Array.push({
                 Id,
                 Name: `${ConfigExam[0].Exam_Start_Date} - ${ConfigExam[0].Exam_End_Date}`,
-                Background: "LightGreen",
+                Background: "#A5D6A7",
                 IsLink: true,
                 fontWeight: 'bold',
                 IsConfigured: true,
