0498884d3e710a89e6b73f61bdc7fdd6124319d4
Harshwardhanchougule <harshchougule25@gmail.com>
2024-12-30 17:13:39 +0530

    Fix : Profile Page

--- src/librariesWeb/Profile.tsx
+++ src/librariesWeb/Profile.tsx
@@ -4,49 +4,74 @@ import Header from './Header';
 import UserPhoto from 'src/libraries/UserPhoto/UserPhoto';
 
 const Profile: React.FC = () => {
-    const [tabValue, setTabValue] = useState(0);
+  const [tabValue, setTabValue] = useState(0);
 
-    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-        setTabValue(newValue);
-    };
-    const UserName = sessionStorage.getItem('StudentName') || '-';
-    const DesignationName = sessionStorage.getItem('DesignationName') || '-';
-    const ClassTeacher = sessionStorage.getItem('ClassName') || '-';
-    const MobileNumber = sessionStorage.getItem('MobileNumber') || '-';
-    const ImgUrl = sessionStorage.getItem('PhotoFilePath');
-    const UserLoginDetails1 = localStorage.getItem('UserLoginDetails1');
-    const userPhoto =
-        ImgUrl && ImgUrl.length !== 0
-            ? 'data:image/png;base64,' + ImgUrl
-            : '/imges/defualtUser.jpg';
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
+  const UserName = sessionStorage.getItem('StudentName') || '-';
+  const DesignationName = sessionStorage.getItem('DesignationName') || '-';
+  const ClassTeacher = sessionStorage.getItem('ClassName') || '-';
+  const MobileNumber = sessionStorage.getItem('MobileNumber') || '-';
+  const ImgUrl = sessionStorage.getItem('PhotoFilePath');
+  const UserLoginDetails1 = localStorage.getItem('UserLoginDetails1');
+  const userPhoto =
+    ImgUrl && ImgUrl.length !== 0
+      ? 'data:image/png;base64,' + ImgUrl
+      : '/imges/defualtUser.jpg';
 
-
-    return (
-        <Box sx={{ height: '382px', maxWidth: '50', p: 1, backgroundColor: 'white' }}>
-            <Grid container>
-                <Grid item xs={12}>
-                    <Header Title="Teacher Details" />
-                </Grid>
-                <Grid item md={12} mt={1} ml={18.5}>
-                    {/* <Avatar src={userPhoto}
-                        alt="Profile Picture" sx={{ width: 150, height: 185, }}
-                    /> */}
-                    <UserPhoto
-                        ImgUrl={userPhoto}
-                        alt={'user.name'}
-                        width={'140px'}
-                        height={'180px'}
-                    />
-                </Grid>
-                <Grid item md={12} sx={{ p: 2 }} >
-                    <Typography variant="h4" align='center'>{UserName}</Typography>
-                    <Typography variant="subtitle1" align='center'>{DesignationName} ({ClassTeacher})</Typography>
-                    <Typography variant="body1" align='center'>{MobileNumber}</Typography>
-                    <Typography align='center'><br /><b>Last Login:</b> {UserLoginDetails1}</Typography>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{ height: { xs: 'auto', md: '382px' }, 
+      maxWidth: { xs: '100%', md: '50' }, p: 1, backgroundColor: 'white' }}
+    >
+      <Grid container>
+        <Grid item xs={12}>
+          <Header Title="Teacher Details" />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={12}
+          mt={1}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <UserPhoto
+            ImgUrl={userPhoto}
+            alt={'user.name'}
+            width={'140px'}
+            height={'180px'}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Typography variant="h4" align="center">
+            {UserName}
+          </Typography>
+          <Typography variant="subtitle1" align="center">
+            {DesignationName} ({ClassTeacher})
+          </Typography>
+          <Typography variant="body1" align="center">
+            {MobileNumber}
+          </Typography>
+          <Typography align="center">
+            <br />
+            <b>Last Login:</b> {UserLoginDetails1}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Profile;
