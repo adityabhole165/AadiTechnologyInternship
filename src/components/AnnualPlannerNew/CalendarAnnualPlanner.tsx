e45af6876be0814c16b29a4277f62f421dc4f717
AmanKadam-16 <aman16112002kadam@gmail.com>
2025-01-06 17:59:06 +0530

    fix: Annaul Planner Month / Year issue leading to empty value in dropdown | Annual Planner BaseScreen

--- src/components/AnnualPlannerNew/CalendarAnnualPlanner.tsx
+++ src/components/AnnualPlannerNew/CalendarAnnualPlanner.tsx
@@ -6,12 +6,29 @@ import CalendarList from './CalendarList';
 import SearchAnnualPlanner from './SearchAnnualPlanner';
 
 const CalendarAnnualPlanner = ({ DaysList, ClickDate, ClickCalendarItem, FilterList,
-    ClickFilterItem, SelectedDate, SelectedFilter, EventType, ClickEventType,AnnualPlannerViewAccess }) => {
+    ClickFilterItem, SelectedDate, SelectedFilter, EventType, ClickEventType, AnnualPlannerViewAccess }) => {
+    const allowCalendarNavigator = (selectedYear, selectedMonth, isForward) => {
+        let initialYear = FilterList.YearList[0]?.Value;
+        let endYear = FilterList.YearList[FilterList.YearList.length - 1]?.Value;
+        let initial
+        if (!isForward) { // Check for backward arrow
+            // Disable backward arrow if it's the first year and January
+            if (selectedYear.toString() === initialYear && selectedMonth === 0) {
+                return true;
+            }
+        } else { // Check for forward arrow
+            // Disable forward arrow if it's the last year and December
+            if (selectedYear.toString() === endYear && selectedMonth === 11) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     const handlePrevNextMonth = (PrevNext) => {
         const newDate = new Date(SelectedDate);
         newDate.setDate(1)
         newDate.setMonth(newDate.getMonth() - PrevNext);
-
         if (newDate.getMonth() === 11) {
             newDate.setFullYear(newDate.getFullYear());
         }
@@ -19,45 +36,47 @@ const CalendarAnnualPlanner = ({ DaysList, ClickDate, ClickCalendarItem, FilterL
     }
     return (
         <>
-        <Box sx={{ backgroundColor: 'white' }} p={2}>
-            <Stack direction={'row'} justifyContent={'flex-end'} gap={1}>
+            <Box sx={{ backgroundColor: 'white' }} p={2}>
+                <Stack direction={'row'} justifyContent={'flex-end'} gap={1}>
                     <SearchAnnualPlanner ItemList={FilterList} ClickItem={ClickFilterItem} DefaultValue={SelectedFilter} />
                 </Stack>
-        </Box>
-        <Box sx={{ backgroundColor: 'white' }} p={2}>
-
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconButton color={'primary'} onClick={() => handlePrevNextMonth(1)}
-                        sx={{
-                            backgroundColor: (theme) =>
-                                alpha(theme.palette.primary.main, 0.2),
-                            marginRight: '20px', // Adjusted margin to separate the icons
-                            paddingRight: '12px' // Adjusted padding for spacing
-                        }}
-                    >
-                        <ArrowBackIosNewIcon />
-                    </IconButton>
-                    <Typography m={0} variant={'h3'} sx={{ marginX: '-8px' }}>
-                        <b>{getMonthYearSpaceFormatted(SelectedDate)}</b>
-                    </Typography>
-                    <IconButton color={'primary'} onClick={() => handlePrevNextMonth(-1)}
-                        sx={{
-                            backgroundColor: (theme) =>
-                                alpha(theme.palette.primary.main, 0.2),
-                            marginLeft: '20px', // Adjusted margin to separate the icons
-                            paddingLeft: '12px' // Adjusted padding for spacing
-                        }}
-                    >
-                        <ArrowForwardIosIcon />
-                    </IconButton>
-                </Box>
-                
             </Box>
-            <CalendarList ItemList={DaysList} ClickItem={ClickDate}
-                DefaultValue={SelectedDate} EventType={EventType}
-                ClickEventType={ClickEventType} AnnualPlannerViewAccess={AnnualPlannerViewAccess}/>
-        </Box>
+            <Box sx={{ backgroundColor: 'white' }} p={2}>
+
+                <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <IconButton color={'primary'} onClick={() => handlePrevNextMonth(1)}
+                            sx={{
+                                backgroundColor: (theme) =>
+                                    alpha(theme.palette.primary.main, 0.2),
+                                marginRight: '20px', // Adjusted margin to separate the icons
+                                paddingRight: '12px' // Adjusted padding for spacing
+                            }}
+                            disabled={allowCalendarNavigator(new Date(SelectedDate).getFullYear(), new Date(SelectedDate).getMonth(), false)}
+                        >
+                            <ArrowBackIosNewIcon />
+                        </IconButton>
+                        <Typography m={0} variant={'h3'} sx={{ marginX: '-8px' }}>
+                            <b>{getMonthYearSpaceFormatted(SelectedDate)}</b>
+                        </Typography>
+                        <IconButton color={'primary'} onClick={() => handlePrevNextMonth(-1)}
+                            sx={{
+                                backgroundColor: (theme) =>
+                                    alpha(theme.palette.primary.main, 0.2),
+                                marginLeft: '20px', // Adjusted margin to separate the icons
+                                paddingLeft: '12px' // Adjusted padding for spacing
+                            }}
+                            disabled={allowCalendarNavigator(new Date(SelectedDate).getFullYear(), new Date(SelectedDate).getMonth(), true)}
+                        >
+                            <ArrowForwardIosIcon />
+                        </IconButton>
+                    </Box>
+
+                </Box>
+                <CalendarList ItemList={DaysList} ClickItem={ClickDate}
+                    DefaultValue={SelectedDate} EventType={EventType}
+                    ClickEventType={ClickEventType} AnnualPlannerViewAccess={AnnualPlannerViewAccess} />
+            </Box>
         </>
     )
 }
