35959353fac393e5b8a544940943780fbf75ed9f
Pamyamane <pramodmane043@gmail.com>
2025-01-06 17:23:07 +0530

    SearchAnnualPlanner

--- src/components/AnnualPlannerNew/SearchAnnualPlanner.tsx
+++ src/components/AnnualPlannerNew/SearchAnnualPlanner.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid, Stack } from '@mui/material';
 import Dropdown from 'src/libraries/dropdown/Dropdown';
+import SearchableDropdown from 'src/libraries/ResuableComponents/SearchableDropdown';
 
 const SearchAnnualPlanner = ({ ItemList, ClickItem, DefaultValue }) => {
   const handleChange = (value, selectedItem) => {
@@ -17,49 +18,62 @@ const SearchAnnualPlanner = ({ ItemList, ClickItem, DefaultValue }) => {
     <Grid container spacing={2} alignItems="center" justifyContent={'flex-end'}>
       <Grid item xs={12} sm={6} md={2}>
         <Box sx={{ minWidth: '100%' }}>
-          <Dropdown
+          <SearchableDropdown
+            size="small"
+            sx={{ minWidth: { xs: '40vw', sm: '15vw' } }}
+            ItemList={ItemList.StandardList}
+            label="Select Standard"
+            defaultValue={DefaultValue.Standard}
+            onChange={(value) => handleChange(value, 'Standard')}
+          />
+
+          {/* <Dropdown
             size="small"
             variant="outlined"
             Array={ItemList.StandardList}
             label="Select Standard"
             defaultValue={DefaultValue.Standard}
             handleChange={(value) => handleChange(value, 'Standard')}
-          />
+          /> */}
+
         </Box>
       </Grid>
       <Grid item xs={12} sm={6} md={2}>
         <Box sx={{ minWidth: '100%' }}>
-          <Dropdown
+          <SearchableDropdown
             size="small"
-            variant="outlined"
-            Array={ItemList.StandardDivisionList}
+            sx={{ minWidth: { xs: '40vw', sm: '15vw' } }}
+            ItemList={ItemList.StandardDivisionList}
             label="Select Division"
             defaultValue={DefaultValue.StandardDivision}
-            handleChange={(value) => handleChange(value, 'StandardDivision')}
+            onChange={(value) => handleChange(value, 'StandardDivision')}
           />
         </Box>
       </Grid>
       <Grid item xs={12} sm={6} md={2}>
         <Box sx={{ minWidth: '100%' }}>
-          <Dropdown
+          <SearchableDropdown
             size="small"
-            variant="outlined"
-            Array={ItemList.MonthList}
+            sx={{ minWidth: { xs: '40vw', sm: '15vw' } }}
+            ItemList={ItemList.MonthList}
             label="Select Month"
             defaultValue={DefaultValue.Month}
-            handleChange={(value) => handleChange(value, 'Month')}
+            onChange={(value) => handleChange(value, 'Month')}
           />
+
+
+
         </Box>
       </Grid>
       <Grid item xs={12} sm={6} md={2}>
         <Box sx={{ minWidth: '100%' }}>
-          <Dropdown
+          <SearchableDropdown
             size="small"
-            variant="outlined"
-            Array={ItemList.YearList}
+            sx={{ minWidth: { xs: '40vw', sm: '15vw' } }}
+            ItemList={ItemList.YearList}
             label="Select Year"
             defaultValue={DefaultValue.Year}
-            handleChange={(value) => handleChange(value, 'Year')}
+            onChange={(value) => handleChange(value, 'Year')}
           />
         </Box>
       </Grid>
